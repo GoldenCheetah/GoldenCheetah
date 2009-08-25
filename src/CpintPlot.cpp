d2efc7594887f1a87e90834c77dc280b09714384
Mark Liversedge <markliversedge@mark-liversedges-macbook-pro.local>
2009-08-25 14:20:27 +0100

    Added WKO RideNotes, applied style guide, fixed CP calc, check version of file

--- src/CpintPlot.cpp
+++ src/CpintPlot.cpp
@@ -67,7 +67,7 @@ bool
 is_ride_filename(const QString filename)
 {
     QRegExp re("^([0-9][0-9][0-9][0-9])_([0-9][0-9])_([0-9][0-9])"
-	       "_([0-9][0-9])_([0-9][0-9])_([0-9][0-9])\\.(raw|srm|csv|tcx|hrm)$");
+	       "_([0-9][0-9])_([0-9][0-9])_([0-9][0-9])\\.(raw|srm|csv|tcx|hrm|wko)$");
     return (re.exactMatch(filename));
 }
 
