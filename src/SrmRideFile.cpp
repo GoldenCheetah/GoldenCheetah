GoldenCheetah/GoldenCheetah

commit 5c0bdd8969cf5b8439b1e7a09fee5c9d99eb0c34

Changes to add altitude data to allplot & elevation gained to ride metrics

src/SrmRideFile.cpp
@@ -37,7 +37,7 @@ RideFile *SrmFileReader::openRideFile(QFile &file, QStringList &errors) const
         SrmDataPoint *p = i.next();
         double nm = p->watts / 2.0 / PI / p->cad * 60.0;
         rideFile->appendPoint(p->secs, p->cad, p->hr, p->km, 
-                              p->kph, nm, p->watts, p->interval);
+                              p->kph, nm, p->watts, p->alt, p->interval);
     }
     return rideFile;
 }

src/srm.h
@@ -29,7 +29,7 @@
 struct SrmDataPoint 
 {
     int cad, hr, watts, interval;
-    double kph, km, secs;
+    double kph, km, secs, alt;
 };
 
 struct SrmData 

src/AllPlot.cpp
@@ -167,6 +167,7 @@ static inline double
 max(double a, double b) { if (a > b) return a; else return b; }
 
 #define MILES_PER_KM 0.62137119
+#define FEET_PER_M 3.2808399
 
 AllPlot::AllPlot():
     settings(NULL),
@@ -174,7 +175,7 @@ AllPlot::AllPlot():
     d_mrk(NULL), rideItem(NULL),
     hrArray(NULL), wattsArray(NULL), 
     speedArray(NULL), cadArray(NULL), timeArray(NULL),
-    distanceArray(NULL), interArray(NULL), smooth(30), bydist(false),
+    distanceArray(NULL), altArray(NULL), interArray(NULL), smooth(30), bydist(false),
     shade_zones(false)
 {
     QSettings *settings;
@@ -218,6 +219,16 @@ AllPlot::AllPlot():
     cadPen.setWidth(2);
     cadCurve->setPen(cadPen);
 
+    altCurve = new QwtPlotCurve("Altitude");
+    // altCurve->setRenderHint(QwtPlotItem::RenderAntialiased);
+    QPen *altPen  = new QPen(QColor(124, 91, 31));
+    altPen->setWidth(1);
+    altCurve->setPen(*altPen);
+    QColor brush_color = QColor(124, 91, 31);
+    brush_color.setAlpha(64);
+    altCurve->setBrush(brush_color);   // fill below the line
+    delete altPen;
+
     grid = new QwtPlotGrid();
     grid->enableX(false);
     QPen gridPen;
@@ -229,10 +240,10 @@ AllPlot::AllPlot():
 }
 
 struct DataPoint {
-    double time, hr, watts, speed, cad;
+    double time, hr, watts, speed, cad, alt;
     int inter;
-    DataPoint(double t, double h, double w, double s, double c, int i) : 
-        time(t), hr(h), watts(w), speed(s), cad(c), inter(i) {}
+    DataPoint(double t, double h, double w, double s, double c, double a, int i) : 
+        time(t), hr(h), watts(w), speed(s), cad(c), alt(a), inter(i) {}
 };
 
 bool AllPlot::shadeZones() const
@@ -287,6 +298,8 @@ AllPlot::recalc()
 	    speedCurve->setData(data, data);
 	if (cadArray)
 	    cadCurve->setData(data, data);
+	if (altArray)
+	    altCurve->setData(data, data);
         return;
     }
     double totalWatts = 0.0;
@@ -294,6 +307,7 @@ AllPlot::recalc()
     double totalSpeed = 0.0;
     double totalCad = 0.0;
     double totalDist = 0.0;
+    double totalAlt = 0.0;
 
     QList<DataPoint*> list;
 
@@ -303,6 +317,7 @@ AllPlot::recalc()
     double *smoothCad       = new double[rideTimeSecs + 1];
     double *smoothTime      = new double[rideTimeSecs + 1];
     double *smoothDistance  = new double[rideTimeSecs + 1];
+    double *smoothAltitude  = new double[rideTimeSecs + 1];
 
     delete [] d_mrk;
     QList<double> interList; //Just store the time that it happened.
@@ -318,6 +333,7 @@ AllPlot::recalc()
         smoothCad[secs]   = 0.0;
         smoothTime[secs]  = secs / 60.0;
         smoothDistance[secs]  = 0.0;
+        smoothAltitude[secs]  = 0.0;
     }
 
     int i = 0;
@@ -330,6 +346,7 @@ AllPlot::recalc()
 			      (wattsArray ? wattsArray[i] : 0),
                               (speedArray ? speedArray[i] : 0),
 			      (cadArray ? cadArray[i] : 0),
+			      (altArray ? altArray[i] : 0),
 			      interArray[i]
 			      );
 	    if (wattsArray)
@@ -340,6 +357,8 @@ AllPlot::recalc()
 		totalSpeed += speedArray[i];
 	    if (cadArray)
 		totalCad   += cadArray[i];
+	    if (altArray)
+		totalAlt   += altArray[i];
             totalDist   = distanceArray[i];
             list.append(dp);
             //Figure out when and if we have a new interval..
@@ -356,6 +375,7 @@ AllPlot::recalc()
             totalHr    -= dp->hr;
             totalSpeed -= dp->speed;
             totalCad   -= dp->cad;
+            totalAlt   -= dp->alt;
             delete dp;
         }
         // TODO: this is wrong.  We should do a weighted average over the
@@ -365,12 +385,14 @@ AllPlot::recalc()
             smoothHr[secs]    = 0.0;
             smoothSpeed[secs] = 0.0;
             smoothCad[secs]   = 0.0;
+            smoothAltitude[secs]   = smoothAltitude[secs - 1];
         }
         else {
             smoothWatts[secs]    = totalWatts / list.size();
             smoothHr[secs]       = totalHr / list.size();
             smoothSpeed[secs]    = totalSpeed / list.size();
             smoothCad[secs]      = totalCad / list.size();
+	    smoothAltitude[secs]      = totalAlt / list.size();
         }
         smoothDistance[secs] = totalDist;
         smoothTime[secs]  = secs / 60.0;
@@ -387,6 +409,8 @@ AllPlot::recalc()
         speedCurve->setData(xaxis, smoothSpeed, rideTimeSecs + 1);
     if (cadArray)
         cadCurve->setData(xaxis, smoothCad, rideTimeSecs + 1);
+    if (altArray)
+        altCurve->setData(xaxis, smoothAltitude, rideTimeSecs + 1);
 
     setAxisScale(xBottom, 0.0, bydist ? totalDist : smoothTime[rideTimeSecs]);
     setYMax();
@@ -429,6 +453,8 @@ AllPlot::recalc()
         delete [] smoothTime;
     if(smoothDistance != NULL)
        delete [] smoothDistance;
+    if(smoothAltitude != NULL)
+       delete [] smoothAltitude;
 }
 
 void
@@ -448,6 +474,15 @@ AllPlot::setYMax()
         ymax = max(ymax, cadCurve->maxYValue());
         ylabel += QString((ylabel == "") ? "" : " / ") + "RPM";
     }
+    if (altCurve->isVisible()) {
+        ymax = max(ymax, altCurve->maxYValue());
+	if (useMetricUnits){
+        	ylabel += QString((ylabel == "") ? "" : " / ") + "Meters";
+	} else {
+		ylabel += QString((ylabel == "") ? "" : " / ") + "Ft";
+	}
+    }
+
     setAxisScale(yLeft, 0.0, ymax * 1.1);
     setAxisTitle(yLeft, ylabel);
 
@@ -483,6 +518,8 @@ AllPlot::setData(RideItem *_rideItem)
         delete [] interArray;
     if(distanceArray != NULL)
         delete [] distanceArray;
+    if(altArray != NULL)
+        delete [] altArray;
 
     RideFile *ride = rideItem->ride;
     if (ride) {
@@ -494,6 +531,7 @@ AllPlot::setData(RideItem *_rideItem)
 	hrArray    = dataPresent->hr ? new double[npoints] : NULL;
 	speedArray = dataPresent->kph ? new double[npoints] : NULL;
 	cadArray   = dataPresent->cad ? new double[npoints] : NULL;
+	altArray   = dataPresent->alt ? new double[npoints] : NULL;
 	timeArray  = new double[npoints];
 	interArray = new int[npoints];
 	distanceArray = new double[npoints];
@@ -503,10 +541,12 @@ AllPlot::setData(RideItem *_rideItem)
 	hrCurve->detach();
 	speedCurve->detach();
 	cadCurve->detach();
+	altCurve->detach();
 	if (wattsArray) wattsCurve->attach(this);
 	if (hrArray) hrCurve->attach(this);
 	if (speedArray) speedCurve->attach(this);
 	if (cadArray) cadCurve->attach(this);
+	if (altArray) altCurve->attach(this);
 
 	arrayLength = 0;
 	QListIterator<RideFilePoint*> i(ride->dataPoints()); 
@@ -524,6 +564,11 @@ AllPlot::setData(RideItem *_rideItem)
 					       : point->kph * MILES_PER_KM));
 	    if (cadArray)
 		cadArray[arrayLength]   = max(0, point->cad);
+            if (altArray)
+                altArray[arrayLength]   = max(0,
+                                              (useMetricUnits
+                                               ? point->alt
+                                               : point->alt * FEET_PER_M));
 	    interArray[arrayLength] = point->interval;
 	    distanceArray[arrayLength] = max(0,
 					     (useMetricUnits
@@ -540,6 +585,7 @@ AllPlot::setData(RideItem *_rideItem)
 	hrCurve->detach();
 	speedCurve->detach();
 	cadCurve->detach();
+	altCurve->detach();
     }
 }
 
@@ -575,6 +621,15 @@ AllPlot::showCad(int state)
 {
     assert(state != Qt::PartiallyChecked);
     cadCurve->setVisible(state == Qt::Checked);
+    setYMax();
+    replot();
+}
+
+void
+AllPlot::showAlt(int state) 
+{
+    assert(state != Qt::PartiallyChecked);
+    altCurve->setVisible(state == Qt::Checked);
     setYMax();
     replot();
 }

src/AllPlot.h
@@ -49,6 +49,7 @@ class AllPlot : public QwtPlot
         QwtPlotCurve *hrCurve;
         QwtPlotCurve *speedCurve;
         QwtPlotCurve *cadCurve;
+        QwtPlotCurve *altCurve;
         QwtPlotMarker *d_mrk;
 	QList <AllPlotZoneLabel *> zoneLabels;
 
@@ -71,6 +72,7 @@ class AllPlot : public QwtPlot
         void showHr(int state);
         void showSpeed(int state);
         void showCad(int state);
+        void showAlt(int state);
         void showGrid(int state);
         void setSmoothing(int value);
         void setByDistance(int value);
@@ -85,6 +87,7 @@ class AllPlot : public QwtPlot
         double *cadArray;
         double *timeArray;
         double *distanceArray;
+        double *altArray;
         int arrayLength;
         int *interArray;
 

src/BasicRideMetrics.cpp
@@ -19,6 +19,7 @@
 #include "RideMetric.h"
 
 #define MILES_PER_KM 0.62137119
+#define FEET_PER_METER 3.2808399
 
 class WorkoutTime : public RideMetric {
     double seconds;
@@ -91,6 +92,44 @@ class TotalDistance : public RideMetric {
 
 static bool totalDistanceAdded =
     RideMetricFactory::instance().addMetric(TotalDistance());
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+class ElevationGain : public PointwiseRideMetric {
+    double elegain;
+    double prevalt;
+
+    public:
+
+    ElevationGain() : elegain(0.0) {}
+    QString name() const { return "elevation_gain"; }
+    QString units(bool metric) const { return metric ? "meters" : "feet"; }
+    double value(bool metric) const {
+        return metric ? elegain : (elegain * FEET_PER_METER);
+    }
+    void perPoint(const RideFilePoint *point, double,
+                  const RideFile *, const Zones *, int) {
+
+	if (prevalt <= 0){
+		prevalt = point->alt;
+	} else if (prevalt <= point->alt) {
+		elegain += (point->alt-prevalt);
+		prevalt = point->alt;
+	} else {
+		prevalt = point->alt;
+	}
+
+    }
+    bool canAggregate() const { return true; }
+    void aggregateWith(RideMetric *other) {
+        elegain += other->value(true);
+    }
+    RideMetric *clone() const { return new ElevationGain(*this); }
+};
+
+static bool elevationGainAdded =
+    RideMetricFactory::instance().addMetric(ElevationGain());
 
 //////////////////////////////////////////////////////////////////////////////
 

src/CsvRideFile.cpp
@@ -126,7 +126,7 @@ RideFile *CsvFileReader::openRideFile(QFile &file, QStringList &errors) const
                 }
             }
             else if (lineno > unitsHeader) {
-                double minutes,nm,kph,watts,km,cad,hr;
+                double minutes,nm,kph,watts,km,cad,alt,hr;
                 int interval;
                 int pause;
                 if (!ergomo && !iBike) {
@@ -137,7 +137,8 @@ RideFile *CsvFileReader::openRideFile(QFile &file, QStringList &errors) const
                      km		 = line.section(',', 4, 4).toDouble();
                      cad	 = line.section(',', 5, 5).toDouble();
                      hr		 = line.section(',', 6, 6).toDouble();
-                     interval	= line.section(',', 7, 7).toInt();
+                     interval	 = line.section(',', 7, 7).toInt();
+                     alt	 = line.section(',', 8, 8).toDouble();
                     if (!metric) {
                         km *= MILES_TO_KM;
                         kph *= MILES_TO_KM;
@@ -191,7 +192,7 @@ RideFile *CsvFileReader::openRideFile(QFile &file, QStringList &errors) const
                     watts = 0;
                 
                 rideFile->appendPoint(minutes * 60.0, cad, hr, km, 
-                                      kph, nm, watts, interval);
+                                      kph, nm, watts, alt, interval);
             }
         ++lineno;
         }

src/MainWindow.cpp
@@ -194,6 +194,10 @@ MainWindow::MainWindow(const QDir &home) :
     showCad->setCheckState(Qt::Checked);
     showLayout->addWidget(showCad);
 
+    showAlt = new QCheckBox("Altitude", window);
+    showAlt->setCheckState(Qt::Checked);
+    showLayout->addWidget(showAlt);
+
     showPower = new QComboBox();
     showPower->addItem(tr("Power + shade"));
     showPower->addItem(tr("Power - shade"));
@@ -513,6 +517,8 @@ MainWindow::MainWindow(const QDir &home) :
             allPlot, SLOT(showSpeed(int)));
     connect(showCad, SIGNAL(stateChanged(int)),
             allPlot, SLOT(showCad(int)));
+    connect(showAlt, SIGNAL(stateChanged(int)),
+            allPlot, SLOT(showAlt(int)));
     connect(showGrid, SIGNAL(stateChanged(int)),
             allPlot, SLOT(showGrid(int)));
     connect(comboDistance, SIGNAL(currentIndexChanged(int)),
@@ -1938,17 +1944,20 @@ void MainWindow::setAllPlotWidgets(RideItem *ride)
 	showHr->setEnabled(dataPresent->hr);
 	showSpeed->setEnabled(dataPresent->kph);
 	showCad->setEnabled(dataPresent->cad);
+	showAlt->setEnabled(dataPresent->alt);
 	allPlot->showPower(showPower->currentIndex());
 	allPlot->wattsCurve->setVisible(dataPresent->watts && (showPower->currentIndex() < 2));
 	allPlot->hrCurve->setVisible(dataPresent->hr && showHr->isChecked());
 	allPlot->speedCurve->setVisible(dataPresent->kph && showSpeed->isChecked());
 	allPlot->cadCurve->setVisible(dataPresent->cad && showCad->isChecked());
+	allPlot->altCurve->setVisible(dataPresent->alt && showAlt->isChecked());
     }
     else {
 	showPower->setEnabled(false);
 	showHr->setEnabled(false);
 	showSpeed->setEnabled(false);
 	showCad->setEnabled(false);
+	showAlt->setEnabled(false);
 	allPlot->showPower(false);
 	allPlot->wattsCurve->setVisible(false);
 	allPlot->hrCurve->setVisible(false);

src/MainWindow.h
@@ -115,6 +115,7 @@ class MainWindow : public QMainWindow
 	QCheckBox *showHr;
 	QCheckBox *showSpeed;
 	QCheckBox *showCad;
+	QCheckBox *showAlt;
 	QComboBox *showPower;
         CpintPlot *cpintPlot;
         QLineEdit *cpintTimeValue;
@@ -162,6 +163,7 @@ class MainWindow : public QMainWindow
 	int histHrID;
 	int histKphID;
 	int histCadID;
+	int histAltID;
 
 	bool useMetricUnits;  // whether metric units are used (or imperial)
 

commit 819235ebd7d948898951c31eb89217f702c03d1c

bug fix: no altitude in srm files
This bug was introduced in 5c0bdd89, which added an alt member to
SrmDataPoint without initializing it.

src/SrmRideFile.cpp
@@ -37,7 +37,7 @@ RideFile *SrmFileReader::openRideFile(QFile &file, QStringList &errors) const
         SrmDataPoint *p = i.next();
         double nm = p->watts / 2.0 / PI / p->cad * 60.0;
         rideFile->appendPoint(p->secs, p->cad, p->hr, p->km, 
-                              p->kph, nm, p->watts, p->alt, p->interval);
+                              p->kph, nm, p->watts, 0.0, p->interval);
     }
     return rideFile;
 }

src/srm.h
@@ -29,7 +29,7 @@
 struct SrmDataPoint 
 {
     int cad, hr, watts, interval;
-    double kph, km, secs, alt;
+    double kph, km, secs;
 };
 
 struct SrmData 