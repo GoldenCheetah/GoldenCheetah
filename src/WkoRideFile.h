afb3fb6d622fff256a129e00e6650f749feaff66
Mark Liversedge <markliversedge@mark-liversedges-macbook-pro.local>
2009-09-03 19:21:16 +0100

    WKO Fix for 64bit, Big Endian and Interval should be 0
    Signed-off-by: Robert Carlsen <robert@robertcarlsen.net>

--- src/WkoRideFile.h
+++ src/WkoRideFile.h
@@ -19,13 +19,23 @@
 #ifndef _WkoRideFile_h
 #define _WkoRideFile_h
 
+#include <stdint.h>
 #include "RideFile.h"
 #include <QDataStream>
 #include <string.h>
 #include <math.h>
 #include <boost/scoped_ptr.hpp>
 #include <boost/scoped_array.hpp>
+#include <QtEndian>
 
+// Explicitly define types using unistd.h definitions to
+// ensure char IS 8 bits and short IS 16 bits and long IS 32 bits
+#define WKO_UCHAR   uint8_t
+#define WKO_CHAR   int8_t
+#define WKO_USHORT uint16_t
+#define WKO_SHORT  int16_t
+#define WKO_ULONG  uint32_t
+#define WKO_LONG   int32_t
 struct WkoFileReader : public RideFileReader {
     RideFile *openRideFile(QFile &file, QStringList &errors) const;
 
@@ -33,28 +43,29 @@ struct WkoFileReader : public RideFileReader {
 
 typedef std::auto_ptr<QDataStream> QDataStreamPtr;
 
-unsigned char *WkoParseHeaderData(unsigned char *data, RideFile *rideFile) ;
-unsigned char *WkoParseRawData(unsigned char *data, RideFile *rideFile) ;
+WKO_UCHAR *WkoParseHeaderData(WKO_UCHAR *data, RideFile *rideFile) ;
+WKO_UCHAR *WkoParseRawData(WKO_UCHAR *data, RideFile *rideFile) ;
 
 // Some Globals -- try and remove them as I refactor code from the original WKO2CSV source
 QString WKO_HOMEDIR;
 
 // UTILITY FUNCTIONS
 // Field decoding
-unsigned int doshort(unsigned char *p, unsigned short *pnum);
-unsigned int donumber(unsigned char *p, unsigned long *pnum);
-unsigned int dotext(unsigned char *p, unsigned char *txt);unsigned int dofloat(unsigned char *p, float *pnum);
-unsigned int dodouble(unsigned char *p, double *pnum);
-unsigned int optpad(unsigned char *p),
-             optpad2(unsigned char *p);
+unsigned int doshort(WKO_UCHAR *p, WKO_USHORT *pnum);
+unsigned int donumber(WKO_UCHAR *p, WKO_ULONG *pnum);
+unsigned int dotext(WKO_UCHAR *p, WKO_UCHAR *txt);
+unsigned int dofloat(WKO_UCHAR *p, float *pnum);
+unsigned int dodouble(WKO_UCHAR *p, double *pnum);
+unsigned int optpad(WKO_UCHAR *p),
+             optpad2(WKO_UCHAR *p);
 
 // Bit twiddlers
-int get_bit(unsigned char *data, unsigned bitoffset); // returns 0 or 1
-unsigned int get_bits(unsigned char* data, unsigned bitOffset, unsigned numBits); // returns 32 bit unsigned
+int get_bit(WKO_UCHAR *data, unsigned bitoffset); // returns 0 or 1
+unsigned int get_bits(WKO_UCHAR* data, unsigned bitOffset, unsigned numBits); // returns 32 bit unsigned
 
 // Decoding setup
 void setxormasks();
-unsigned long nullvals(char graph);
+WKO_ULONG nullvals(char graph);
 unsigned long bitget(char *thelot, int offset, int count);
 unsigned int bitsize(char graph, int device);
 
