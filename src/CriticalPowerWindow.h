6f6f891fd7b9a6225044a981b85ab1f62e35e06f
Sean Rhea <sean.c.rhea@gmail.com>
2009-10-07 09:37:55 -0400

    abstract "Critical Power Plot" tab into its own class

---
+++ src/CriticalPowerWindow.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009 Sean C. Rhea (srhea@srhea.net)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _GC_CriticalPowerWindow_h
+#define _GC_CriticalPowerWindow_h 1
+
+#include <QtGui>
+
+class CpintPlot;
+class MainWindow;
+class RideItem;
+class QwtPlotPicker;
+
+class CriticalPowerWindow : public QWidget
+{
+    Q_OBJECT
+
+    public:
+
+        CriticalPowerWindow(const QDir &home, MainWindow *parent);
+
+        void newRideAdded();
+        void deleteCpiFile(QString filename);
+        void setData(RideItem *ride);
+
+    protected slots:
+
+        void cpintSetCPButtonClicked();
+        void pickerMoved(const QPoint &pos);
+
+    protected:
+
+        QDir home;
+        CpintPlot *cpintPlot;
+        MainWindow *mainWindow;
+        QLineEdit *cpintTimeValue;
+        QLineEdit *cpintTodayValue;
+        QLineEdit *cpintAllValue;
+	QPushButton *cpintSetCPButton;
+        QwtPlotPicker *picker;
+};
+
+#endif // _GC_CriticalPowerWindow_h
+
