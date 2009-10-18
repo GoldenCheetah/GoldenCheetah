c630d281d38a3a51998cf579c189144a9b35de99
Justin Knotzke <jknotzke@shampoo.ca>
2009-10-18 13:37:24 -0400

    Added Season support via xml file.
    Using seasons.xml, support now for a season name, start date and end date.
    
    Fixed some memory leaks.

---
+++ src/SeasonParser.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2009 Justin F. Knotzke (jknotzke@shampoo.ca)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <SeasonParser.h>
+#include <QDate>
+#include <QDebug>
+
+bool SeasonParser::startDocument()
+{
+    buffer.clear();
+    return TRUE;
+}
+
+bool SeasonParser::endElement( const QString&, const QString&, const QString &qName )
+{
+    if(qName == "name")
+        season.setName(buffer.trimmed());
+    else if(qName == "startdate")
+        season.setStart(seasonDateToDate(buffer.trimmed()));
+    else if(qName == "season")
+    {
+        if(seasons.size() >= 1)
+            seasons[seasons.size()-1].setEnd(season.getStart());
+        seasons.append(season);
+    }
+    return TRUE;
+}
+
+bool SeasonParser::startElement( const QString&, const QString&, const QString &name, const QXmlAttributes &attrs )
+{
+    buffer.clear();
+    if(name == "season")
+        season = Season();
+
+    return TRUE;
+}
+
+bool SeasonParser::characters( const QString& str )
+{
+    buffer += str;
+    return TRUE;
+}
+
+QList<Season> SeasonParser::getSeasons()
+{
+    return seasons;
+}
+
+QDate SeasonParser::seasonDateToDate(QString seasonDate)
+{
+    QRegExp rx(".*([0-9][0-9][0-9][0-9])-([0-9][0-9])-([0-9][0-9]$)");
+        if (rx.exactMatch(seasonDate)) {
+            assert(rx.numCaptures() == 3);
+            QDate date = QDate(
+                               rx.cap(1).toInt(),
+                               rx.cap(2).toInt(),
+                               rx.cap(3).toInt()
+                               );
+
+            if (! date.isValid()) {
+                return QDate();
+            }
+            else
+                return date;
+        }
+        else
+            return QDate();          // return value was 1 Jan: changed to null
+}
+bool SeasonParser::endDocument()
+{
+    seasons[seasons.size()-1].setEnd(QDate::currentDate().addYears(10)); //Go 10 years into the future (GC's version of infinity).
+}
--- src/SeasonParser.h
+++ src/SeasonParser.h
@@ -1,56 +1,43 @@
-/* 
+/*
  * Copyright (c) 2009 Justin F. Knotzke (jknotzke@shampoo.ca)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef _GC_DBAccess_h
-#define _GC_DBAccess_h 1
+#ifndef _seasonparser_h
+#define _seasonparser_h
 
+#include <QXmlDefaultHandler>
+#include "Season.h"
 
-
-#import <QDir>
-#import <QHash>
-#import <QtSql>
-#import "SummaryMetrics.h"
-#import "Season.h"
-
-class RideFile;
-class Zones;
-class RideMetric;
-class DBAccess
+class SeasonParser : public QXmlDefaultHandler
 {
-	
-	public:
-	DBAccess(QDir home);
-	typedef QHash<QString,RideMetric*> MetricMap;
-	void importAllRides(QDir path, Zones *zones);
-	bool importRide(SummaryMetrics *summaryMetrics);
-	bool createDatabase();
-	QStringList getAllFileNames();
-    void closeConnection();
-	QList<QDateTime> getAllDates();
-    QList<SummaryMetrics> getAllMetricsFor(QDateTime start, QDateTime end);
-    bool createMetricsTable();
-    QList<Season> getAllSeasons();
-    bool dropMetricTable();
 
-	private:
-	QSqlDatabase db;
-	bool createIndex();
-	QSqlDatabase initDatabase(QDir home);
+public:
+    bool startDocument();
+    bool endDocument();
+    bool endElement( const QString&, const QString&, const QString &qName );
+    bool startElement( const QString&, const QString&, const QString &name, const QXmlAttributes &attrs );
+    bool characters( const QString& str );
+    QList<Season> getSeasons();
+
+protected:
+    QString buffer;
+    QDate seasonDateToDate(QString);
+    Season season;
+    QList<Season> seasons;
 
 };
-#endif
+#endif //SeasonParser
