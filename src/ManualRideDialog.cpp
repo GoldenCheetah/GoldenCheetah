GoldenCheetah/GoldenCheetah

commit 23b2ff416ce9e4c647e2c24a8c9c60af466316eb

add Daniels Points to manual ride entry
And estimate them based on time/distance just like we do with BikeScore.

src/ManualRideDialog.cpp
@@ -33,7 +33,7 @@ ManualRideDialog::ManualRideDialog(MainWindow *mainWindow,
     useMetricUnits = useMetric;
     int row;
 
-    mainWindow->getBSFactors(timeBS,distanceBS);
+    mainWindow->getBSFactors(timeBS,distanceBS,timeDP,distanceDP);
 
     setAttribute(Qt::WA_DeleteOnClose);
     setWindowTitle(tr("Manually Enter Ride Data"));
@@ -122,6 +122,12 @@ ManualRideDialog::ManualRideDialog(MainWindow *mainWindow,
     BSentry->setInputMask("009");
     BSentry->clear();
 
+    // DanielsPoints
+    QLabel *ManualDPLabel = new QLabel(tr("Daniels Points: "), this);
+    DPentry = new QLineEdit(this);
+    DPentry->setInputMask("009");
+    DPentry->clear();
+
     // buttons
     enterButton = new QPushButton(tr("&OK"), this);
     cancelButton = new QPushButton(tr("&Cancel"), this);
@@ -161,6 +167,10 @@ ManualRideDialog::ManualRideDialog(MainWindow *mainWindow,
     glayout->addWidget(BSentry,row,1,1,-1);
     row++;
 
+    glayout->addWidget(ManualDPLabel,row,0);
+    glayout->addWidget(DPentry,row,1,1,-1);
+    row++;
+
     glayout->addWidget(enterButton,row,1);
     glayout->addWidget(cancelButton,row,2);
 
@@ -182,13 +192,13 @@ void
 ManualRideDialog::estBSFromDistance()
 {
     // calculate distance-based BS estimate
-    double bs =  0;
     if (distanceBS) {
-        bs = distanceentry->text().toFloat() * distanceBS;
-        QString text = QString("%1").arg((int)bs);
+        double dist = distanceentry->text().toFloat();
         // cast to int so QLineEdit doesn't interpret "51.3" as "513"
         BSentry->clear();
-        BSentry->insert(text);
+        BSentry->insert(QString("%1").arg((int) (dist * distanceBS)));
+        DPentry->clear();
+        DPentry->insert(QString("%1").arg((int) (dist * distanceDP)));
     }
 }
 
@@ -196,14 +206,14 @@ void
 ManualRideDialog::estBSFromTime()
 {
     // calculate time-based BS estimate
-    double bs =  0;
     if (timeBS) {
-        bs = (hrsentry->text().toInt() * timeBS ) +
-            ((minsentry->text().toInt() * timeBS) / 60) +
-            ((secsentry->text().toInt() * timeBS) / 3600);
-        QString text = QString("%1").arg((int)bs);
+        double hrs = hrsentry->text().toInt()
+            + minsentry->text().toInt() / 60
+            + secsentry->text().toInt() / 3600;
         BSentry->clear();
-        BSentry->insert(text);
+        BSentry->insert(QString("%1").arg((int)(hrs * timeBS)));
+        DPentry->clear();
+        DPentry->insert(QString("%1").arg((int)(hrs * timeDP)));
     }
 
 }
@@ -304,9 +314,9 @@ ManualRideDialog::enterClicked()
 
         out << "manual\n";
         if (useMetricUnits)
-            out << "minutes,kmh,watts,km,hr,bikescore\n";
+            out << "minutes,kmh,watts,km,hr,bikescore,daniels_points\n";
         else
-            out << "minutes,mph,watts,miles,hr,bikescore\n";
+            out << "minutes,mph,watts,miles,hr,bikescore,daniels_points\n";
 
         // data
         double secs = (hrsentry->text().toInt() * 3600) +
@@ -323,6 +333,8 @@ ManualRideDialog::enterClicked()
         out << HRentry->text().toInt();
         out << ",";
         out << BSentry->text().toInt();
+        out << ",";
+        out << DPentry->text().toInt();
         out << "\n";
 
         tmp.close();

src/MainWindow.cpp
@@ -963,15 +963,16 @@ MainWindow::intervalTreeWidgetSelectionChanged()
     intervalSelected();
 }
 
-void MainWindow::getBSFactors(double &timeBS, double &distanceBS)
+void MainWindow::getBSFactors(double &timeBS, double &distanceBS,
+                              double &timeDP, double &distanceDP)
 {
     int rides;
-    double seconds, distance, bs;
+    double seconds, distance, bs, dp;
     QProgressDialog * progress;
     bool aborted = false;
     seconds = rides = 0;
-    distance = bs = 0;
-    timeBS = distanceBS = 0.0;
+    distance = bs = dp = 0;
+    timeBS = distanceBS = timeDP = distanceDP = 0.0;
 
     QVariant BSdays = settings->value(GC_BIKESCOREDAYS);
     if (BSdays.isNull() || BSdays.toInt() == 0)
@@ -1017,6 +1018,10 @@ void MainWindow::getBSFactors(double &timeBS, double &distanceBS)
                     distance += m->value(true);
                 }
 
+                if ((m = item->metrics.value("daniels_points"))) {
+                    dp += m->value(true);
+                }
+
                 rides++;
             }
             // check progress
@@ -1034,10 +1039,12 @@ void MainWindow::getBSFactors(double &timeBS, double &distanceBS)
             distance *= MILES_PER_KM;
         timeBS = (bs * 3600) / seconds;  // BS per hour
         distanceBS = bs / distance;  // BS per mile or km
+        timeDP = (dp * 3600) / seconds;  // DP per hour
+        distanceDP = dp / distance;  // DP per mile or km
     }
 done:
     if (aborted) {
-        timeBS = distanceBS = 0;
+        timeBS = distanceBS = timeDP = distanceDP = 0;
     }
 
     delete progress;

src/MainWindow.h
@@ -59,7 +59,8 @@ class MainWindow : public QMainWindow
         const QTreeWidgetItem *allRideItems() { return allRides; }
         const QTreeWidgetItem *allIntervalItems() { return allIntervals; }
         QTreeWidgetItem *mutableIntervalItems() { return allIntervals; }
-	void getBSFactors(double &timeBS, double &distanceBS);
+	void getBSFactors(double &timeBS, double &distanceBS,
+                          double &timeDP, double &distanceDP);
         QDir home;
         void setCriticalPower(int cp);
 

src/ManualRideDialog.h
@@ -44,7 +44,7 @@ class ManualRideDialog : public QDialog
 	void estBSFromTime();
 
 	bool useMetricUnits;
-	double timeBS, distanceBS;
+	double timeBS, distanceBS, timeDP, distanceDP;
         MainWindow *mainWindow;
         QDir home;
         QPushButton *enterButton, *cancelButton;
@@ -54,7 +54,7 @@ class ManualRideDialog : public QDialog
 	QLineEdit *hrsentry, *minsentry, *secsentry; 
 	QLabel * distancelbl;
 	QLineEdit *distanceentry;
-	QLineEdit *HRentry, *BSentry;
+	QLineEdit *HRentry, *BSentry, *DPentry;
 	QDateTimeEdit *dateTimeEdit;
 	QRadioButton *estBSbyTimeButton, *estBSbyDistButton;
 

commit 0edc94d00a3a439e07073b73ef5c183250c516fc

bug fix: integer division error
I introduced this bug in commit 23b2ff4.

src/ManualRideDialog.cpp
@@ -208,8 +208,8 @@ ManualRideDialog::estBSFromTime()
     // calculate time-based BS estimate
     if (timeBS) {
         double hrs = hrsentry->text().toInt()
-            + minsentry->text().toInt() / 60
-            + secsentry->text().toInt() / 3600;
+            + minsentry->text().toInt() / 60.0
+            + secsentry->text().toInt() / 3600.0;
         BSentry->clear();
         BSentry->insert(QString("%1").arg((int)(hrs * timeBS)));
         DPentry->clear();