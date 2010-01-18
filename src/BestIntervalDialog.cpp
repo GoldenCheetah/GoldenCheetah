46ef47d2543695ed1f5a3e903db36491d21f66d8
Sean Rhea <sean.c.rhea@gmail.com>
2010-01-18 10:06:17 -0800

    remove eol spaces -- no functional change

--- src/BestIntervalDialog.cpp
+++ src/BestIntervalDialog.cpp
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (c) 2008 Sean C. Rhea (srhea@srhea.net)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -23,7 +23,7 @@
 #include <assert.h>
 #include <math.h>
 
-BestIntervalDialog::BestIntervalDialog(MainWindow *mainWindow) : 
+BestIntervalDialog::BestIntervalDialog(MainWindow *mainWindow) :
     mainWindow(mainWindow)
 {
     setAttribute(Qt::WA_DeleteOnClose);
@@ -84,8 +84,8 @@ BestIntervalDialog::BestIntervalDialog(MainWindow *mainWindow) :
     resultsTable->horizontalHeader()->hide();
 //    resultsTable->verticalHeader()->hide();
     resultsTable->setShowGrid(false);
- 
-    QHBoxLayout *buttonLayout = new QHBoxLayout; 
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
     findButton = new QPushButton(tr("&Find Intervals"), this);
     buttonLayout->addWidget(findButton);
     doneButton = new QPushButton(tr("&Done"), this);
@@ -111,7 +111,7 @@ clearResultsTable(QTableWidget *resultsTable)
     }
 }
 
-void 
+void
 BestIntervalDialog::findClicked()
 {
     const RideFile *ride = mainWindow->currentRide();
@@ -126,7 +126,7 @@ BestIntervalDialog::findClicked()
                              + secsSpinBox->value());
 
     if (windowSizeSecs == 0.0) {
-        QMessageBox::critical(this, tr("Bad Interval Length"), 
+        QMessageBox::critical(this, tr("Bad Interval Length"),
                               tr("Interval length must be greater than zero!"));
         return;
     }
@@ -258,7 +258,7 @@ BestIntervalDialog::findClicked()
     resultsTable->setColumnWidth(2,200);
 }
 
-void 
+void
 BestIntervalDialog::doneClicked()
 {
     clearResultsTable(resultsTable); // clear out that table!
