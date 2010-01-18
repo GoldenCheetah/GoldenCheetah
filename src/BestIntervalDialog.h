075829500f6c588cb5beab97d4e415d0531ad14c
Sean Rhea <sean.c.rhea@gmail.com>
2010-01-18 10:06:17 -0800

    remove eol spaces -- no functional change

--- src/BestIntervalDialog.h
+++ src/BestIntervalDialog.h
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (c) 2008 Sean C. Rhea (srhea@srhea.net)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -23,7 +23,7 @@
 
 class MainWindow;
 
-class BestIntervalDialog : public QDialog 
+class BestIntervalDialog : public QDialog
 {
     Q_OBJECT
 
@@ -36,7 +36,7 @@ class BestIntervalDialog : public QDialog
         void addClicked(); // add to inverval selections
 
     private:
- 
+
         QMap<double,double> results;
 
         MainWindow *mainWindow;
