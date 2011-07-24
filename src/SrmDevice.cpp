dbadd1b3946612f9bd1c9202765774e02742198c
Rainer Clasen <bj@zuto.de>
2011-07-24 12:19:40 +0200

    SrmDevice: switched to full API
    implemented preview().
    
    download() now gets only selected blocks.
    download() splits data into several files.

--- src/SrmDevice.cpp
+++ src/SrmDevice.cpp
@@ -38,18 +38,6 @@ SrmDevices::newDevice( CommPortPtr dev )
     return DevicePtr( new SrmDevice( dev, protoVersion));
 }
 
-static Device::ProgressCallback cb;
-
-static void progress( size_t total, size_t done, void *user_data )
-{
-    (void)user_data;
-
-    // XXX: hack, unnecessary when switching from srmio_pc_xfer_all
-    cb( tr("progress: %1/%2")
-        .arg(done)
-        .arg(total));
-}
-
 static bool
 get_tmpname(const QDir &tmpdir, QString &tmpname, QString &err)
 {
@@ -167,10 +155,50 @@ SrmDevice::close( void )
 bool
 SrmDevice::preview( StatusCallback statusCallback, QString &err )
 {
-    (void) statusCallback;
-    (void) err;
-    // XXX add preview
+    struct _srmio_pc_xfer_block_t block;
+
+    if( ! is_open ){
+        statusCallback( tr("opening device %1").arg(dev->name()) );
+        if( ! open( err ) )
+            return false;
+    }
+
+    rideList.clear();
+
+    if( ! srmio_pc_can_preview( pc ) )
+        // nothing to do
+        return true;
+
+    if( ! srmio_pc_xfer_start( pc ) ){
+        err = tr("failed to start download: %1")
+            .arg(strerror(errno));
+        goto fail;
+    }
+
+    while( srmio_pc_xfer_block_next( pc, &block )){
+        DeviceRideItemPtr ride( new DeviceRideItem );
+
+        ride->startTime.setTime_t( 0.1 * block.start );
+        ride->work = block.total;
+        rideList.append( ride );
+
+        if( block.athlete )
+            free( block.athlete );
+        block.athlete = NULL;
+    }
+
+    if( ! srmio_pc_xfer_finish( pc ) ){
+        err = tr("download failed: %1")
+            .arg(strerror(errno));
+        goto fail;
+    }
+
+
     return true;
+
+fail:
+    rideList.clear();
+    return false;
 }
 
 bool
@@ -181,21 +209,49 @@ SrmDevice::download( const QDir &tmpdir,
                     ProgressCallback progressCallback,
                     QString &err)
 {
+    unsigned firmware;
+    srmio_io_baudrate_t baudrateId;
+    unsigned baudrate;
+    struct _srmio_pc_xfer_block_t block;
     srmio_data_t data( NULL );
-    FILE *fh( NULL );
-    QDateTime startTime;
-
-    cb = progressCallback;
-
-    DeviceDownloadFile file;
-    file.extension = "srm";
-
-    if (!get_tmpname(tmpdir, file.name, err))
-        return false;
+    srmio_data_t *splitList( NULL );
+    srmio_data_t *split;
+    int mfirst( -1 );
+    size_t block_cnt, block_num( 0 );
+    size_t prog_sum( 0 ), prog_prev( 0 );
+    size_t chunks_done( 0 );
+    srmio_time_t splitGap( 72000 ); // 2h - XXX: make this configurable
 
     if( ! is_open ){
         statusCallback( tr("opening device %1").arg(dev->name()) );
         if( ! open( err ) )
+            return false;
+    }
+
+    if( ! srmio_pc_get_version( pc, &firmware ) ){
+        err = tr("failed to get firmware version: %1")
+            .arg(strerror(errno));
+        goto fail;
+    }
+
+    if( ! srmio_pc_get_baudrate( pc, &baudrateId ) ){
+        err = tr("failed to get baud rate: %1")
+            .arg(strerror(errno));
+        goto fail;
+    }
+
+    if( ! srmio_io_baud2name( baudrateId, &baudrate ) ){
+        err = tr("failed to get baud rate name: %1")
+            .arg(strerror(errno));
+    }
+
+    statusCallback(tr("found Powercontrol version 0x%1 using %2 baud")
+        .arg(firmware, 4, 16 )
+        .arg(baudrate));
+
+    // fetch preview in case user didn't
+    if( srmio_pc_can_preview(pc) && rideList.size() == 0 ){
+        if( ! preview( statusCallback, err ) )
             return false;
     }
 
@@ -211,9 +267,136 @@ SrmDevice::download( const QDir &tmpdir,
         goto fail;
     }
 
-    // XXX use rideList, migrate from xfer_all to chunk_next
-    if( ! srmio_pc_xfer_all( pc, data, progress, NULL ) ){
-        err = tr( "failed to download data from Powercontrol: %1")
+    if( ! srmio_pc_xfer_start( pc )){
+        err = tr("failed to start download: %1")
+            .arg(strerror(errno));
+        goto fail;
+    }
+
+    if( ! srmio_pc_xfer_get_blocks( pc, &block_cnt ) ){
+        err = tr("failed to get number of data blocks: %1")
+            .arg(strerror(errno));
+        goto fail1;
+    }
+    statusCallback(tr("found %1 ride blocks").arg(block_cnt));
+
+    for( int i = 0; i < rideList.size(); ++i ){
+        if( rideList.at(i)->wanted )
+            prog_sum += rideList.at(i)->work;
+    }
+
+    while( srmio_pc_xfer_block_next( pc, &block )){
+        bool wanted = false;
+        struct _srmio_chunk_t chunk;
+        bool is_int;
+        bool is_first;
+        size_t prog_total;
+
+        if( rideList.empty() ){
+            wanted = true;
+
+        } else {
+            for( int i = 0; i < rideList.size(); ++i ){
+                if( rideList.at(i)->startTime.toTime_t() == block.start / 10 ){
+                    wanted = rideList.at(i)->wanted;
+                    break;
+                }
+            }
+        }
+
+        if( ! wanted ){
+            statusCallback(tr("skipping unselected ride block %1")
+                .arg(block_num +1));
+            continue;
+        }
+        statusCallback(tr("downloading ride block %1/%2")
+            .arg(block_num +1)
+            .arg(block_cnt) );
+
+        data->slope = block.slope;
+        data->zeropos = block.zeropos;
+        data->circum = block.circum;
+        if( block.athlete ){
+            if( data->athlete )
+                free( data->athlete );
+            data->athlete = strdup( block.athlete );
+        }
+
+        if( ! rideList.empty() ){
+            prog_total = prog_sum;
+
+        } else if( block_cnt == 1 ){
+            prog_total = block.total;
+
+        } else {
+            prog_total = block_cnt * 1000;
+        }
+
+        while( srmio_pc_xfer_chunk_next( pc, &chunk, &is_int, &is_first  ) ){
+            if( cancelCallback() ){
+                err = tr("download cancelled");
+                goto fail1;
+            }
+
+            if( chunks_done % 16 == 0 ){
+                size_t block_done;
+
+                srmio_pc_xfer_block_progress( pc, &block_done );
+                if( ! rideList.empty() ){
+                    block_done += prog_prev;
+
+                } else if( block_cnt == 1 ){
+                    // unchanged
+
+                } else {
+                    block_done = (double)block_num * 1000
+                        + 1000 * block.total / block_done;
+                }
+
+                progressCallback( tr("progress: %1/%2")
+                    .arg(block_done)
+                    .arg(prog_total));
+            }
+
+            if( ! srmio_data_add_chunk( data, &chunk ) )
+                goto fail1;
+
+            ++chunks_done;
+
+            /* finish previous marker */
+            if( mfirst >= 0 && ( ! is_int || is_first ) )
+                srmio_data_add_marker( data, mfirst, data->cused -1 );
+
+            /* start marker */
+            if( is_first ){
+                mfirst = (int)data->cused;
+
+            } else if( ! is_int ){
+                mfirst = -1;
+
+            }
+        }
+
+        /* finalize marker at block end */
+        if( mfirst >= 0 ){
+            srmio_data_add_marker( data, mfirst, data->cused -1 );
+            mfirst = -1;
+        }
+
+        if( ! rideList.empty() )
+            prog_prev += block.total;
+        else
+            prog_prev += 1000;
+
+        if( block.athlete )
+            free( block.athlete );
+        block.athlete = NULL;
+
+        ++block_num;
+    }
+
+    if( ! srmio_pc_xfer_finish( pc ) ){
+        err = tr( "download failed: %1")
             .arg(strerror(errno));
         goto fail;
     }
@@ -230,31 +413,69 @@ SrmDevice::download( const QDir &tmpdir,
         goto fail;
     }
 
-    fh = fopen( file.name.toAscii().constData(), "w" );
-    if( ! fh ){
-        err = tr( "failed to open file %1: %2")
-            .arg(file.name)
+    splitList = srmio_data_split( data, splitGap, 1000 );
+    if( ! splitList ){
+        err = tr("Couldn't split data: %1")
             .arg(strerror(errno));
         goto fail;
     }
 
-    if( ! srmio_file_srm7_write(data, fh) ){
-        err = tr("Couldn't write to file %1: %2")
-            .arg(file.name)
-            .arg(strerror(errno));
-        goto fail;
+    for( split = splitList; *split; ++split ){
+        FILE *fh( NULL );
+        srmio_time_t stime;
+        DeviceDownloadFile file;
+
+        file.extension = "srm";
+
+        if (!get_tmpname(tmpdir, file.name, err))
+            goto fail;
+
+        if( ! srmio_data_time_start( *split, &stime ) ){
+            err = tr("Couldn't get start time of data: %1")
+                .arg(strerror(errno));
+            goto fail;
+        }
+        file.startTime.setTime_t( 0.1 * stime );
+
+        fh = fopen( file.name.toAscii().constData(), "w" );
+        if( ! fh ){
+            err = tr( "failed to open file %1: %2")
+                .arg(file.name)
+                .arg(strerror(errno));
+            goto fail;
+        }
+
+        if( ! srmio_file_srm7_write(*split, fh) ){
+            err = tr("Couldn't write to file %1: %2")
+                .arg(file.name)
+                .arg(strerror(errno));
+            fclose(fh);
+            goto fail;
+        }
+
+        files.append(file);
+
+        fclose( fh );
+
     }
 
-    file.startTime.setTime_t( 0.1 * data->chunks[0]->time );
-    files << file;
+    for( split = splitList; *split; ++split )
+        srmio_data_free( *split );
+    free(splitList);
 
     srmio_data_free( data );
-    fclose( fh );
     return true;
+
+fail1:
+    srmio_pc_xfer_finish(pc);
 
 fail:
     if( data ) srmio_data_free( data );
-    if( fh ) fclose( fh );
+    if( splitList ){
+        for( split = splitList; *split; ++split )
+            srmio_data_free( *split );
+        free(splitList);
+    }
     close();
     return false;
 }
