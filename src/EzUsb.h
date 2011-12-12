330ad7df42836e6fc72af372e6e68dae887ff372
Mark Liversedge <liversedge@gmail.com>
2011-12-12 22:24:22 +0000

    Mingw build fixups
    - #ident not supported in EzUsb.cpp
    - sleep() missing <unistd.h> in LibUsb.cpp

--- src/EzUsb.h
+++ src/EzUsb.h
@@ -19,7 +19,6 @@
  *    along with this program; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
-#ident "$Id: ezusb.h,v 1.1 2007/03/19 20:46:30 cfavi Exp $"
 
 
 /*
