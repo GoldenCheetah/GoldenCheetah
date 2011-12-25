GoldenCheetah/GoldenCheetah

commit 3c95f54f0e9a128b2530f2188b6c34f8791ae63a

Training Dials have own Color setting
For expoediency the training dials used the qwtplotmarker
color but this often lead to things like lap, time being
displayed in black on a black background.

We now have a realtime dials default color setting in the
appearances pane. The default is gray, so will show up on
a black or white background.

src/Colors.cpp
@@ -23,7 +23,7 @@
 #include <QDir>
 #include "Settings.h"
 
-static Colors ColorList[61], DefaultColorList[61];
+static Colors ColorList[62], DefaultColorList[62];
 
 static void copyArray(Colors source[], Colors target[])
 {
@@ -35,7 +35,7 @@ static bool setupColors()
 {
     // consider removing when we can guarantee extended initialisation support in gcc
     // (c++0x not supported by Qt currently and not planned for 4.8 or 5.0)
-    Colors init[61] = {
+    Colors init[62] = {
         { "Plot Background", "COLORPLOTBACKGROUND", Qt::white },
         { "Ride Plot Background", "COLORRIDEPLOTBACKGROUND", Qt::black },
         { "Plot Thumbnail Background", "COLORPLOTTHUMBNAIL", Qt::gray },
@@ -54,6 +54,7 @@ static bool setupColors()
         { "Altitude Shading", "COLORALTITUDESHADE", QColor(124,91,31) },
         { "Wind Speed", "COLORWINDSPEED", Qt::darkGreen },
         { "Torque", "COLORTORQUE", Qt::magenta },
+        { "Temperature", "COLORTEMPERATURE", Qt::yellow },
         { "Short Term Stress", "COLORSTS", Qt::blue },
         { "Long Term Stress", "COLORLTS", Qt::green },
         { "Stress Balance", "COLORSB", Qt::black },
@@ -95,7 +96,7 @@ static bool setupColors()
         { "Activity History Group", "CRIDEGROUP", QColor(236,246,255) },
         { "SpinScan Left", "CSPINSCANLEFT", Qt::gray },
         { "SpinScan Right", "CSPINSCANRIGHT", Qt::cyan },
-        { "Temperature", "COLORTEMPERATURE", Qt::yellow },
+        { "Default Dial Color", "CDIAL", Qt::gray },
         { "", "", QColor(0,0,0) },
     };
 

src/Colors.h
@@ -135,5 +135,6 @@ class ColorEngine : public QObject
 #define CSPINSCANLEFT         57
 #define CSPINSCANRIGHT        58
 #define CTEMP                 59
+#define CDIAL                 60
 
 #endif

src/DialWindow.cpp
@@ -442,7 +442,7 @@ void DialWindow::seriesChanged()
     case RealtimeData::VI:
     case RealtimeData::SkibaVI:
     case RealtimeData::None:
-            foreground = GColor(CPLOTMARKER);
+            foreground = GColor(CDIAL);
             break;
 
     case RealtimeData::Load:

commit 96a3d9f49a93e98cfe84466d486eddd542cf65c1

Fix color array offset broken in commit 3c95f54

src/Colors.cpp
@@ -54,7 +54,6 @@ static bool setupColors()
         { "Altitude Shading", "COLORALTITUDESHADE", QColor(124,91,31) },
         { "Wind Speed", "COLORWINDSPEED", Qt::darkGreen },
         { "Torque", "COLORTORQUE", Qt::magenta },
-        { "Temperature", "COLORTEMPERATURE", Qt::yellow },
         { "Short Term Stress", "COLORSTS", Qt::blue },
         { "Long Term Stress", "COLORLTS", Qt::green },
         { "Stress Balance", "COLORSB", Qt::black },
@@ -96,6 +95,7 @@ static bool setupColors()
         { "Activity History Group", "CRIDEGROUP", QColor(236,246,255) },
         { "SpinScan Left", "CSPINSCANLEFT", Qt::gray },
         { "SpinScan Right", "CSPINSCANRIGHT", Qt::cyan },
+        { "Temperature", "COLORTEMPERATURE", Qt::yellow },
         { "Default Dial Color", "CDIAL", Qt::gray },
         { "", "", QColor(0,0,0) },
     };