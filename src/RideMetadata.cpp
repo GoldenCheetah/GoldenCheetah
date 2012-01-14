43964f97e83ae6fc8d32a06f33c144d7293c51e8
Mark Liversedge <liversedge@gmail.com>
2012-01-14 12:21:39 +0000

    Disable roch text in metadata
    It causes significant performance issues when
    pasting complex font information, we only allow
    plain text since we only serialise the plain text
    elements anyway.
    
    Fixes #599.

--- src/RideMetadata.cpp
+++ src/RideMetadata.cpp
@@ -430,6 +430,10 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
 
     case FIELD_TEXTBOX : // textbox
         widget = new QTextEdit(this);
+
+        // rich text hangs 'fontd' for some users
+        dynamic_cast<QTextEdit*>(widget)->setAcceptRichText(false); 
+
         if (field.name == "Change History") {
             dynamic_cast<QTextEdit*>(widget)->setReadOnly(true);
             // pick up when ride saved - since it gets updated then
