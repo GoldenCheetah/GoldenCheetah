71a639b95b3554c543be961adc8e8d57838184a1
Jamie Kimberley <jamie.kimberley@gmail.com>
2012-01-15 18:06:14 -0500

    Add other TRIMP metrics to performance manager-v3.
    Add the ability to use the TRIMP 100 and TRIMP ZONAL metrics in the
    performace manager.

--- src/PerformanceManagerWindow.cpp
+++ src/PerformanceManagerWindow.cpp
@@ -54,6 +54,8 @@ PerformanceManagerWindow::PerformanceManagerWindow(MainWindow *mainWindow) :
     metricCombo->addItem(tr("Use BikeScore"), "skiba_bike_score");
     metricCombo->addItem(tr("Use DanielsPoints"), "daniels_points");
     metricCombo->addItem(tr("Use TRIMP"), "trimp_points");
+    metricCombo->addItem(tr("Use TRIMP 100"), "trimp_100_points");
+    metricCombo->addItem(tr("Use Trimp Zonal"), "trimp_zonal_points");
     QString metricName =
         appsettings->value(this, GC_PERF_MAN_METRIC, "skiba_bike_score").toString();
     for (int i = 0; i < metricCombo->count(); ++i) {
