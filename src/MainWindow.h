b9f2e98e471692552aca44ae1d143f880f0cb32a
Damien <Damien.Grauser@pev-geneve.ch>
2012-11-14 22:33:58 +0100

    Add myzeo data download Add ZQ and sleep time metrics

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -47,6 +47,7 @@ class RideFile;
 class ErgFile;
 class RideMetadata;
 class WithingsDownload;
+class ZeoDownload;
 class CalendarDownload;
 class DiaryWindow;
 class ICalendar;
@@ -293,6 +294,7 @@ class MainWindow : public QMainWindow
         void downloadMeasures();
         void exportMeasures();
         void importMeasures();
+        void downloadMeasuresFromZeo();
 
         // get calendars
         void refreshCalendar();
@@ -394,6 +396,7 @@ class MainWindow : public QMainWindow
         QuarqdClient *client;
         QSignalMapper *toolMapper;
         WithingsDownload *withingsDownload;
+        ZeoDownload *zeoDownload;
         bool parseRideFileName(const QString &name, QString *notesFileName, QDateTime *dt);
 
 #ifdef Q_OS_MAC
