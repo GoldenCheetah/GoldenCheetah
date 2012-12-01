a5e606c3fee0b509ef4cf9ac521f336dd134b8f9
Damien <Damien.Grauser@pev-geneve.ch>
2012-12-02 00:57:12 +0100

    Return cached value

--- src/RideFile.cpp
+++ src/RideFile.cpp
@@ -667,8 +667,10 @@ RideFile::getWeight()
     QList<SummaryMetrics> measures = mainwindow->metricDB->getAllMeasuresFor(QDateTime::fromString("Jan 1 00:00:00 1900"), startTime());
     int i = measures.count()-1;
     if (i) {
-        while (i>=0 && weight_ == 0) {
-            weight_ = measures[i].getText("Weight", "0.0").toDouble();
+        while (i>=0) {
+            if ((weight_ = measures[i].getText("Weight", "0.0").toDouble()) > 0) {
+               return weight_;
+            }
             i--;
         }
     }
