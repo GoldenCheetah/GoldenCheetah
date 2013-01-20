0479a1aa0d03073c960f99ccf652889215d7b525
Mark Liversedge <liversedge@gmail.com>
2013-01-20 19:40:40 +0000

    remove  WFAPI hack code in mainwindow
    .. accidentally committed earlier

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -122,8 +122,6 @@
 #include "LibraryParser.h"
 #include "TrainDB.h"
 
-#include "WFApi.h"
-
 QList<MainWindow *> mainwindows; // keep track of all the MainWindows we have open
 QDesktopWidget *desktop = NULL;
 
@@ -132,9 +130,6 @@ MainWindow::MainWindow(const QDir &home) :
     zones_(new Zones), hrzones_(new HrZones),
     ride(NULL), workout(NULL)
 {
-    qDebug()<<"currentstate="<<WFApi::getInstance()->currentState();
-    WFApi::getInstance()->enableBTLE(true, true);
-
     if (desktop == NULL) desktop = QApplication::desktop();
     static const QIcon hideIcon(":images/toolbar/main/hideside.png");
     static const QIcon rhideIcon(":images/toolbar/main/hiderside.png");
