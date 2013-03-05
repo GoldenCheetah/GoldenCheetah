d2e3be3bd3f2c89a3017592c032439d8b84d2bf0
Damien <Damien.Grauser@pev-geneve.ch>
2013-03-05 22:49:42 +0100

    LTMSidebar: store/use last selected date range

--- src/Settings.h
+++ src/Settings.h
@@ -74,6 +74,7 @@
 #define GC_WORKOUTDIR      "workoutDir"
 #define GC_TRAIN_SPLITTER_SIZES  "trainwindow/splitterSizes"
 #define GC_LTM_SPLITTER_SIZES  "ltmwindow/splitterSizes"
+#define GC_LTM_LAST_DATE_RANGE "ltmwindow/lastDateRange"
 #define GC_BLANK_ANALYSIS "blank/analysis"
 #define GC_BLANK_TRAIN    "blank/train"
 #define GC_BLANK_HOME     "blank/home"
