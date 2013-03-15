73d295d0d39b5725107f3d269a110c8b51f87269
Mark Liversedge <liversedge@gmail.com>
2013-03-15 19:46:21 +0000

    Windows: style selection fixup
    .. infinite loop when clicking style selector on Linux or
    Windows fixed and signals reconnected.

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -385,6 +385,8 @@ class MainWindow : public QMainWindow
 
         void toggleStyle();
         void setStyle();
+        // special case for linux/win qtsegmentcontrol toggline
+        void setStyleFromSegment(int);
 
 #ifndef Q_OS_MAC
         void toggleFullScreen();
