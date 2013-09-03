8de642c9ed8b353227be032993ee5f4a1081abbe
Eric Johnson <ericrj@gmail.com>
2013-09-02 22:58:39 -0400

    Use float division when converting wheel size
    In training mode, calculations of speed using wheel size and RPM from
    a speed sensor used integer division. This truncated all wheel
    circumferences to an integral number of meters. 2100mm wheels were
    treated as 2 meters and 1960mm wheels were treated as 1 meter,
    effectively reducing calculated speeds by roughly 5% and 50%
    respectively.
    
    Fixes #585
    Fixes #651
    Fixes #669

--- src/BT40.cpp
+++ src/BT40.cpp
@@ -179,7 +179,7 @@ void BT40::run()
 
             // set speed from wheelRpm and configured wheelsize
             double x = rt.getWheelRpm();
-            if (devConf) rt.setSpeed(x * (devConf->wheelSize/1000) * 60 / 1000);
+            if (devConf) rt.setSpeed(x * devConf->wheelSize / 1000 * 60 / 1000);
             else rt.setSpeed(x * 2.10 * 60 / 1000);
             pvars.unlock();
         }
