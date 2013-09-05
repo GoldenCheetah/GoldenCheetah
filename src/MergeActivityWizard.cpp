82c7bac2b08291a9416aed8d27888e1dea8666cc
Damien <Damien.Grauser@pev-geneve.ch>
2013-09-05 14:06:51 +0200

    MergeActivityWizard: Use index for last point

--- src/MergeActivityWizard.cpp
+++ src/MergeActivityWizard.cpp
@@ -296,7 +296,8 @@ MergeSync::getSamplesForRide(RideFile *ride1)
             currentPoint = nextPoint;
 
             //watts, cad, kph, alt, hr;
-            if (secs < maxTime-ride1->recIntSecs())
+            //if (secs < maxTime-ride1->recIntSecs())
+            if (index <= ride1->timeIndex(maxTime))
                 nextPoint = new DataPoint(ride1->getPointValue(index, RideFile::secs),  ride1->getPointValue(index, RideFile::watts),  ride1->getPointValue(index, RideFile::cad),  ride1->getPointValue(index, RideFile::kph), ride1->getPointValue(index, RideFile::alt), ride1->getPointValue(index++, RideFile::hr));
         }
         // next point
