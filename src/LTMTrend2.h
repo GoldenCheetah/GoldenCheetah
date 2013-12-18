41978d804014cd6e323516b2909f1b34f3141094
Mark Liversedge <liversedge@gmail.com>
2013-12-18 14:27:42 +0000

    Add copyright and fix bomb character in LTMTrend2

--- src/LTMTrend2.h
+++ src/LTMTrend2.h
@@ -1,6 +1,23 @@
-﻿#include <QList>
-#include <QPointF>
+/*
+ * Copyright (c) 2010 Mark Liversedge (liversedge@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
+#ifndef _GC_LTMTrend2_h
+#define _GC_LTMTrend2_h
 
 class LTMTrend2
 {
@@ -111,3 +128,5 @@ class LTMTrend2
 
 
 };
+
+#endif
