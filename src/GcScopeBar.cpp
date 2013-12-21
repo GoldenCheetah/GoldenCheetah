20739ebb2913c22f677988516e25fd1c3e3362a5
Mark Liversedge <liversedge@gmail.com>
2013-12-21 21:08:21 +0000

    Fixup Mac toolbar for Qt5 and Qt4 build
    .. should be a bit better now.

--- src/GcScopeBar.cpp
+++ src/GcScopeBar.cpp
@@ -24,7 +24,6 @@
 
 GcScopeBar::GcScopeBar(Context *context) : QWidget(context->mainWindow), context(context)
 {
-
     setFixedHeight(23);
     setContentsMargins(10,0,10,0);
     layout = new QHBoxLayout(this);
@@ -172,11 +171,8 @@ GcScopeBar::paintBackground(QPaintEvent *)
     QRect all(0,0,width(),height());
 
     // fill with a linear gradient
-#ifdef Q_OS_MAC
-    int shade = isActiveWindow() ? 178 : 225;
-#else
+#ifndef Q_OS_MAC
     int shade = isActiveWindow() ? 200 : 250;
-#endif
     QLinearGradient linearGradient(0, 0, 0, 23);
     linearGradient.setColorAt(0.0, QColor(shade,shade,shade, 100));
     linearGradient.setColorAt(0.5, QColor(shade,shade,shade, 180));
@@ -185,13 +181,13 @@ GcScopeBar::paintBackground(QPaintEvent *)
     painter.setPen(Qt::NoPen);
     painter.fillRect(all, linearGradient);
 
-    QPen black(QColor(100,100,100));
-    painter.setPen(black);
-    painter.drawLine(0,height()-1, width()-1, height()-1);
-
     QPen gray(QColor(230,230,230));
     painter.setPen(gray);
     painter.drawLine(0,0, width()-1, 0);
+#endif
+    QPen black(QColor(100,100,100));
+    painter.setPen(black);
+    painter.drawLine(0,height()-1, width()-1, height()-1);
 
     painter.restore();
 }
