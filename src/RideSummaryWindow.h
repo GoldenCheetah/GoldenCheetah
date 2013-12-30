0aee3c2e14f06e5057cd869cd6ae2121537e78ca
Mark Liversedge <liversedge@gmail.com>
2013-12-30 17:08:17 +0000

    RideSummary Compare Mode
    .. you can now compare intervals or date ranges via
       the ride summary window.

--- src/RideSummaryWindow.h
+++ src/RideSummaryWindow.h
@@ -99,12 +99,13 @@ class RideSummaryWindow : public GcChartWindow
         void setFilter(QStringList);
 #endif
 
-        // compare mode started
-        void compareChanged(bool);
+        // compare mode started or items to compare changed
+        void compareChanged();
 
     protected:
 
-        QString htmlSummary() const;
+        QString htmlSummary() const;        // summary of a ride or a date range
+        QString htmlCompareSummary() const; // comparing intervals or seasons
 
         Context *context;
         QWebView *rideSummary;
