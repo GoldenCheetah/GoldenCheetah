5bf58647e8426b51d8f75fb389bb748359d1033d
Mark Liversedge <liversedge@gmail.com>
2014-02-13 09:50:21 +0000

    Qt 5.2.1 Unified Toolbar Cosmetic
    .. add a separator line between unified toolbar and main widget

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -246,8 +246,8 @@ MainWindow::MainWindow(const QDir &home)
     head->setStyleSheet(" QToolBar:active { border: 0px; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #C6C6C6, stop: 1 #A5A5A5 ); } "
                         " QToolBar:!active { border: 0px; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #D9D9D9, stop: 1 #D6D6D6 ); } "); 
 #else
-    head->setStyleSheet(" QToolBar:!active { border: 0px; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #F0F0F0, stop: 1 #E8E8E8 ); } "
-                        " QToolBar:active { border: 0px; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #D9D9D9, stop: 1 #D6D6D6 ); } "); 
+    head->setStyleSheet(" QToolBar:!active { border: 0px; black; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #F0F0F0, stop: 1 #E8E8E8 ); } "
+                        " QToolBar:active { border: 0px; black; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #D9D9D9, stop: 1 #D6D6D6 ); } "); 
 #endif
 
     // widgets
@@ -535,6 +535,16 @@ MainWindow::MainWindow(const QDir &home)
     mainLayout->addWidget(head);
 #endif
     mainLayout->addWidget(tabbar);
+#if (defined Q_OS_MAC) && (QT_VERSION >= 0x50201)
+    QWidget *line = new QWidget(this);
+    line->setContentsMargins(0,0,0,0);
+    line->setFixedHeight(1);
+    QPalette linePalette;
+    linePalette.setBrush(backgroundRole(), Qt::darkGray);
+    line->setPalette(linePalette);
+    line->setAutoFillBackground(true);
+    mainLayout->addWidget(line);
+#endif
     mainLayout->addWidget(tabStack);
     setCentralWidget(central);
 
