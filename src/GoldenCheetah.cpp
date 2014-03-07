5895f0cf1f5e4881914681907315d5063d4f5239
Mark Liversedge <liversedge@gmail.com>
2014-03-07 20:46:19 +0000

    Reverse last commit re spacing
    .. not because it was a bad idea, it is a great idea.
    
    .. we need to replace the tabbar with a scopebar and
       customise it for the view menu and support scrolling
       left and right.
    
    .. will sort this weekend

--- src/GoldenCheetah.cpp
+++ src/GoldenCheetah.cpp
@@ -262,8 +262,9 @@ GcWindow::paintEvent(QPaintEvent * /*event*/)
         // fill in the title bar
         QRect bar(0,0,width(),contentsMargins().top());
         painter.setPen(Qt::darkGray);
-        //painter.drawRect(QRect(0,0,width()-1,height()-1));
-        painter.drawLine(0,0,width(),0);
+        painter.drawRect(QRect(0,0,width()-1,height()-1));
+        //XXX need scopebar as tabbar before we do this
+        //XXX painter.drawLine(0,0,width(),0);
 
         // heading
         QFont font;
