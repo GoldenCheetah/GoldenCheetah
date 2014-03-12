05cecd0d8f8124aa62f0ba57788c1924a78baa07
Mark Liversedge <liversedge@gmail.com>
2014-03-12 15:20:16 +0000

    Aerolab honour background colors

--- src/AerolabWindow.cpp
+++ src/AerolabWindow.cpp
@@ -299,6 +299,19 @@ void
 AerolabWindow::configChanged()
 {
   allZoomer->setRubberBandPen(GColor(CPLOTSELECT));
+  setProperty("color", GColor(CPLOTBACKGROUND));
+
+  QPalette palette;
+  palette.setBrush(QPalette::Window, QBrush(GColor(CPLOTBACKGROUND)));
+  palette.setBrush(QPalette::Background, QBrush(GColor(CPLOTBACKGROUND)));
+  palette.setBrush(QPalette::Base, QBrush(GColor(CPLOTBACKGROUND)));
+  palette.setColor(QPalette::WindowText, GCColor::invertColor(GColor(CPLOTBACKGROUND)));
+  palette.setColor(QPalette::Text, GCColor::invertColor(GColor(CPLOTBACKGROUND)));
+  palette.setColor(QPalette::Normal, QPalette::Window, GCColor::invertColor(GColor(CPLOTBACKGROUND)));
+  setPalette(palette);
+  setStyleSheet(QString("background-color: %1; color: %2; border: %1")
+                  .arg(GColor(CPLOTBACKGROUND).name())
+                  .arg(GCColor::invertColor(GColor(CPLOTBACKGROUND)).name()));
 }
 
 void
