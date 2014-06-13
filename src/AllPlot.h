884c4e8c94b4c298a87c2bfb53575c49c655ee18
Mark Liversedge <liversedge@gmail.com>
2014-06-13 12:44:12 +0100

    Isolate Curve Labels but not Vertical Markers
    .. as they represent intervals and events and are therefore
       related to the x-axis (time/distance) rather than any
       y-axis we may be showing / hiding.

--- src/AllPlot.h
+++ src/AllPlot.h
@@ -140,6 +140,9 @@ class CurveColors : public QObject
             // Labels
             foreach(QwtPlotItem *item, plot->itemList(QwtPlotItem::Rtti_PlotMarker)) {
 
+                // ignore event / interval markers
+                if (static_cast<QwtPlotMarker*>(item)->lineStyle() == QwtPlotMarker::VLine) continue;
+
                 QwtScaleWidget *x = plot->axisWidget(static_cast<QwtPlotMarker*>(item)->yAxis());
                 labels.insert(static_cast<QwtPlotMarker*>(item), x);
             }
