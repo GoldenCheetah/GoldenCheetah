c6f8bf3c2daf06b2b205b99f8538ae763af11cf8
Mark Liversedge <liversedge@gmail.com>
2014-06-28 14:49:33 +0100

    GC_VIDEO_XXX Configuration
    .. you can now select what kind of video widget you
       want to build.
    
       GC_VIDEO_NONE       - No media support compiled in
       GC_VIDEO_VLC        - Use VLC (only on Win/Linux)
       GC_VIDEO_QUICKTIME  - Use Apple QuickTime (only on OS X)
       GC_VIDEO_QT5        - QT5 video and associated backends (O/S dependent)
    
    NOTE: This needs testing on non-Linux platforms.

--- src/VideoWindow.h
+++ src/VideoWindow.h
@@ -20,8 +20,83 @@
 #define _GC_VideoWindow_h 1
 #include "GoldenCheetah.h"
 
-// QT5.2 we adopt the native video player
+// We need to determine what options the user has chosen 
+// for compiling, which differ for Mac vs Win/Linux
+//
+// Options are, GC_VIDEO_xxxx where xxxx is one of:
+// GC_VIDEO_VLC
+// GC_VIDEO_QUICKTIME
+// GC_VIDEO_NONE
+// GC_VIDEO_QT5
+//
+// If the user hasn't set one of the above then we determine
+// which one should apply !
+
+//----------------------------------------------------------------------
+// MAC VIDEO OPTION SETTING
+//----------------------------------------------------------------------
+#ifdef Q_OS_MAC
+
+// if we aint chosen one or the other then use quicktime
+#if !defined GC_VIDEO_QUICKTIME && !defined GC_VIDEO_NONE && !defined GC_VIDEO_QT5
+#define GC_VIDEO_QUICKTIME
+#endif
+
+// but qt5 and vlc are not valid options !
+#if defined GC_VIDEO_VLC
+#error "VLC is not a support on Mac OS X builds of GoldenCheetah"
+#endif
+
+// but qt5 *is* supported, but use at your own risk!
+#if defined GC_VIDEO_QT5
+#if QT_VERSION >= 0x050201
+#warning "QT 5 video is supported experimentally in this version"
+#else
+#error "QT5 video is only supported with QT 5.2.1 or higher"
+#endif
+#endif // GC_VIDEO_QT5
+
+#endif //Q_OS_MAC
+
+//----------------------------------------------------------------------
+// LINUX AND WINDOWS
+//----------------------------------------------------------------------
+
+#if defined Q_OS_LINUX || defined Q_OS_WIN
+
+// did the user specify location for VLC but not GC_VIDEO_XXXX
+#if defined GC_HAVE_VLC && !defined GC_VIDEO_NONE && !defined GC_VIDEO_QT5
+#if !defined GC_VIDEO_VLC
+#define GC_VIDEO_VLC
+#endif
+#endif
+
+// if we aint chosen one or the other then use VLC QT < 5.2.1 or QT5 >= 5.2.1
+#if !defined GC_VIDEO_NONE && !defined GC_VIDEO_QT5 && !defined GC_VIDEO_VLC
 #if QT_VERSION < 0x50201
+#define GC_VIDEO_VLC
+#else
+#define GC_VIDEO_QT5
+#endif
+#endif // !defined NONE,QT5,VLC
+
+// now check for stupid settings
+#if defined GC_VIDEO_QUICKTIME
+#error "QuickTime is only supported on Mac OS X"
+#endif
+
+// but qt5 *is* supported, but use at your own risk!
+#if defined GC_VIDEO_QT5 && QT_VERSION < 0x050201
+#error "QT5 video is only supported with QT 5.2.1 or higher"
+#endif // GC_VIDEO_QT5
+
+#endif // Q_OS_LINUX || Q_OS_WIN
+
+//----------------------------------------------------------------------
+// Now options are set lets pull in the headers we need then
+//----------------------------------------------------------------------
+
+#ifdef GC_VIDEO_VLC // VLC Video used
 
 // for vlc
 #include <stdio.h>
@@ -33,12 +108,12 @@ extern "C" {
 #include <vlc/libvlc_media.h>
 }
 
-#else
+#endif // VLC
 
+#ifdef GC_VIDEO_QT5 // QT5 Video Stuff
 #include <QVideoWidget>
 #include <QMediaPlayer>
 #include <QMediaContent>
-
 #endif
 
 // QT stuff etc
@@ -50,10 +125,12 @@ extern "C" {
 #include "RealtimeData.h"
 #include "TrainSidebar.h"
 
+// Linux container changed in QT5
 #if (defined Q_OS_LINUX) && (QT_VERSION < 0x050000)
 #include <QX11EmbedContainer>
 #endif
 
+// regardless we always have a media helper
 class MediaHelper
 {
     public:
@@ -68,7 +145,7 @@ class MediaHelper
 
     private:
         QStringList supported;
-#if QT_VERSION < 0x50201
+#ifdef GC_VIDEO_VLC 
         libvlc_instance_t * inst;
 #endif
 };
@@ -103,15 +180,16 @@ class VideoWindow : public GcWindow
 
         bool m_MediaChanged;
 
-#if QT_VERSION < 0x50201
+#ifdef GC_VIDEO_VLC
 
         // vlc for older QT
         libvlc_instance_t * inst;
         //libvlc_exception_t exceptions;
         libvlc_media_player_t *mp;
         libvlc_media_t *m;
-#else
+#endif
 
+#ifdef GC_VIDEO_QT5
         // QT native
         QMediaContent mc;
         QVideoWidget *wd;
@@ -129,6 +207,8 @@ class VideoWindow : public GcWindow
 #ifdef WIN32
         QWidget *container;
 #endif
+
+        bool init; // we initialised ok ?
 };
 
 #endif // _GC_VideoWindow_h
