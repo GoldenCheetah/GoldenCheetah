30ac368f035543afd5ff3d720eec56ba6266c3d8
Mark Liversedge <liversedge@gmail.com>
2014-06-30 16:38:03 +0100

    GProgressDialog
    .. in an attempt to avoid the QT 5.3.1 QProgressSialog
       bug and also put a bit of our own styling onto this
       a GC implementation of QProgressDialog
    
    .. it has no abort button DELIBERATELY
    
    .. not tested on OS X .. yet

---
+++ src/GProgressDialog.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2014 Mark Liversedge (liversedge@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "GProgressDialog.h"
+
+GProgressDialog::GProgressDialog(QString title, int min, int max, QWidget *parent) : 
+
+    // sheet on mac and no window manager chrome
+    QDialog(parent, Qt::Sheet | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint),
+
+    // defaults
+    title(title), min(min), max(max)
+{
+    // only block mainwindow
+    setWindowModality(Qt::WindowModal); // only block mainwindow
+
+    // zap me when closed and make me see through
+    setAttribute(Qt::WA_DeleteOnClose, true);
+    setAttribute(Qt::WA_TranslucentBackground, true);
+
+    // not too big dude
+    setFixedSize(200, 130);
+}
+
+void GProgressDialog::paintEvent(QPaintEvent *)
+{
+    // setup the colors
+    QColor translucentGray = GColor(CPLOTMARKER);
+    translucentGray.setAlpha(240);
+    QColor translucentWhite = GColor(CPLOTBACKGROUND);
+    translucentWhite.setAlpha(240);
+    QColor black = GCColor::invertColor(translucentWhite);
+
+    // setup a painter and the area to paint
+    QPainter painter(this);
+
+    painter.save();
+    QRect all(0,0,width(),height());
+
+    // fill
+    painter.setPen(Qt::NoPen);
+    painter.fillRect(all, translucentWhite);
+
+    // border
+    QRect border(0,0,width()-1,height()-1);
+    black.setAlpha(50);
+    painter.setPen(QPen(black, 0.5f));
+    painter.drawRect(border);
+
+    // heading
+    QRectF titlebox(0,0,width(),25);
+    painter.fillRect(titlebox, black);
+    black.setAlpha(240);
+    painter.setPen(QPen(black));
+    painter.drawText(titlebox, title, Qt::AlignVCenter | Qt::AlignCenter);
+
+    // informative text
+    QRectF labelbox(0,25,width(),height()-33);
+    painter.drawText(labelbox, text, Qt::AlignVCenter | Qt::AlignCenter);
+
+    // progressbar
+    QRectF progress(0, height()-8, double(value) / double(max) * double(width()), 8);
+    painter.fillRect(progress, translucentGray);
+    painter.restore();
+}
+
+void GProgressDialog::setValue(int x)
+{
+    value = x;
+    if (max < x) max = x;
+    repaint();
+}
+
+// set the progress text
+void GProgressDialog::setLabelText(QString label)
+{
+    text = label;
+    repaint();
+}
+
---
+++ src/GProgressDialog.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2014 Mark Liversedge (liversedge@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _GC_GProgressDialog_h
+#define _GC_GProgressDialog_h 1
+
+#include "GoldenCheetah.h"
+#include "Colors.h"
+
+#include <QWidget>
+#include <QPaintEvent>
+#include <QString>
+#include <QDialog>
+
+class GProgressDialog : public QDialog
+{
+    Q_OBJECT
+
+    public:
+
+        // no frame, translucent, no button, no parent - always modal with chrome heading
+        GProgressDialog(QString title, int min, int max, QWidget *parent = NULL);
+
+        // set value, which in turn repaints the progress at the bottom
+        void setValue(int x);
+
+        // set the progress text
+        void setLabelText(QString text);
+
+        // compatibility
+        bool wasCanceled() { return false; }
+
+        // painting 
+        void paintEvent(QPaintEvent *);
+
+    private:
+        QString title, text;
+        int min, max, value;
+};
+#endif
