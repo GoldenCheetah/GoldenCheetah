7250dac2321cf31d37dd8e500938a383377ef340
Mark Liversedge <liversedge@gmail.com>
2014-07-27 21:38:10 +0100

    Version 3.1 RELEASE CANDIDATE 3
    .. updated version number

--- src/GcUpgrade.h
+++ src/GcUpgrade.h
@@ -39,18 +39,19 @@
 // 3051 - V3.1 DEVELOPMENT
 // 3055 - V3.1 RC1
 // 3056 - V3.1 RC2
+// 3057 - V3.1 RC3
 
 #define VERSION3_BUILD 3010 // released
 #define VERSION3_SP1   3030 // released
 #define VERSION3_SP2   3032 // released
-#define VERSION31_UPG  3056 // first build with 3.1 upgrade process
+#define VERSION31_UPG  3057 // first build with 3.1 upgrade process
 
 // will keep changing during testing and before final release
 #define VERSION31_BUILD VERSION31_UPG
 
 // these three will change until we release
-#define VERSION_LATEST 3056
-#define VERSION_STRING "V3.1 RC2"
+#define VERSION_LATEST 3057
+#define VERSION_STRING "V3.1 RC3"
 
 class GcUpgrade
 {
