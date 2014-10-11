1f00790069851927e47bbd2a928ffd8974959cfd
Mark Liversedge <liversedge@gmail.com>
2014-10-11 11:26:47 +0100

    Add Labels on Matches in Ride Plot
    .. makes it easier to scan and see where it got to without
       having to mouse over the points one by one

--- src/AllPlot.h
+++ src/AllPlot.h
@@ -32,6 +32,7 @@
 #include <qwt_scale_widget.h>
 #include <qwt_compat.h>
 #include <QtGui>
+#include <QFont>
 
 #include <QTableWidget>
 #include <QStackedWidget>
@@ -362,6 +363,7 @@ class AllPlotObject : public QObject
     QVector<double> match;
     QVector<double> matchTime;
     QVector<double> matchDist;
+    QVector<QwtPlotMarker*> matchLabels;
     QVector<double> wprime;
     QVector<double> wprimeTime;
     QVector<double> wprimeDist;
@@ -456,6 +458,7 @@ class AllPlot : public QwtPlot
         void setDataFromPlots(QList<AllPlot*>); // user for single series comparing
         void setDataFromObject(AllPlotObject *object, AllPlot *reference); // for allplot when one per ride in a stack
                                                                            // reference is for settings et al
+        void setMatchLabels(AllPlotObject *object); // set labels from object
 
         // convert from time/distance to index in *smoothed* datapoints
         int timeIndex(double) const;
@@ -583,6 +586,7 @@ class AllPlot : public QwtPlot
         bool isolation;
         LTMToolTip *tooltip;
         LTMCanvasPicker *_canvasPicker; // allow point selection/hover
+        QFont labelFont;
 
         static void nextStep( int& step );
 };
