80f97951b005229c3097b32522cc5bbe978007b0
Mark Liversedge <liversedge@gmail.com>
2014-11-03 20:34:00 +0000

    Fix Moxy header copyright and ifdef

--- src/MoxyDevice.cpp
+++ src/MoxyDevice.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Damien Grauser (Damien.Grauser@pev-geneve.ch)
+ * Copyright (c) 2014 Mark Liversedge (liversedge@gmail.com)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
