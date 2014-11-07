3a220af8f5d22e258f2c2c6d2829860d4439d329
Alejandro Martinez <amtriathlon@gmail.com>
2014-11-06 22:09:35 -0300

    Initial GOVSS implementation Includes LTP, RTP, IWF and GOVSS metrics with PMC integration

--- src/LTMTool.cpp
+++ src/LTMTool.cpp
@@ -1108,6 +1108,82 @@ QList<MetricDetail> LTMTool::providePMmetrics() {
     trimpLTR.uunits = tr("Ramp");
     metrics.append(trimpLTR);
 
+    // GOVSS LTS
+    MetricDetail govssLTS;
+    govssLTS.type = METRIC_PM;
+    govssLTS.symbol = "govss_lts";
+    govssLTS.metric = NULL; // not a factory metric
+    govssLTS.penColor = QColor(Qt::blue);
+    govssLTS.curveStyle = QwtPlotCurve::Lines;
+    govssLTS.symbolStyle = QwtSymbol::NoSymbol;
+    govssLTS.smooth = false;
+    govssLTS.trendtype = 0;
+    govssLTS.topN = 1;
+    govssLTS.uname = govssLTS.name = tr("GOVSS Long Term Stress");
+    govssLTS.units = "Stress";
+    govssLTS.uunits = tr("Stress");
+    metrics.append(govssLTS);
+
+    MetricDetail govssSTS;
+    govssSTS.type = METRIC_PM;
+    govssSTS.symbol = "govss_sts";
+    govssSTS.metric = NULL; // not a factory metric
+    govssSTS.penColor = QColor(Qt::magenta);
+    govssSTS.curveStyle = QwtPlotCurve::Lines;
+    govssSTS.symbolStyle = QwtSymbol::NoSymbol;
+    govssSTS.smooth = false;
+    govssSTS.trendtype = 0;
+    govssSTS.topN = 1;
+    govssSTS.uname = govssSTS.name = tr("GOVSS Short Term Stress");
+    govssSTS.units = "Stress";
+    govssSTS.uunits = tr("Stress");
+    metrics.append(govssSTS);
+
+    MetricDetail govssSB;
+    govssSB.type = METRIC_PM;
+    govssSB.symbol = "govss_sb";
+    govssSB.metric = NULL; // not a factory metric
+    govssSB.penColor = QColor(Qt::yellow);
+    govssSB.curveStyle = QwtPlotCurve::Steps;
+    govssSB.symbolStyle = QwtSymbol::NoSymbol;
+    govssSB.smooth = false;
+    govssSB.trendtype = 0;
+    govssSB.topN = 1;
+    govssSB.uname = govssSB.name = tr("GOVSS Stress Balance");
+    govssSB.units = "Stress Balance";
+    govssSB.uunits = tr("Stress Balance");
+    metrics.append(govssSB);
+
+    MetricDetail govssSTR;
+    govssSTR.type = METRIC_PM;
+    govssSTR.symbol = "govss_sr";
+    govssSTR.metric = NULL; // not a factory metric
+    govssSTR.penColor = QColor(Qt::darkGreen);
+    govssSTR.curveStyle = QwtPlotCurve::Steps;
+    govssSTR.symbolStyle = QwtSymbol::NoSymbol;
+    govssSTR.smooth = false;
+    govssSTR.trendtype = 0;
+    govssSTR.topN = 1;
+    govssSTR.uname = govssSTR.name = tr("GOVSS STS Ramp");
+    govssSTR.units = "Ramp";
+    govssSTR.uunits = tr("Ramp");
+    metrics.append(govssSTR);
+
+    MetricDetail govssLTR;
+    govssLTR.type = METRIC_PM;
+    govssLTR.symbol = "govss_lr";
+    govssLTR.metric = NULL; // not a factory metric
+    govssLTR.penColor = QColor(Qt::darkBlue);
+    govssLTR.curveStyle = QwtPlotCurve::Steps;
+    govssLTR.symbolStyle = QwtSymbol::NoSymbol;
+    govssLTR.smooth = false;
+    govssLTR.trendtype = 0;
+    govssLTR.topN = 1;
+    govssLTR.uname = govssLTR.name = tr("GOVSS LTS Ramp");
+    govssLTR.units = "Ramp";
+    govssLTR.uunits = tr("Ramp");
+    metrics.append(govssLTR);
+
     // done
 
     return metrics;
