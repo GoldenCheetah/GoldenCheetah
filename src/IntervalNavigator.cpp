9b982731cd1165fa9dc37073351477803e6fb71a
Damien <Damien.Grauser@pev-geneve.ch>
2014-11-12 23:39:17 +0100

    IntervalNavigator: Add the interval

--- src/IntervalNavigator.cpp
+++ src/IntervalNavigator.cpp
@@ -890,6 +890,7 @@ IntervalNavigator::selectRide(const QModelIndex &index)
     }
 
     f->clearIntervals();
+    f->addInterval(start, end, "1");
 
     RideItem* rideItem = new RideItem(RIDE_TYPE, f, date, context->rideItem()->zones, context->rideItem()->hrZones, context );
 
