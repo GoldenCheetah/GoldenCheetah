f82b22da5a13424f34988a6d295a18233d66ae2c
Chet Henry <henry@visionlink.org>
2014-11-18 13:49:53 -0700

    Clean up white space

--- contrib/RideLogger/src/com/ridelogger/listners/Gps.java
+++ contrib/RideLogger/src/com/ridelogger/listners/Gps.java
@@ -13,33 +13,33 @@ public class Gps extends Base
 {
     public Gps(Context mContext) 
     {
-    	super(mContext);
-    	
-    	LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        super(mContext);
+        
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
-    	
-    	LocationListener locationListener = new LocationListener() {
-    		    public void onLocationChanged(Location location) {
-			        Map<String, String> map = new HashMap<String, String>();
-					map.put("ALTITUDE", Double.toString(location.getAltitude()) );
-					map.put("KPH",      Float.toString( location.getSpeed())    );
-					map.put("bearing",  Float.toString( location.getBearing())  );
-					map.put("gpsa",     Float.toString( location.getAccuracy()) );
-					map.put("LAT",      Double.toString(location.getLatitude()) );
-					map.put("LON",      Double.toString(location.getLongitude()));
-					writeData(map);
-			    }
-			
-			    @Override
-			    public void onStatusChanged(String provider, int status, Bundle extras) {}
-			
-			    @Override
-			    public void onProviderEnabled(String provider) {}
-			
-			    @Override
-			    public void onProviderDisabled(String provider) {}
-		  };
-    	  
-    	  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        
+        LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("ALTITUDE", Double.toString(location.getAltitude()) );
+                    map.put("KPH",      Float.toString( location.getSpeed())    );
+                    map.put("bearing",  Float.toString( location.getBearing())  );
+                    map.put("gpsa",     Float.toString( location.getAccuracy()) );
+                    map.put("LAT",      Double.toString(location.getLatitude()) );
+                    map.put("LON",      Double.toString(location.getLongitude()));
+                    writeData(map);
+                }
+            
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+            
+                @Override
+                public void onProviderEnabled(String provider) {}
+            
+                @Override
+                public void onProviderDisabled(String provider) {}
+          };
+          
+          locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 }
