bb947a4d3dfae34cf65c540cd47d43e40047c60d
Mark Liversedge <liversedge@gmail.com>
2015-01-08 15:39:55 +0000

    Fix CP Estimates
    .. we filtered out the CP2 and CP3 estimates by checking
       for non-zero values for PMax and FTP that neither of
       these models provide estimates for !

--- src/RideCache.cpp
+++ src/RideCache.cpp
@@ -671,8 +671,8 @@ RideCache::refreshCPModelMetrics()
 
             if (add.CP && add.WPrime) add.EI = add.WPrime / add.CP ;
 
-            // so long as the model derived values are sensible ...
-            if (add.WPrime > 1000 && add.CP > 100 && add.PMax > 100 && add.FTP > 100)
+            // so long as the important model derived values are sensible ...
+            if (add.WPrime > 1000 && add.CP > 100) 
                 context->athlete->PDEstimates << add;
 
             //qDebug()<<add.to<<add.from<<model->code()<< "W'="<< model->WPrime() <<"CP="<< model->CP() <<"pMax="<<model->PMax();
