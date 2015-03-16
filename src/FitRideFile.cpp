2e21cced4090c1ad293295f7876862b05354e11f
Stephan Lips <stephan.lips@gmail.com>
2015-03-16 17:19:42 +0000

    SRM Powercontrol 8 FIT support
    .. the PC8 uses FIT files (yay!) and this patch ensures we set the
       device type properly.

--- src/FitRideFile.cpp
+++ src/FitRideFile.cpp
@@ -276,6 +276,17 @@ struct FitFileReaderState
                 default: rideFile->setDeviceType(QString("Garmin %1").arg(prod));
             }
 
+        } else if (manu == 6 ) {
+
+            // SRM powercontrol now uses FIT files from PC8
+            switch (prod) {
+
+            case 6: rideFile->setDeviceType("SRM PC6");break;
+            case 7: rideFile->setDeviceType("SRM PC7");break;
+            case 8: rideFile->setDeviceType("SRM PC8");break;
+            default: rideFile->setDeviceType("SRM Powercontrol");break;
+            }
+
         } else  if (manu == 38) {
             switch (prod) {
                 case 1: rideFile->setDeviceType("o_synce navi2coach"); break;
