50d4e9bde10e7473069e60a1e569e7f29e56b845
Mark Liversedge <liversedge@gmail.com>
2015-05-16 10:08:30 +0100

    Search all interval names
    .. not just user defined ones

--- src/FreeSearch.cpp
+++ src/FreeSearch.cpp
@@ -100,12 +100,8 @@ QList<QString> FreeSearch::search(QString query)
             }
         }
 
-        // user intervals
+        // user intervals - even autodiscovered
         foreach(IntervalItem *interval, item->intervals()) {
-
-            // just user ones and named segments / routes
-            if (interval->type != RideFileInterval::USER &&
-                interval->type != RideFileInterval::ROUTE) continue;
 
             foreach (QString token, tokens) {
 
