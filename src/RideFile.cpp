6911c1e485ee0174b857ee78b1df207a8ef781b2
grauser <Damien.Grauser@gmail.com>
2015-05-18 19:28:50 +0200

    Auto Interval Discovery : SPRINT detection

--- src/RideFile.cpp
+++ src/RideFile.cpp
@@ -390,6 +390,7 @@ QString RideFileInterval::typeDescription(intervaltype x)
     case DEVICE : return tr("DEVICE"); break;
     case USER : return tr("USER"); break;
     case PEAKPOWER : return tr("PEAK POWER"); break;
+    case SPRINT : return tr("SPRINT"); break;
     case ROUTE : return tr("SEGMENTS"); break;
     case PEAKHR : return tr("PEAK HR"); break;
     case CLIMB : return tr("CLIMBING"); break;
