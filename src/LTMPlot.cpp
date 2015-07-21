9e852faf5cc70c650472c85f95d27394b1ec10e2
Mark Liversedge <liversedge@gmail.com>
2015-07-21 19:13:37 +0100

    LTM high and low curves not deleted
    .. if you select e.g. the TriScore PMC in the Library chart
       in Trends, the high/low curves get left behind because they
       use the same name (Best_xxx).
    
    .. ironically this was already worked around by creating a name
       that included the symbol, but this was not used when adding to
       the curve mapping.

--- src/LTMPlot.cpp
+++ src/LTMPlot.cpp
@@ -900,7 +900,7 @@ LTMPlot::setData(LTMSettings *set)
                                      metricDetail.bestSymbol : metricDetail.symbol);
             QwtPlotCurve *top = new QwtPlotCurve(topName);
             top->setVisible(!metricDetail.hidden);
-            curves.insert(topName, top);
+            curves.insert(topSymbol, top);
 
             top->setRenderHint(QwtPlotItem::RenderAntialiased);
             top->setStyle(QwtPlotCurve::Dots);
@@ -1989,7 +1989,7 @@ LTMPlot::setCompareData(LTMSettings *set)
                                     .arg((metricDetail.type == METRIC_BEST || metricDetail.type == METRIC_STRESS) ? 
                                         metricDetail.bestSymbol : metricDetail.symbol);
                 QwtPlotCurve *top = new QwtPlotCurve(topName);
-                curves.insert(topName, top);
+                curves.insert(topSymbol, top);
 
                 top->setRenderHint(QwtPlotItem::RenderAntialiased);
                 top->setStyle(QwtPlotCurve::Dots);
