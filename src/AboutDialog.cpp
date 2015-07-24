8c0aea993ce1e74850bbd35efe6b538620bfbdba
Vianney Boyer <vlcvboyer@gmail.com>
2015-07-24 15:14:07 +0200

    Update contributors

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -183,6 +183,7 @@ ContributorsPage::ContributorsPage(Context *context) : context(context)
     contributors.append("Steven Gribble");
     contributors.append("Thomas Weichmann");
     contributors.append("Tilman Schmiedeberg");
+    contributors.append("Vianney Boyer");
     contributors.append("Walter B&#252;rki");
     contributors.append("Yves Arrouye");
 
