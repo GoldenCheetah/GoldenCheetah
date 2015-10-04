22abb5301abed083e7741449317eb738ac412b84
Joern <joern.rm@gmail.com>
2015-10-04 18:50:55 +0200

    Add "Shared Network Folder" to as new "File Store" implementation ... use an shared network drive (e.g. mounted via WebDAV) to synchronize activity data    (similar to Dropbox,...)
    Open/TBD:
    ... where to best put the Preferences for the different FileStores
       (currently add the Network folder configuration on Preferences->Passwords,
        even though no PW is needed - with growing number of FileStores a
        dedicated Preferences Page may be the solution)
    ... where to put the MainWindow Menu entries if the number of FileStores increases

---
+++ src/NetworkFileStore.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Joern Rischmueller (joern.rm@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GC_NetworkFileStore_h
+#define GC_NetworkFileStore_h
+
+#include "FileStore.h"
+
+class NetworkFileStore : public FileStore {
+
+    Q_OBJECT
+
+    public:
+
+        NetworkFileStore(Context *context);
+        ~NetworkFileStore();
+
+        QString name() { return (tr("Shared Network Folder")); }
+
+        // open/connect and close/disconnect
+        bool open(QStringList &errors);
+        bool close();
+
+        // home directory
+        QString home();
+
+        // write a file 
+        bool writeFile(QByteArray &data, QString remotename);
+
+        // read a file
+        bool readFile(QByteArray *data, QString remotename); 
+
+        // create a folder
+        bool createFolder(QString path);
+
+        // dirent style api
+        FileStoreEntry *root() { return root_; }
+        QList<FileStoreEntry*> readdir(QString path, QStringList &errors);
+
+    private:
+        Context *context;
+        FileStoreEntry *root_;
+
+};
+#endif
