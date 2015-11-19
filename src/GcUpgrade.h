5bbf5bd5af69ec90a64bc748e9b39e104731fc6c
Mark Liversedge <liversedge@gmail.com>
2015-11-19 22:26:44 +0000

    V3.3 Release Candidate 2
    .. update build and version number

--- src/GcUpgrade.h
+++ src/GcUpgrade.h
@@ -59,6 +59,7 @@
 // 3900 - V3.3 DEVELOPMENT STARTS
 // 3901 - V3.3 PUBLIC DEVELOPMENT BUILD
 // 3902 - V3.3 RC1
+// 3903 - V3.3 RC2
 
 #define VERSION3_BUILD    3010 // released
 #define VERSION3_SP1      3030 // released
@@ -72,8 +73,8 @@
 #define VERSION31_BUILD VERSION31_UPG
 
 // these three will change until we release
-#define VERSION_LATEST 3902
-#define VERSION_STRING "V3.3 RC1"
+#define VERSION_LATEST 3903
+#define VERSION_STRING "V3.3 RC2"
 
 // default config for this release is available and maintained here:
 // not updated for v3.3 *yet* -- need to sort this during the release cycle
