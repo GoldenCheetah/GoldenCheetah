cf96ab4cb6b74b85fa98fa9c179547e833d1f3c5
Vianney Boyer <vlcvboyer@gmail.com>
2015-12-03 21:31:06 +0100

    place back the value validity check

--- src/FitRideFile.cpp
+++ src/FitRideFile.cpp
@@ -672,12 +672,11 @@ struct FitFileReaderState
         }
 
         foreach(const FitField &field, def.fields) {
-            // FIXME : validity check has been removed, to be confirmed:
-            // fit_value_t value = values[i++].v;
-            //
-            // if( value == NA_VALUE )
-            //    continue;
             const FitValue& value = values[i++];
+
+            if( value.v == NA_VALUE )
+                continue;
+
             if (FIT_DEBUG) {
                 printf ("\tfield: num: %d ", field.num);
                 DumpFitValue(value);
