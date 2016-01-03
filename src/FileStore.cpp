95d7f51077abfc6fc0fd96e9dee77643a1103bb8
Magnus Gille <mgille@gmail.com>
2016-01-02 20:42:20 -0500

    Make OAuth scope configurable for GoogleDrive.

--- src/FileStore.cpp
+++ src/FileStore.cpp
@@ -426,9 +426,7 @@ FileStoreDialog::createFolderClicked()
 {
     FolderNameDialog dialog(this);
     int ret = dialog.exec();
-    printf("CREATE\n");
     if (ret == QDialog::Accepted && dialog.name() != "") {
-        printf("CREATE22\n");
         // go and create it !
         store->createFolder(pathname + "/" + dialog.name());
 
