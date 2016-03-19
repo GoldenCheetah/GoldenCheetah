0895713d0dc366a10e073523353def295b524a2e
gcoco <gcoco@users.noreply.github.com>
2016-03-18 21:42:50 -0400

    Install Qt 5.5.1 from brew
    Because QT 5.6 breaks stuff
    [ci skip]

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -6,8 +6,8 @@ then
   echo ${workdir}
   cd $( brew --prefix )
   pwd
-  # Select QT 5.4.2
-  git checkout 00e46351980 Library/Formula/qt5.rb
+  # Select QT 5.5.1
+  git checkout fb64f6cd91ff Library/Formula/qt5.rb
   cd ${workdir}
   pwd
 fi
