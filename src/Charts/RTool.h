2735803d55c072f85a7d4468ccf992fd7bd8408b
Mark Liversedge <liversedge@gmail.com>
2016-04-25 20:11:03 +0100

    R SHLIB GC.display()
    .. phew. that was hard.
    
    .. To register routines with R you need to place them in a shared
       library.
    
    .. The routines we want to register are part of  the GC codebase so
       cannot be linked into that shared library (it would be the whole
       of GC).
    
    .. So; we have a shared library (RGoldenCheetah.cpp) which has stubs
       for all the registered functions and an array of pointers to the
       actual functions.
    
    .. We load the library (once R is embedded it is loaded in main.cpp)
    
    .. After the library is loaded we then call one of its public
       functions (GCInitialiseFunctions) to tell it where all the GC
       functions are (we only have GCdisplay at present for this proof
       of the concept).
    
    .. Along the way we need to deref/cast DL_FUNC in RTool.cpp which
       is not permitted in ISO C, so we also update qmake to add a
       special rule to compile `dodgy' sources with -fpermissive. And
       the only dodgy source is RTool.cpp.
    
    .. This commmit will break GC_WANT_R builds on OSX, and will be
       fixed up shortly.
    
    .. The motivation behind this is to avoid RInside/Rcpp for Windows
       builds -- enabling R support (which is not currently possible).

--- src/Charts/RTool.h
+++ src/Charts/RTool.h
@@ -46,6 +46,10 @@ class RTool {
         static Rcpp::DataFrame metrics();
 
         bool starting;
+
+        // to link GC.xxx() functions to the routine
+        // stubs in the RGoldenCheetah DLL/SO/DYLIB
+        void registerRoutines();
 };
 
 // there is a global instance created in main
