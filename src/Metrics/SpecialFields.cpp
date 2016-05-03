3fe01d418bd1f4f2ad9e6bfb1c3870a15b0373fa
Mark Liversedge <liversedge@gmail.com>
2016-05-03 19:16:31 +0100

    R Metrics include Metadata
    .. so you can use Workout_Code and friends to classify
       or color etc

--- src/Metrics/SpecialFields.cpp
+++ src/Metrics/SpecialFields.cpp
@@ -27,40 +27,11 @@ SpecialFields::SpecialFields()
     namesmap.insert("Start Date", tr("Start Date"));                 // linked to RideFile::starttime
     namesmap.insert("Start Time", tr("Start Time"));                 // linked to RideFile::starttime
     namesmap.insert("Identifier", tr("Identifier"));                 // linked to RideFile::id
-    namesmap.insert("Workout Code", tr("Workout Code"));             // in WKO and possibly others
-    namesmap.insert("Sport", tr("Sport"));                           // in WKO and possible others
-    namesmap.insert("Objective", tr("Objective"));                   // in WKO as "goal" nad possibly others
-    namesmap.insert("Summary", tr("Summary"));                       // embeds the RideSummary widget
-    namesmap.insert("Notes", tr("Notes"));                           // linked to Context::rideNotes
-    namesmap.insert("Keywords", tr("Keywords"));                     // extracted from Notes / used for highlighting calendar
     namesmap.insert("Recording Interval", tr("Recording Interval")); // linked to RideFile::recIntSecs
-    namesmap.insert("Weight", tr("Weight"));                         // in WKO and possibly others
-    namesmap.insert("Device", tr("Device"));                         // RideFile::devicetype
-    namesmap.insert("Device Info", tr("Device Info"));               // in WKO and TCX and possibly others
-    namesmap.insert("Dropouts", tr("Dropouts"));                     // calculated from source data by FixGaps
-    namesmap.insert("Dropout Time", tr("Dropout Time"));             // calculated from source data vy FixGaps
-    namesmap.insert("Spikes", tr("Spikes"));                         // calculated from source data by FixSpikes
-    namesmap.insert("Spike Time", tr("Spike Time"));                 // calculated from source data by FixSpikes
-    namesmap.insert("Torque Adjust", tr("Torque Adjust"));           // the last torque adjust applied
-    namesmap.insert("Filename", tr("Filename"));                     // set by the rideFile reader
-    namesmap.insert("Year", tr("Year"));                             // set by the rideFile reader
     namesmap.insert("Change History", tr("Change History"));         // set by RideFileCommand
     namesmap.insert("Calendar Text", "Calendar Text");               // set by openRideFile and rideMetadata DO NOT TRANSLATE
-    namesmap.insert("Data", tr("Data"));                             // set by openRideFile for areDataPresent
-    namesmap.insert("Lean Mass", tr("Lean Mass"));                   // measure
-    namesmap.insert("Fat Mass", tr("Fat Mass"));                     // measure
-    namesmap.insert("Fat Ratio", tr("Fat Ratio"));                   // measure
-    namesmap.insert("Height", tr("Height"));                         // measure
-    namesmap.insert("BMI", tr("BMI"));                               // measure
-    namesmap.insert("File Format", tr("File Format"));               // set by openRideFile
-    namesmap.insert("Athlete", tr("Athlete"));                       // set by openRideFile
-    namesmap.insert("Year", tr("Year"));                             // set by openRideFile
-    namesmap.insert("Month", tr("Month"));                            // set by openRideFile
-    namesmap.insert("Weekday", tr("Weekday"));                       // set by openRideFile
     namesmap.insert("Source Filename", tr("Source Filename"));       // set by openRideFile
     namesmap.insert("Route", tr("Route"));                           // GPS map Route tag
-    namesmap.insert("RPE", tr("RPE"));                               // used by Session RPE metric
-
 
     // now add all the metric fields (for metric overrides)
     const RideMetricFactory &factory = RideMetricFactory::instance();
