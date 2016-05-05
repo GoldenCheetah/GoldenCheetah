d4048f0fc1f4ffcd2d28dac9997ad4197e7577d5
Mark Liversedge <liversedge@gmail.com>
2016-05-05 19:15:54 +0100

    R Fix text scaling via world transform
    .. instead of hacking the text to avoid a global world
       transform we now transform y ourselves
    
    .. it looks like text needs to be centered will test and
       fix shortly.

--- src/Charts/RCanvas.cpp
+++ src/Charts/RCanvas.cpp
@@ -42,7 +42,7 @@ RCanvas::RCanvas(Context *context, QWidget *parent) : QGraphicsView(parent), con
     // flip horizontal as y-co-ordinates place the
     // origin in the bottom left, not screen co-ords
     // of top left. x axis is fine though.
-    scale(1,-1);
+    //scale(1,-1);
 
     // turn on antialiasing too
     setRenderHint(QPainter::Antialiasing, true);
@@ -90,6 +90,8 @@ RCanvas::newPage()
 void
 RCanvas::circle(double x, double y, double r, QPen p, QBrush b)
 {
+    y =-y;
+
     QGraphicsEllipseItem *c=new QGraphicsEllipseItem(x-r,y-r,r+r,r+r);
     c->setPen(p);
     c->setBrush(b);
@@ -99,6 +101,10 @@ RCanvas::circle(double x, double y, double r, QPen p, QBrush b)
 void
 RCanvas::line(double x1, double y1, double x2, double y2, QPen p)
 {
+    y1 =-y1;
+    y2 =-y2;
+
+
     QGraphicsLineItem *l=new QGraphicsLineItem(x1,y1,x2,y2);
     l->setPen(p);
     scene->addItem(l);
@@ -107,13 +113,14 @@ RCanvas::line(double x1, double y1, double x2, double y2, QPen p)
 void
 RCanvas::polyline(int n, double *x, double *y, QPen p)
 {
+
     // origin
     double x0=x[0], y0=y[0];
 
     // create a line per path
     for(int i=1; i<n;i++) {
 
-        double x1=x[i], y1=y[i];
+        double x1=x[i], y1=y[i]; // no transform since done by line !
         line(x0,y0,x1,y1,p);
 
         x0=x1;
@@ -127,7 +134,7 @@ RCanvas::polygon(int n, double *x, double *y, QPen p, QBrush b)
     QVector<QPointF> points(n);
 
     // create a line per path
-    for(int i=0; i<n;i++)  points[i] = QPointF(x[i],y[i]);
+    for(int i=0; i<n;i++)  points[i] = QPointF(x[i],-y[i]);
 
     QGraphicsPolygonItem *g= new QGraphicsPolygonItem(QPolygonF(points),0);
     g->setPen(p);
@@ -138,7 +145,7 @@ RCanvas::polygon(int n, double *x, double *y, QPen p, QBrush b)
 void
 RCanvas::rectangle(double x0, double y0, double x1, double y1,QPen p, QBrush b)
 {
-    QGraphicsRectItem *r=new QGraphicsRectItem(x0,y0,x1-x0,y1-y0);
+    QGraphicsRectItem *r=new QGraphicsRectItem(x0,-y0,x1-x0,-(y1-y0));
     r->setPen(p);
     r->setBrush(b);
     scene->addItem(r);
@@ -147,19 +154,15 @@ RCanvas::rectangle(double x0, double y0, double x1, double y1,QPen p, QBrush b)
 void
 RCanvas::text(double x, double y, QString s, double rot, double , QPen p, QFont f)
 {
+    // add height too
+    QFontMetrics fm(f);
+
     QGraphicsTextItem *t = new QGraphicsTextItem(s);
-    t->setFlag(QGraphicsItem::ItemIgnoresTransformations);
     t->setFont(f);
-    t->setPos(0,0);
+    t->setPos(x,-y-fm.height());
     t->setRotation(rot);
     t->setDefaultTextColor(p.color());
 
-    // add to group
-    QList<QGraphicsItem*> texts;
-    texts<<t;
-    QGraphicsItemGroup *grp = scene->createItemGroup(texts);
+    scene->addItem(t);
 
-    // add height too
-    QFontMetrics fm(f);
-    grp->setPos(x,y+fm.height());
 }
