1367d165e9c90e1f434505a326820af3ff3c0875
Mark Liversedge <liversedge@gmail.com>
2016-05-11 07:54:31 +0100

    R Load Library at Runtime (wip)
    .. add Graphics device entry points and almost done with defining
       proxy functions to call at runtime
    
    .. will need to create a QLibrary object to load and resolve
       symbols for the proxies to call next.
    
    .. if you add GC_WANT_R_DYNAMIC to gcconfig.pri it enables
       this, but it will crash, this is a wip.

--- src/R/RLibrary.cpp
+++ src/R/RLibrary.cpp
@@ -22,6 +22,8 @@
 #include <R_ext/RStartup.h>
 #include <R_ext/Parse.h>
 #include <R_ext/Rdynload.h>
+#include <R_ext/GraphicsEngine.h>
+#include <R_ext/GraphicsDevice.h>
 
 // R Embedding methods
 void GC_R_dot_Last(void){ }
@@ -37,7 +39,31 @@ void NORET GC_Rf_error(const char *, ...){ }
 void NORET GC_Rf_warning(const char *, ...){ }
 void GC_Rf_PrintValue(SEXP) { }
 DllInfo *GC_R_getEmbeddingDllInfo(void) { }
-
+int GC_R_registerRoutines(DllInfo *info, const R_CMethodDef * const croutines,
+                          const R_CallMethodDef * const callRoutines,
+                          const R_FortranMethodDef * const fortranRoutines,
+                          const R_ExternalMethodDef * const externalRoutines) { }
+#ifndef WIN32
+void (*GC_ptr_R_Suicide)(const char *);
+void (*GC_ptr_R_ShowMessage)(const char *);
+int  (*GC_ptr_R_ReadConsole)(const char *, unsigned char *, int, int);
+void (*GC_ptr_R_WriteConsole)(const char *, int);
+void (*GC_ptr_R_WriteConsoleEx)(const char *, int, int);
+void (*GC_ptr_R_ResetConsole)(void);
+void (*GC_ptr_R_FlushConsole)(void);
+void (*GC_ptr_R_ClearerrConsole)(void);
+void (*GC_ptr_R_Busy)(int);
+void (*GC_ptr_R_CleanUp)(SA_TYPE, int, int);
+int  (*GC_ptr_R_ChooseFile)(int, char *, int);
+int  (*GC_ptr_R_EditFile)(const char *);
+void (*GC_ptr_R_loadhistory)(SEXP, SEXP, SEXP, SEXP);
+void (*GC_ptr_R_savehistory)(SEXP, SEXP, SEXP, SEXP);
+void (*GC_ptr_R_addhistory)(SEXP, SEXP, SEXP, SEXP);
+int  (*GC_ptr_R_EditFiles)(int, const char **, const char **, const char *);
+void (*GC_ptr_R_ProcessEvents)();
+#endif
+FILE * GC_R_Outputfile;
+FILE * GC_R_Consolefile;
 
 // R data
 SEXP GC_Rf_allocVector(SEXPTYPE, R_xlen_t){ }
@@ -63,6 +89,15 @@ SEXP GC_Rf_install(const char *){ }
 SEXP GC_Rf_setAttrib(SEXP, SEXP, SEXP) { }
 Rboolean (GC_Rf_isNull)(SEXP s) { }
 char *(GC_R_CHAR)(SEXP x) { }
+
+// Graphics Device
+pGEDevDesc GC_GEcreateDevDesc(pDevDesc dev) { }
+void GC_GEaddDevice2(pGEDevDesc, const char *) { }
+void GC_Rf_onintr(void) { }
+int GC_R_GE_getVersion(void) { }
+void GC_R_CheckDeviceAvailable(void) { }
+Rboolean GC_R_interrupts_suspended;
+int GC_R_interrupts_pending;
 
 // globals
 SEXP GC_R_GlobalEnv, GC_R_NilValue,
