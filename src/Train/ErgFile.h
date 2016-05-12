49c1f6917f531f99cb7089fd0444be6d5c2de7fb
Keith Reynolds <mtbkeith@gmail.com>
2016-05-12 00:30:20 -0400

    Adding to previous commit

--- src/Train/ErgFile.h
+++ src/Train/ErgFile.h
@@ -33,46 +33,40 @@
 #include <QRegExp>
 #include "Zones.h"      // For zones ... see below vvvv
 
-// which section of the file are we in?
-#define NOMANSLAND  0
-#define SETTINGS 1
-#define DATA     2
-#define END      3
-
-// is this in .erg or .mrc format?
-#define ERG     1
-#define MRC     2
-#define CRS     3
-
 class ErgFilePoint
 {
+    friend class ErgFile;
     public:
 
         ErgFilePoint() : x(0), y(0), val(0) {}
         ErgFilePoint(double x, double y, double val) : x(x), y(y), val(val) {}
 
+    protected:
         double x;     // x axis - time in msecs or distance in meters
         double y;     // y axis - load in watts or altitude
-
         double val;   // the value to send to the device (watts/gradient)
 };
 
 class ErgFileSection
 {
+    friend class ErgFile;
     public:
         ErgFileSection() : duration(0), start(0), end(0) {}
         ErgFileSection(int duration, int start, int end) : duration(duration), start(start), end(end) {}
 
+    protected:
         int duration;
         double start, end;
 };
 
 class ErgFileText
 {
+    friend class ErgFile;
     public:
         ErgFileText() : x(0), pos(0), text("") {}
         ErgFileText(double x, int pos, QString text) : x(x), pos(pos), text(text) {}
 
+    protected:
         double x;
         int pos;
         QString text;
@@ -80,7 +74,8 @@ class ErgFileText
 
 class ErgFileLap
 {
-    public:
+    friend class ErgFile;
+    protected:
         long x;     // when does this LAP marker occur? (time in msecs or distance in meters
         int LapNum;     // from 1 - n
         bool selected; // used by the editor
@@ -90,10 +85,32 @@ class ErgFileLap
 class ErgFile
 {
     public:
-        ErgFile(QString, int, Context *context);       // constructor uses filename
+
+        enum ErgSection {
+            NomanslandSection,
+            SettingsSection,
+            DataSection,
+            EndSection
+        };
+        
+        enum ErgMode {
+            ERG,
+            MRC,
+            CRS,
+            
+            NOMODESET = 99
+        };
+        
+        enum class ErgFileFormat : unsigned int {
+            ErgFormat,
+            CrsFormat,
+            MrcFormat
+        };
+    
+        ErgFile(QString filename, enum ErgMode mode, Context *context); // constructor uses filename
         ErgFile(Context *context); // no filename, going to use a string
 
-        ~ErgFile();             // delete the contents
+        virtual ~ErgFile();             // delete the contents
 
         void setFrom(ErgFile *f); // clone an existing workout
         bool save(QStringList &errors); // save back, with changes
@@ -108,8 +125,6 @@ class ErgFile
         void parseZwift();         // its a zwo file (zwift xml)
 
         bool isValid();         // is the file valid or not?
-        double Cp;
-        int format;             // ERG, CRS or MRC currently supported
         int wattsAt(long, int&);      // return the watts value for the passed msec
         double gradientAt(long, int&);      // return the gradient value for the passed meter
         int nextLap(long);      // return the msecs value for the next Lap marker
@@ -118,30 +133,35 @@ class ErgFile
         // than a list of points
         QList<ErgFileSection> Sections();
 
-        QString Version,        // version number / identifer
-                Units,          // units used
-                Filename,       // filename from inside file
-                filename,       // filename on disk
-                Name,           // description in file
-                Description,    // long narrative for workout
-                ErgDBId,        // if downloaded from ergdb
-                Source;         // where did this come from
-        QStringList Tags;       // tagged strings
-                
+        void calculateMetrics(); // calculate NP value for ErgFile
+        long GetDuration() const;
+        enum ErgFileFormat GetFormat();
+        Context* GetContext();
+        
+    private:
+        double Cp;
+        enum ErgFileFormat format;             // ERG, CRS or MRC currently supported
+        enum ErgMode mode;
         long    Duration;       // Duration of this workout in msecs
         int     Ftp;            // FTP this file was targetted at
         int     MaxWatts;       // maxWatts in this ergfile (scaling)
         bool valid;             // did it parse ok?
-        int mode;
-
-
+        
+        
         int leftPoint, rightPoint;            // current points we are between
-
+        
         QList<ErgFilePoint> Points;    // points in workout
         QList<ErgFileLap>   Laps;      // interval markers in the file
         QList<ErgFileText>  Texts;     // texts to display
-
-        void calculateMetrics(); // calculate NP value for ErgFile
+        QString Version,        // version number / identifer
+        Units,          // units used
+        Filename,       // filename from inside file
+        filename,       // filename on disk
+        Name,           // description in file
+        Description,    // long narrative for workout
+        ErgDBId,        // if downloaded from ergdb
+        Source;         // where did this come from
+        QStringList Tags;       // tagged strings
 
         // Metrics for this workout
         double maxY;                // maximum Y value
@@ -149,11 +169,11 @@ class ErgFile
         double AP, NP, IF, TSS, VI; // Coggan for erg / mrc
         double XP, RI, BS, SVI; // Skiba for erg / mrc
         double ELE, ELEDIST, GRADE;    // crs
-
+    
+    protected:
         Context *context;
-
-    private:
-        int nomode;
+    
+    
 };
 
 #endif
