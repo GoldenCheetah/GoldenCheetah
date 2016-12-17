56c9eb3c0f3585e88ed6330f1f64b256b21461ed
Mark Liversedge <liversedge@gmail.com>
2016-12-17 13:40:35 +0000

    Training Planning Development Starts
    .. added coaching notes
    
    .. added new planning widget to trends view (for now)
    
    .. no functionality, just kicking off the next 2 weeks of coding.

---
+++ src/Planning/PlanningWindow.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2016 Mark Liversedge (liversedge@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#include "PlanningWindow.h"
+
+PlanningWindow::PlanningWindow(Context *context) :
+    GcChartWindow(context), context(context)
+{
+    setContentsMargins(0,0,0,0);
+    setProperty("color", GColor(CTRENDPLOTBACKGROUND));
+
+    setControls(NULL);
+
+    QVBoxLayout *main = new QVBoxLayout;
+    setChartLayout(main);
+
+    connect(context, SIGNAL(configChanged(qint32)), this, SLOT(configChanged(qint32)));
+
+    // set the widgets etc
+    configChanged(CONFIG_APPEARANCE);
+}
+
+void
+PlanningWindow::resizeEvent(QResizeEvent *)
+{
+    // TBC
+}
+
+void
+PlanningWindow::configChanged(qint32)
+{
+    setProperty("color", GColor(CTRENDPLOTBACKGROUND));
+
+    // text edit colors
+    QPalette palette;
+    palette.setColor(QPalette::Window, GColor(CTRAINPLOTBACKGROUND));
+    palette.setColor(QPalette::Background, GColor(CTRAINPLOTBACKGROUND));
+
+    // only change base if moved away from white plots
+    // which is a Mac thing
+#ifndef Q_OS_MAC
+    if (GColor(CTRENDPLOTBACKGROUND) != Qt::white)
+#endif
+    {
+        //palette.setColor(QPalette::Base, GCColor::alternateColor(GColor(CTRAINPLOTBACKGROUND)));
+        palette.setColor(QPalette::Base, GColor(CTRAINPLOTBACKGROUND));
+        palette.setColor(QPalette::Window, GColor(CTRAINPLOTBACKGROUND));
+    }
+
+#ifndef Q_OS_MAC // the scrollers appear when needed on Mac, we'll keep that
+    //code->setStyleSheet(TabView::ourStyleSheet());
+#endif
+
+    palette.setColor(QPalette::WindowText, GCColor::invertColor(GColor(CTRAINPLOTBACKGROUND)));
+    palette.setColor(QPalette::Text, GCColor::invertColor(GColor(CTRAINPLOTBACKGROUND)));
+    //code->setPalette(palette);
+    repaint();
+}
+
+bool
+PlanningWindow::eventFilter(QObject *obj, QEvent *event)
+{
+    bool returning = false;
+
+    // we only filter out keyboard shortcuts for undo redo etc
+    // in the qwkcode editor, anything else is of no interest.
+    if (event->type() == QEvent::KeyPress) {
+
+        // we care about cmd / ctrl
+        Qt::KeyboardModifiers kmod = static_cast<QInputEvent*>(event)->modifiers();
+        bool ctrl = (kmod & Qt::ControlModifier) != 0;
+
+        switch(static_cast<QKeyEvent*>(event)->key()) {
+
+        case Qt::Key_Y:
+            if (ctrl) {
+                //workout->redo();
+                returning = true; // we grab all key events
+            }
+            break;
+
+        case Qt::Key_Z:
+            if (ctrl) {
+                //workout->undo();
+                returning=true;
+            }
+            break;
+
+        }
+
+    }
+    return returning;
+}
