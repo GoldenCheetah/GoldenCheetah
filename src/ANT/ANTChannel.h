8ee6084dcb075ac5279349416e5fb5f50833c879
Jon Escombe <jone@dresco.co.uk>
2016-12-26 09:39:47 +0000

    Monotonic timer for ANT sensors
    Use a monotonic timer for ANT sensor calculations, instead
    of the wall clock time.

--- src/ANT/ANTChannel.h
+++ src/ANT/ANTChannel.h
@@ -102,10 +102,10 @@ class ANTChannel : public QObject {
         char id[10]; // short identifier
         ANTChannelInitialisation mi;
 
-        int   messages_received; // for signal strength metric
-        int   messages_dropped;
-        QTime lastMessageTimestamp;
-        QTime lastMessageTimestamp2;
+        int    messages_received; // for signal strength metric
+        int    messages_dropped;
+        qint64 lastMessageTimestamp; // for time comparisons
+        qint64 lastMessageTimestamp2;
 
         unsigned char rx_burst_data[RX_BURST_DATA_LEN];
         int           rx_burst_data_index;
