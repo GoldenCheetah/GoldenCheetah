988683518d798f9e22dd6bb6cd0d69f36082e165
Joern <joern.rm@gmail.com>
2017-01-04 19:48:58 +0100

    CloudDB - Telemetry ... add user permission dialog ... add telemetry update call

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -151,6 +151,8 @@
 #define GC_SWIMPACE                     "<global-general>swimpace"
 #define GC_ELEVATION_HYSTERESIS         "<global-general>elevationHysteresis"
 #define GC_UNIT                         "<global-general>unit"
+#define GC_ALLOW_TELEMETRY              "<global-general>telemetry"
+#define GC_ALLOW_TELEMETRY_DATE         "<global-general>telemetryDate"
 
 
 
