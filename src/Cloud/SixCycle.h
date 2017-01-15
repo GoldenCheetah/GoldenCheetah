228a76ccaf5bd332e60ef83733ec9aef30a7c11c
Mark Liversedge <liversedge@gmail.com>
2017-01-15 20:47:59 +0000

    Oops
    .. SixCycle store wasn't renamed after cut/paste
       from code for Today's Plan.

--- src/Cloud/SixCycle.h
+++ src/Cloud/SixCycle.h
@@ -32,7 +32,7 @@ class SixCycle : public FileStore {
         SixCycle(Context *context);
         ~SixCycle();
 
-        QString name() { return (tr("Today's Plan")); }
+        QString name() { return (tr("SixCycle")); }
 
         // open/connect and close/disconnect
         bool open(QStringList &errors);
