5912b94227d5b3ae5c9ddf70736553760182de9c
Mark Liversedge <liversedge@gmail.com>
2017-02-21 13:31:44 +0000

    Don't disable RideSummaryWindow
    .. users need to be able to scroll up and down the
       contents to see intervals etc.
    
    .. but it means it will continue to steal focus.

--- src/Charts/RideSummaryWindow.cpp
+++ src/Charts/RideSummaryWindow.cpp
@@ -92,7 +92,7 @@ RideSummaryWindow::RideSummaryWindow(Context *context, bool ridesummary) :
 
  #ifdef NOWEBKIT
     rideSummary = new QWebEngineView(this);
-    rideSummary->setEnabled(false);
+    //XXX steals focus rideSummary->setEnabled(false);
  #else
     rideSummary = new QWebView(this);
  #endif
