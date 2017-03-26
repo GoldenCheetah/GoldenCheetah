fdb12b4f047ec946f8ce0594bbf93f0031395fe1
Mark Liversedge <liversedge@gmail.com>
2017-03-26 14:15:19 +0100

    Cloud Refactor - VeloHero
    .. convert ShareDialog => CloudService

--- src/Cloud/Velohero.h
+++ src/Cloud/Velohero.h
@@ -23,6 +23,11 @@
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 
+// parsing xml responses
+#include <QXmlInputSource>
+#include <QXmlSimpleReader>
+#include <QXmlDefaultHandler>
+
 class Velohero : public CloudService {
 
     Q_OBJECT
@@ -54,11 +59,84 @@ class Velohero : public CloudService {
         Context *context;
         QNetworkAccessManager *nam;
         QNetworkReply *reply;
+        QString sessionId;
         CloudServiceEntry *root_;
 
         QMap<QNetworkReply*, QByteArray*> buffers;
 
     private slots:
         void onSslErrors(QNetworkReply *reply, const QList<QSslError>&error);
+};
+
+// parse upload response
+class VeloHeroParser : public QXmlDefaultHandler
+{
+    friend class Velohero;
+
+public:
+
+    bool startElement(const QString&, const QString&, const QString&, const QXmlAttributes&) {
+        cdata = "";
+        return true;
+    };
+
+    bool endElement(const QString&, const QString&, const QString& qName) {
+        if(qName == "error"){
+            error = cdata;
+            return true;
+        }
+        return true;
+    }
+
+    bool characters(const QString& str)
+    {
+        cdata += str;
+        return true;
+    };
+
+protected:
+
+    QString cdata;
+    QString error;
+};
+
+// parsing the response from a session id request
+class VeloHeroSessionParser : public VeloHeroParser
+{
+    friend class Velohero;
+
+public:
+
+    bool endElement(const QString& a, const QString&b, const QString& qName)
+    {
+        if(qName == "session"){
+            session = cdata;
+            return true;
+        }
+        return VeloHeroParser::endElement(a, b, qName);
+    };
+
+protected:
+    QString session;
+};
+
+class VeloHeroPostParser : public VeloHeroParser
+{
+public:
+    friend class Velohero;
+
+    bool endElement( const QString& a, const QString&b, const QString& qName )
+    {
+        if( qName == "id" ){
+            id = cdata;
+            return true;
+
+        }
+
+        return VeloHeroParser::endElement(a, b, qName);
+    };
+
+protected:
+    QString id;
 };
 #endif
