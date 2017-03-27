84d353d5dbf749686e663b37040554e8891e188e
Mark Liversedge <liversedge@gmail.com>
2017-03-27 11:51:16 +0100

    Cloud Refactor - Strava
    .. ShareDialog => CloudService

--- src/Cloud/Strava.cpp
+++ src/Cloud/Strava.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017 Mark Liversedge (liversedge@gmail.com)
+ * Copyright (c) 2013 Damien.Grauser (damien.grauser@pev-geneve.ch)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -19,6 +20,7 @@
 #include "Strava.h"
 #include "Athlete.h"
 #include "Settings.h"
+#include "mvjson.h"
 #include <QByteArray>
 #include <QHttpMultiPart>
 #include <QJsonDocument>
@@ -55,7 +57,8 @@ Strava::Strava(Context *context) : CloudService(context), context(context), root
         connect(nam, SIGNAL(sslErrors(QNetworkReply*, const QList<QSslError> & )), this, SLOT(onSslErrors(QNetworkReply*, const QList<QSslError> & )));
     }
 
-    uploadCompression = none; // gzip
+    uploadCompression = gzip; // gzip
+    filetype = CloudService::uploadType::TCX;
     useMetric = true; // distance and duration metadata
 }
 
@@ -73,6 +76,11 @@ bool
 Strava::open(QStringList &errors)
 {
     printd("Strava::open\n");
+    QString token = appsettings->cvalue(context->athlete->cyclist, GC_STRAVA_TOKEN, "").toString();
+    if (token == "") {
+        errors << tr("No authorisation token configured.");
+        return false;
+    }
     return true;
 }
 
@@ -91,8 +99,84 @@ Strava::writeFile(QByteArray &data, QString remotename, RideFile *ride)
 
     printd("Strava::writeFile(%s)\n", remotename.toStdString().c_str());
 
+    QString token = appsettings->cvalue(context->athlete->cyclist, GC_STRAVA_TOKEN, "").toString();
+
+    // access the original file for ride start
+    QDateTime rideDateTime = ride->startTime();
+
+    // trap network response from access manager
+
+    QUrl url = QUrl( "https://www.strava.com/api/v3/uploads" ); // The V3 API doc said "https://api.strava.com" but it is not working yet
+    QNetworkRequest request = QNetworkRequest(url);
+
+    //QString boundary = QString::number(qrand() * (90000000000) / (RAND_MAX + 1) + 10000000000, 16);
+    QString boundary = QVariant(qrand()).toString() +
+        QVariant(qrand()).toString() + QVariant(qrand()).toString();
+
+    // MULTIPART *****************
+
+    QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+    multiPart->setBoundary(boundary.toLatin1());
+
+    QHttpPart accessTokenPart;
+    accessTokenPart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                              QVariant("form-data; name=\"access_token\""));
+    accessTokenPart.setBody(token.toLatin1());
+
+    QHttpPart activityTypePart;
+    activityTypePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                               QVariant("form-data; name=\"activity_type\""));
+    if (ride->isRun())
+      activityTypePart.setBody("run");
+    else if (ride->isSwim())
+      activityTypePart.setBody("swim");
+    else
+      activityTypePart.setBody("ride");
+
+    QHttpPart activityNamePart;
+    activityNamePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"activity_name\""));
+    activityNamePart.setBody(remotename.toLatin1());
+
+    QHttpPart dataTypePart;
+    dataTypePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"data_type\""));
+    dataTypePart.setBody("tcx.gz");
+
+    QHttpPart externalIdPart;
+    externalIdPart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"external_id\""));
+    externalIdPart.setBody("Ride");
+
+    //XXXQHttpPart privatePart;
+    //XXXprivatePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+    //XXX                      QVariant("form-data; name=\"private\""));
+    //XXXprivatePart.setBody(parent->privateChk->isChecked() ? "1" : "0");
+
+    //XXXQHttpPart commutePart;
+    //XXXcommutePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+    //XXX                      QVariant("form-data; name=\"commute\""));
+    //XXXcommutePart.setBody(parent->commuteChk->isChecked() ? "1" : "0");
+    //XXXQHttpPart trainerPart;
+    //XXXtrainerPart.setHeader(QNetworkRequest::ContentDispositionHeader,
+    //XXX                      QVariant("form-data; name=\"trainer\""));
+    //XXXtrainerPart.setBody(parent->trainerChk->isChecked() ? "1" : "0");
+
+    QHttpPart filePart;
+    filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("text/xml"));
+    filePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"file\"; filename=\"file.tcx.gz\"; type=\"text/xml\""));
+    filePart.setBody(data);
+
+    multiPart->append(accessTokenPart);
+    multiPart->append(activityTypePart);
+    multiPart->append(activityNamePart);
+    multiPart->append(dataTypePart);
+    multiPart->append(externalIdPart);
+    //XXXmultiPart->append(privatePart);
+    //XXXmultiPart->append(commutePart);
+    //XXXmultiPart->append(trainerPart);
+    multiPart->append(filePart);
+
     // this must be performed asyncronously and call made
     // to notifyWriteCompleted(QString remotename, QString message) when done
+    reply = nam->post(request, multiPart);
 
     // catch finished signal
     connect(reply, SIGNAL(finished()), this, SLOT(writeFileCompleted()));
@@ -111,14 +195,57 @@ Strava::writeFileCompleted()
 
     printd("reply:%s\n", reply->readAll().toStdString().c_str());
 
-    if (reply->error() == QNetworkReply::NoError) {
-        notifyWriteComplete(
-            replyName(static_cast<QNetworkReply*>(QObject::sender())),
-            tr("Completed."));
+    bool uploadSuccessful = false;
+    QString response = reply->readLine();
+    QString uploadError="invalid response or parser error";
+
+    try {
+
+        // parse !
+        MVJSONReader jsonResponse(string(response.toLatin1()));
+
+        // get error field
+        if (jsonResponse.root) {
+            if (jsonResponse.root->hasField("error")) {
+                uploadError = jsonResponse.root->getFieldString("error").c_str();
+            } else {
+                uploadError = ""; // no error
+            }
+
+            // get upload_id, but if not available use id
+            //XXX if (jsonResponse.root->hasField("upload_id")) {
+            //XXX     stravaUploadId = jsonResponse.root->getFieldInt("upload_id");
+            //XXX } else if (jsonResponse.root->hasField("id")) {
+            //XXX     stravaUploadId = jsonResponse.root->getFieldInt("id");
+            //XXX } else {
+            //XXX     stravaUploadId = 0;
+            //XXX }
+        } else {
+            uploadError = "no connection";
+        }
+
+    } catch(...) { // not really sure what exceptions to expect so do them all (bad, sorry)
+        uploadError=tr("invalid response or parser exception.");
+    }
+
+    if (uploadError.toLower() == "none" || uploadError.toLower() == "null") uploadError = "";
+
+    // if successful update ID
+    if (uploadError.length()>0 || reply->error() != QNetworkReply::NoError)  uploadSuccessful = false;
+    else {
+
+        //XXXride->ride()->setTag("Strava uploadId", QString("%1").arg(stravaUploadId));
+        //XXXride->setDirty(true);
+
+        //qDebug() << "uploadId: " << stravaUploadId;
+        uploadSuccessful = true;
+    }
+
+    // return response
+    if (uploadSuccessful && reply->error() == QNetworkReply::NoError) {
+        notifyWriteComplete(replyName(static_cast<QNetworkReply*>(QObject::sender())), tr("Completed."));
     } else {
-        notifyWriteComplete(
-            replyName(static_cast<QNetworkReply*>(QObject::sender())),
-            tr("Network Error - Upload failed."));
+        notifyWriteComplete(replyName(static_cast<QNetworkReply*>(QObject::sender())), uploadError);
     }
 }
 
