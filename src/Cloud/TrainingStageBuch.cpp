f3083b0826c065e8b43aa9d5c4b0fece20dfaaa4
Mark Liversedge <liversedge@gmail.com>
2017-03-27 11:25:11 +0100

    Cloud Refactor - Preserve Copyright
    .. Damien was missed off by accident.

--- src/Cloud/TrainingStageBuch.cpp
+++ src/Cloud/TrainingStageBuch.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017 Mark Liversedge (liversedge@gmail.com)
+ * Copyright (c) 2013 Damien.Grauser (damien.grauser@pev-geneve.ch)
  * Copyright (c) 2012 Rainer Clasen <bj@zuto.de>
  *
  * This program is free software; you can redistribute it and/or modify it
