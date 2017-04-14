GoldenCheetah/GoldenCheetah

commit c187bda78525f1ce8bcef4dac0ee4e0cf1af2b61

Prevent divide by zero Prevent use of NULL pointer Mark params as unused Delete objects to prevent memory leak Move new of object to prevent memory leak

src/Train/WorkoutWidget.cpp
@@ -1437,6 +1437,7 @@ WorkoutWidget::ergFileSelected(ErgFile *ergFile)
             // as we goo these just increase to the last point
             if (add->x > maxWX_) maxVX_ = maxWX_ = add->x;
             if (add->y > maxY_) maxY_ = add->y;
+            delete add;
         }
 
         maxY_ *= 1.1f;

src/Charts/PfPvPlot.cpp
@@ -746,7 +746,7 @@ PfPvPlot::intervalHover(IntervalItem *x)
         }
 
         // any data ?
-        if (aepfArray.size()) {
+        if (count > 0 && aepfArray.size()) {
             QwtSymbol *sym = new QwtSymbol;
             sym->setStyle(QwtSymbol::Ellipse);
             sym->setSize(4*dpiXFactor);

src/Charts/PowerHist.cpp
@@ -1151,10 +1151,12 @@ PowerHist::binData(HistData &standard, QVector<double>&x, // x-axis for data
             x[i] = high*delta;
             y[i]  = 1e-9;  // nonzero to accommodate log plot
             sy[i] = 1e-9;  // nonzero to accommodate log plot
-            while (low < high && low<arrayLength) {
-                if (selectedArray && (*selectedArray).size()>low)
-                    sy[i] += dt * (*selectedArray)[low];
-                y[i] += dt * (*array)[low++];
+            if (array) {
+                while (low < high && low<arrayLength) {
+                    if (selectedArray && (*selectedArray).size()>low)
+                        sy[i] += dt * (*selectedArray)[low];
+                    y[i] += dt * (*array)[low++];
+                }
             }
         }
         y[i] = 1e-9;       // nonzero to accommodate log plot
@@ -2402,7 +2404,7 @@ bool PowerHist::isSelected(const RideFilePoint *p, double sample)
 
 bool PowerHist::isSelected(const double t, double sample)
 {
-    if (!rideItem) {
+    if (rideItem) {
 
         foreach (IntervalItem *interval, rideItem->intervalsSelected()) {
             if (interval->selected && t+sample>interval->start && t<interval->stop) 

src/Charts/ScatterPlot.cpp
@@ -672,7 +672,7 @@ ScatterPlot::intervalHover(IntervalItem *ri)
         }
 
         // any data ?
-        if (xArray.size()) {
+        if (count > 0 && xArray.size()) {
             QwtSymbol *sym = new QwtSymbol;
             sym->setStyle(QwtSymbol::Ellipse);
             sym->setSize(4*dpiXFactor);

src/Cloud/SixCycle.cpp
@@ -193,6 +193,9 @@ SixCycle::createFolder(QString)
 QList<CloudServiceEntry*>
 SixCycle::readdir(QString path, QStringList &errors, QDateTime from, QDateTime to)
 {
+    Q_UNUSED(from)
+    Q_UNUSED(to)
+    
     printd("Sixcycle::readdir(%s)\n", path.toStdString().c_str());
 
     QList<CloudServiceEntry*> returning;

src/FileIO/PolarRideFile.cpp
@@ -543,6 +543,7 @@ RideFile *PolarFileReader::openRideFile(QFile &file, QStringList &errors, QList<
               {
                   // Pdd file exist but it does not contain
                   // hrmFile_orig (probably been deleted)
+                  delete rideFile;
                   return NULL;
               }
           else if (hrmFile_orig == hrvFile && hrmFile != hrvFile)
@@ -584,6 +585,7 @@ RideFile *PolarFileReader::openRideFile(QFile &file, QStringList &errors, QList<
                     // If both exist only read when equal to ".hrm"
                     errors << ("This is a R-R file: \""
                                + file.fileName() + "\"");
+                    delete rideFile;
                     return NULL;
                 }
                 else {
@@ -605,6 +607,7 @@ RideFile *PolarFileReader::openRideFile(QFile &file, QStringList &errors, QList<
             {
                 errors << ("Can not find : \""
                            + file.fileName() + "\"");
+                delete rideFile;
                 return NULL;
             }
         haveGPX = gpxfile.exists();

src/Metrics/PeakPower.cpp
@@ -69,11 +69,12 @@ class PeakPercent : public RideMetric {
             double CP = 250;
             double WPRIME = 22000;
 
-            if (item->context->athlete->zones(item->isRun)) {
+            const Zones* zones = item->context->athlete->zones(item->isRun);
+            if (zones) {
 
                 // if range is -1 we need to fall back to a default value
-                CP = item->zoneRange >= 0 ? item->context->athlete->zones(item->isRun)->getCP(item->zoneRange) : 250;
-                WPRIME = item->zoneRange >= 0 ? item->context->athlete->zones(item->isRun)->getWprime(item->zoneRange) : 22000;
+                CP = item->zoneRange >= 0 ? zones->getCP(item->zoneRange) : 250;
+                WPRIME = item->zoneRange >= 0 ? zones->getWprime(item->zoneRange) : 22000;
 
                 // did we override CP in metadata ?
                 int oCP = item->getText("CP","0").toInt();
@@ -128,7 +129,8 @@ class PowerZone : public RideMetric {
     void compute(RideItem *item, Specification, const QHash<QString,RideMetric*> &deps) {
 
         // no zones
-        if (!item->context->athlete->zones(item->isRun) || !item->ride()->areDataPresent()->watts) {
+        const Zones* zones = item->context->athlete->zones(item->isRun);
+        if (!zones || !item->ride()->areDataPresent()->watts) {
             setValue(RideFile::NIL);
             setCount(0);
             return;
@@ -138,7 +140,7 @@ class PowerZone : public RideMetric {
         double percent=0;
 
         // if range is -1 we need to fall back to a default value
-        int zone = item->zoneRange >= 0 ? item->context->athlete->zones(item->isRun)->whichZone(item->zoneRange, ap) + 1 : 0;
+        int zone = item->zoneRange >= 0 ? zones->whichZone(item->zoneRange, ap) + 1 : 0;
 
         // ok, how far up  the zone was this?
         if (item->zoneRange >= 0 && zone) {
@@ -146,13 +148,13 @@ class PowerZone : public RideMetric {
             // get zone info
             QString name, description;
             int low, high;
-            item->context->athlete->zones(item->isRun)->zoneInfo(item->zoneRange, zone-1, name, description, low, high);
+            zones->zoneInfo(item->zoneRange, zone-1, name, description, low, high);
 
             // use Pmax as upper bound, this is used
             // for the limit of upper zone ALWAYS
-            if (high > item->context->athlete->zones(item->isRun)->getPmax(item->zoneRange)) 
-                high = item->context->athlete->zones(item->isRun)->getPmax(item->zoneRange);
-
+            const int pmax = zones->getPmax(item->zoneRange);
+            high = std::max(high, pmax);
+            
             // how far in?
             percent = double(ap-low) / double(high-low);
 

src/Train/TodaysPlanWorkoutDownload.cpp
@@ -416,12 +416,12 @@ TodaysPlanWorkoutDownload::getFileList(QString &error, QDateTime from, QDateTime
             for(int i=0; i<results.size(); i++) {
                 QJsonObject each = results.at(i).toObject();
                 QJsonObject scheduled = each["scheduled"].toObject();
-                TodaysPlanWorkoutListEntry *add = new TodaysPlanWorkoutListEntry();
 
                 // skip "Rest" days
                 if (scheduled["workout"].toString() == "rest" && scheduled["tscorepwr"].toDouble() == 0.0) continue;
 
                 // workout details
+                TodaysPlanWorkoutListEntry *add = new TodaysPlanWorkoutListEntry();
                 add->workoutId = QString("%1").arg(each["workoutId"].toInt());
                 add->planDate = QDateTime::fromMSecsSinceEpoch(each["startTs"].toDouble()).date();
                 add->duration = scheduled["durationSecs"].toDouble();

commit 2b4759c16d55003efc04de9158931adefd79dea6

Fix train view select ERG crash
.. regression introduced by c187bda7

src/Train/WorkoutWidget.cpp
@@ -1434,10 +1434,9 @@ WorkoutWidget::ergFileSelected(ErgFile *ergFile)
             WWPoint *add = new WWPoint(this, point.x / 1000.0f, point.y); // in ms
 
             // increase view and workout maxes to match workout loaded
-            // as we goo these just increase to the last point
+           // as we goo these just increase to the last point
             if (add->x > maxWX_) maxVX_ = maxWX_ = add->x;
             if (add->y > maxY_) maxY_ = add->y;
-            delete add;
         }
 
         maxY_ *= 1.1f;