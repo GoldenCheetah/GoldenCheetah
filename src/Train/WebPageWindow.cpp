ec1866cddf7c47618768399cc924f5c179cb96f4
Mark Liversedge <liversedge@gmail.com>
2017-05-20 11:34:37 +0100

    Fix Webpage Download Intercept Duplicates
    .. only process the download request if we are visible.
    
    .. otherwise you get an import wizard for every active
       WebPageWindow (!).

--- src/Train/WebPageWindow.cpp
+++ src/Train/WebPageWindow.cpp
@@ -152,10 +152,10 @@ WebPageWindow::WebPageWindow(Context *context) : GcChartWindow(context), context
     view->page()->profile()->setCachePath(temp);
     view->page()->profile()->setPersistentStoragePath(temp);
 
-    // web scheme handler
-    WebSchemeHandler *handler = new WebSchemeHandler(view);
+    // web scheme handler - not needed must compile with QT 5.9 or higher
+    //WebSchemeHandler *handler = new WebSchemeHandler(view);
     //view->page()->profile()->installUrlSchemeHandler("filesystem:https", handler);
-    view->page()->profile()->installUrlSchemeHandler("filesystem", handler);
+    //view->page()->profile()->installUrlSchemeHandler("filesystem", handler);
 
     // add some settings
     view->settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessRemoteUrls, true);
@@ -285,6 +285,9 @@ WebPageWindow::event(QEvent *event)
 void
 WebPageWindow::downloadRequested(QWebEngineDownloadItem *item)
 {
+    // only do it if I am visible, as shared across web page instances
+    if (!amVisible()) return;
+
     //qDebug()<<"Download Requested:"<<item->path()<<item->url().toString();
 
     //qDebug() << "Format: " <<  item->savePageFormat();
