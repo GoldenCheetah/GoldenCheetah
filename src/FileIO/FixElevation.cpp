d5c041ef162f74d5e651510c0f3f9e134a4a891a
Joern <joern.rm@gmail.com>
2017-06-05 19:56:49 +0200

    Fix Build Errors in CI

--- src/FileIO/FixElevation.cpp
+++ src/FileIO/FixElevation.cpp
@@ -25,6 +25,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QMessageBox>
 
 // MapQuest default API key.
 // If you have reliability problems with Fix Elevation, caused by too
