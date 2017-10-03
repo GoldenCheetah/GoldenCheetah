d50030064d19508a3ee625f45354f193adf9e654
Leif Warland <leif.warland@getmail.no>
2017-10-03 18:58:08 +0200

    Add option to caculate new speed and distance using FixGPS

--- src/FileIO/FixGPS.cpp
+++ src/FileIO/FixGPS.cpp
@@ -20,6 +20,7 @@
 #include "Settings.h"
 #include "Units.h"
 #include "HelpWhatsThis.h"
+#include "FixGPS.h"
 #include <algorithm>
 #include <QVector>
 
@@ -30,12 +31,22 @@ class FixGPSConfig : public DataProcessorConfig
     Q_DECLARE_TR_FUNCTIONS(FixGPSConfig)
     friend class ::FixGPS;
     protected:
+        QHBoxLayout *layout;
+        QCheckBox *adjustSpeed;
     public:
         // there is no config
         FixGPSConfig(QWidget *parent) : DataProcessorConfig(parent) {
 
             HelpWhatsThis *help = new HelpWhatsThis(parent);
             parent->setWhatsThis(help->getWhatsThisText(HelpWhatsThis::MenuBar_Edit_FixGPSErrors));
+
+            layout = new QHBoxLayout(this);
+            layout->setContentsMargins(0,0,0,0);
+            setContentsMargins(0,0,0,0);
+
+            adjustSpeed = new QCheckBox(tr("Adjust distance and speed"));
+            layout->addWidget(adjustSpeed);
+            layout->addStretch();
         }
 
         QString explain() {
@@ -44,8 +55,13 @@ class FixGPSConfig : public DataProcessorConfig
                            "or the data that was recorded is invalid.")));
         }
 
-        void readConfig() {}
-        void saveConfig() {}
+        void readConfig() {
+            bool SetSpeed = appsettings->value(NULL, GC_DPGPS_SPEED, Qt::Unchecked).toBool();
+            adjustSpeed->setCheckState(SetSpeed ? Qt::Checked : Qt::Unchecked);
+        }
+        void saveConfig() {
+            appsettings->setValue(GC_DPGPS_SPEED, adjustSpeed->checkState());
+        }
 };
 
 
@@ -79,17 +95,24 @@ static bool fixGPSAdded = DataProcessorFactory::instance().registerProcessor(QSt
 bool
 FixGPS::postProcess(RideFile *ride, DataProcessorConfig *config, QString op)
 {
-    Q_UNUSED(config)
     Q_UNUSED(op)
 
     // ignore null or files without GPS data
     if (!ride || ride->areDataPresent()->lat == false || ride->areDataPresent()->lon == false)
         return false;
 
-    int errors=0;
+    bool setSpeed;
+
+    if (config == NULL) { // beging called automatically
+        setSpeed = appsettings->value(NULL, GC_DPGPS_SPEED, Qt::Unchecked).toBool();
+    }
+    else { // being called manually
+        setSpeed = (bool) ((FixGPSConfig*)(config))->adjustSpeed->checkState();
+    }
 
     ride->command->startLUW("Fix GPS Errors");
 
+    int errors=0;
     int lastgood = -1;  // where did we last have decent GPS data?
     for (int i=0; i<ride->dataPoints().count(); i++) {
         // is this one decent?
@@ -101,10 +124,36 @@ FixGPS::postProcess(RideFile *ride, DataProcessorConfig *config, QString op)
                 // then set last good to here
                 double deltaLat = (ride->dataPoints()[i]->lat - ride->dataPoints()[lastgood]->lat) / double(i-lastgood);
                 double deltaLon = (ride->dataPoints()[i]->lon - ride->dataPoints()[lastgood]->lon) / double(i-lastgood);
+
+                double _dist, _old_dist, _avg_speed;
+
+                if (setSpeed) {
+                    _old_dist = ride->dataPoints()[lastgood]->km - ride->dataPoints()[i]->km;
+                    if (deltaLat == 0.0 && deltaLon == 0.0){
+                        _dist = 0.0;
+                    }
+                    else {
+                        _dist = sin(_deg2rad(ride->dataPoints()[lastgood]->lat)) * sin(_deg2rad(ride->dataPoints()[i]->lat));
+                        _dist += cos(_deg2rad(ride->dataPoints()[lastgood]->lat)) * cos(_deg2rad(ride->dataPoints()[i]->lat)) * cos(_deg2rad(deltaLon));
+                        _dist = acos(_dist) * 6371;
+                    }
+                    _avg_speed = _dist / (ride->dataPoints()[i]->secs - ride->dataPoints()[lastgood]->secs) * 3600;
+                    _dist /= double(i-lastgood);
+                }
+
                 for (int j=lastgood+1; j<i; j++) {
+                    if (setSpeed) {
+                        ride->command->setPointValue(j, RideFile::kph, _avg_speed);
+                        ride->command->setPointValue(j, RideFile::km, ride->dataPoints()[lastgood]->km + (double(j-lastgood)*_dist));
+                    }
                     ride->command->setPointValue(j, RideFile::lat, ride->dataPoints()[lastgood]->lat + (double(j-lastgood)*deltaLat));
                     ride->command->setPointValue(j, RideFile::lon, ride->dataPoints()[lastgood]->lon + (double(j-lastgood)*deltaLon));
                     errors++;
+                }
+                if (setSpeed && errors>0) {
+                    for (int j=i; j<ride->dataPoints().count(); j++) {
+                        ride->command->setPointValue(j, RideFile::km, ride->dataPoints()[j]->km + _dist - _old_dist);
+                    }
                 }
             } else if (lastgood == -1) {
                 // fill to front
