cb853773532d49e1b62997a6adc8f584340a802a
slorenz <lorenz.stefan@gmail.com>
2017-12-10 00:12:22 +0100

    Use utf-8 for series names

--- src/Python/SIP/sipgoldencheetahQString.cpp
+++ src/Python/SIP/sipgoldencheetahQString.cpp
@@ -62,7 +62,7 @@ static PyObject *convertFrom_QString(void *sipCppV, PyObject *)
     ::QString *sipCpp = reinterpret_cast< ::QString *>(sipCppV);
 
 #line 38 "goldencheetah.sip"
-    return PyUnicode_FromString(sipCpp->toLatin1().data());
+    return PyUnicode_FromString(sipCpp->toUtf8().data());
 #line 67 "./sipgoldencheetahQString.cpp"
 }
 
