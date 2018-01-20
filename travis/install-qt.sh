15361f6f1d5fe4c5dd91b4eae4bd095a8b96a1e1
Stefan Lorenz <stefan@sisao.de>
2018-01-20 19:39:54 +0100

    attempt to fix travis build (changed qt 5.5.1 mirror download location)

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -3,7 +3,7 @@ echo "QT=>" "$QT" "<="
 #!/bin/bash
 if [ "$QT_VER" = "5.5.1" ]
 then
- brew install https://raw.githubusercontent.com/Homebrew/homebrew/fb64f6cd91ff19d9c651b7851a74808bde6bc60f/Library/Formula/qt5.rb
+ brew install ./qt5.rb
 else
  brew install $QT
 fi
