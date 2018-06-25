f5dabd540ca82f919a4afa52004e6ac7af730972
Galileo Sartor <galileo.sartor@gmail.com>
2018-06-25 10:47:43 +0200

    Reduce qt dependency pull

--- travis/linux/before_install_QT5.sh
+++ travis/linux/before_install_QT5.sh
@@ -8,7 +8,8 @@ set -e
 # Update this file on a newer qt version.
 sudo add-apt-repository ppa:beineri/opt-qt595-trusty -y
 sudo apt update
-sudo apt install -y qt59-meta-full flex bison git
+sudo apt install -y qt59-meta-minimal qt59base qt59tools qt59serialport qt59svg\
+ qt59multimedia qt59script qt59connectivity flex bison git
 
-source /opt/qt59/bin/qt59-env.sh; 
+source /opt/qt59/bin/qt59-env.sh
 
