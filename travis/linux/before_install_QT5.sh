6f8c65991f6837b52385450b1c2743fd4f442ccc
Galileo Sartor <galileo.sartor@gmail.com>
2018-06-25 12:37:29 +0200

    Fix linux qt source

--- travis/linux/before_install_QT5.sh
+++ travis/linux/before_install_QT5.sh
@@ -8,5 +8,15 @@ set -e
 # Update this file on a newer qt version.
 sudo add-apt-repository ppa:beineri/opt-qt595-trusty -y
 sudo apt update -qq
-sudo apt install -y qt59-meta-minimal qt59base qt59tools qt59serialport qt59svg\
- qt59multimedia qt59script qt59connectivity flex bison git qt5-default
+sudo apt install -y qt59base qt59tools qt59serialport qt59svg\
+ qt59multimedia qt59script qt59connectivity qt59webengine qt59charts-no-lgpl\
+ flex bison git qt5-default
+
+source /opt/qt*/bin/qt*-env.sh
+#export QTDIR=/opt/qt59
+#export PATH=/opt/qt59/bin:$PATH
+#export LD_LIBRARY_PATH=/opt/qt59/lib/x86_64-linux-gnu:/opt/qt59/lib:$LD_LIBRARY_PATH
+#export PKG_CONFIG_PATH=/opt/qt59/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# Temporarily alias ldrelease
+#ln -s $QTDIR/bin/ldrelease-qt5 $QTDIR/bin/ldrelease-qt4
--- travis/linux/before_script_QT5.sh
+++ travis/linux/before_script_QT5.sh
@@ -2,13 +2,20 @@
 
 # Build GoldenCheetah
 
+set -x
+set -e
+
 cd qwt
 cp qwtconfig.pri.in qwtconfig.pri
 cd ../src
 cp gcconfig.pri.in gcconfig.pri
-
+cd ..
 
 # Can we enable optimizations in the appimage?
 #QMAKE_CXXFLAGS += -O3
 
-source /opt/qt*/bin/qt*-env.sh; qmake -recursive; make
+# Disable webkit, and enable webengine?
+if [[ "$WEBKIT" == "0" ]]; then echo DEFINES += NOWEBKIT >> src/gcconfig.pri; fi
+
+qmake -recursive
+make
