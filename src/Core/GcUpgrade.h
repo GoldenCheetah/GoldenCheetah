60dbc5d2e61cbb65156c65dd306c89dcc776067c
Mark Liversedge <liversedge@gmail.com>
2019-01-18 17:07:48 +0000

    DEVELOPMENT BUILD 1901
    .. markup a new development build.

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -92,6 +92,7 @@
 // 3966 - V3.5 DEVELOPMENT 1804
 // 3967 - V3.5 DEVELOPMENT 1806
 // 3968 - V3.5 DEVELOPMENT 1810
+// 3969 - V3.5 DEVELOPMENT 1901
 
 
 #define VERSION3_BUILD    3010 // released
@@ -102,14 +103,14 @@
 #define VERSION32_BUILD   3200 // released
 #define VERSION33_BUILD   3933 // development release
 #define VERSION34_BUILD   3955 // released
-#define VERSION35_BUILD   3968 // development release
+#define VERSION35_BUILD   3969 // development release
 
 // will keep changing during testing and before final release
 #define VERSION31_BUILD VERSION31_UPG
 
 // the next two will with each build/release
-#define VERSION_LATEST 3968
-#define VERSION_STRING "DEV-V3.5 1810"
+#define VERSION_LATEST 3969
+#define VERSION_STRING "DEV-V3.5 1901"
 
 // default config for this release cycle
 #define VERSION_CONFIG_PREFIX "http://www.goldencheetah.org/defaults/4.0"
