c00efb94e5d472aaf9943b2d393c46383dc800f2
Ale Martinez <amtriathlon@gmail.com>
2019-04-25 14:39:32 -0300

    Set sync on startup and import to false when a cloud account is deleted Fixes #2936

--- src/Gui/Pages.cpp
+++ src/Gui/Pages.cpp
@@ -551,6 +551,8 @@ CredentialsPage::deleteClicked()
 
         // set it inactive
         appsettings->setCValue(context->athlete->cyclist, service->activeSettingName(), false);
+        appsettings->setCValue(context->athlete->cyclist, service->syncOnStartupSettingName(), false);
+        appsettings->setCValue(context->athlete->cyclist, service->syncOnImportSettingName(), false);
 
         // reset
         resetList();
