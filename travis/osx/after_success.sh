893f2a9b28492dd729c025c4d5816142cce8ffe2
Ale Martinez <amtriathlon@gmail.com>
2019-11-17 12:23:53 -0300

    Upload travis-ci MacOS build results to transfer.sh To allow user testing, similar to linux builds. Includes some minor fixes: -Avoid compiler warnings for deprecated declarations to reduce the log size -Patch GoldenCheetah.dmg to include missing libicudata.64.dylib -fix command line error in mackdeployqt -fs option

--- travis/osx/after_success.sh
+++ travis/osx/after_success.sh
@@ -4,7 +4,10 @@ cd src
 echo "Checking GoldenCheetah.app can execute"
 GoldenCheetah.app/Contents/MacOS/GoldenCheetah --help
 echo "About to create dmg file and fix up"
-/usr/local/opt/qt5/bin/macdeployqt GoldenCheetah.app -verbose=2 -fs hfs+ -dmg
+# This is a hack to include libicudata.64.dylib, not handled by macdployqt[fix]
+mkdir GoldenCheetah.app/Contents/Frameworks
+cp /usr/local/opt/icu4c/lib/libicudata.64.dylib GoldenCheetah.app/Contents/Frameworks
+/usr/local/opt/qt5/bin/macdeployqt GoldenCheetah.app -verbose=2 -fs=hfs+ -dmg
 python ../travis/macdeployqtfix.py GoldenCheetah.app /usr/local/opt/qt5
 echo "Cleaning up installed QT libraries from qt5"
 brew remove qt5
@@ -15,6 +18,8 @@ echo "Mounting dmg file and testing it can execute"
 hdiutil mount $FINAL_NAME
 cd /Volumes/GoldenCheetah
 GoldenCheetah.app/Contents/MacOS/GoldenCheetah --help
+echo "Uploading for user tests"
+curl --upload-file $FINAL_NAME https://transfer.sh/$FINAL_NAME
 echo "Make sure we are back in the Travis build directory"
 cd $TRAVIS_BUILD_DIR
 exit
