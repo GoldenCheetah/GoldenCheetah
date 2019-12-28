6eea504796aa3e79eba971aba89e86b002372f55
Ale Martinez <amtriathlon@gmail.com>
2019-12-28 11:09:10 -0300

    Fix travis-ci macOS builds

--- travis/osx/before_install.sh
+++ travis/osx/before_install.sh
@@ -3,7 +3,9 @@ set -ev
 ## try early just to check, can delete later
 date
 brew update
-brew install qt5
+# brew install qt5
+/usr/local/opt/qt5/bin/qmake --version
+brew unlink python@2 # to avoid conflicts with libical dependence on python
 brew install libical
 brew upgrade libusb
 brew install srmio
