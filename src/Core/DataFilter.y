GoldenCheetah/GoldenCheetah

commit 1231f59b1a84fa94e2b0ecfeafe3936a280845d7

DataFilter Grammar for [index]
.. expressions like a+b*c[n] were being parsed as equivalent to
   (a+b*c)[n] instead of a+b+(c[n]).

.. to resolve this symbol[index] is declared first in the grammar
   and whilst it will introduce reduce/reduce warnings that is
   actually what we want (we declare it early to override the
   definition from expr below).

.. keener yacc wizards might have a more elegant solution for
   this one....

src/Core/DataFilter.y
@@ -297,7 +297,17 @@ lexpr:
                                                 }
         ;
 
-array:  expr '[' expr ']'                    {
+array:
+         symbol '[' expr ']'                    { // reduce/reduce conflict, but added here so gets resolved
+                                                  // first (order appeared in this file.
+                                                  // e.g. a+b[1] is resolved as a+(b[1]) rather than (a+b)[1]
+                                                  $$ = new Leaf(@1.first_column, @4.last_column);
+                                                  $$->type = Leaf::Index;
+                                                  $$->lvalue.l = $1;
+                                                  $$->fparms << $3;
+                                                  $$->op = 0;
+                                                }
+         | expr '[' expr ']'                    {
                                                   $$ = new Leaf(@1.first_column, @4.last_column);
                                                   $$->type = Leaf::Index;
                                                   $$->lvalue.l = $1;

src/Core/DataFilter.cpp
@@ -2705,6 +2705,7 @@ Result Leaf::eval(DataFilterRuntime *df, Leaf *leaf, float x, long it, RideItem 
             returning.vector[1]=calc.b;
             returning.vector[2]=calc.r2;
             returning.vector[3]=calc.see;
+            returning.number = calc.m + calc.b + calc.r2 + calc.see; // sum
 
             return returning;
         }

commit 4912f3731db67dfa0bb3ad3d8f4ccb486817734c

Fix [index] reduce/reduce warning for Datafilter.y
.. increasing the precedence for the [ ] symbols resolved the
   reduce/reduce issue introduced in commit 1231f59b1a8

.. as a result the new rule added in that commit has been
   removed.

.. thanks to Ale Martinez for the correct fix.

src/Core/DataFilter.y
@@ -78,12 +78,12 @@ extern Leaf *DataFilterroot; // root node for parsed statement
 %type <comp> statements
 
 %right '?' ':'
-%right '[' ']'
 %right AND OR
 %right EQ NEQ LT LTE GT GTE MATCHES ENDSWITH CONTAINS
 %left ADD SUBTRACT
 %left MULTIPLY DIVIDE
 %right POW
+%right '[' ']'
 
 %start filter;
 %%
@@ -287,16 +287,7 @@ lexpr:
         ;
 
 array:
-         symbol '[' expr ']'                    { // reduce/reduce conflict, but added here so gets resolved
-                                                  // first (order appeared in this file.
-                                                  // e.g. a+b[1] is resolved as a+(b[1]) rather than (a+b)[1]
-                                                  $$ = new Leaf(@1.first_column, @4.last_column);
-                                                  $$->type = Leaf::Index;
-                                                  $$->lvalue.l = $1;
-                                                  $$->fparms << $3;
-                                                  $$->op = 0;
-                                                }
-         | expr '[' expr ']'                    {
+         expr '[' expr ']'                    {
                                                   $$ = new Leaf(@1.first_column, @4.last_column);
                                                   $$->type = Leaf::Index;
                                                   $$->lvalue.l = $1;