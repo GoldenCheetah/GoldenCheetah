c7515e040ba54a76be0a7e6a40c6a138ae6f6a64
Mark Liversedge <liversedge@gmail.com>
2020-04-11 21:29:57 +0100

    Reinstate head toolbar color
    .. Theme colors for chrome were not being applied to the main toolbar
       after the last commit. A couple of temporary hacks got left into the
       commit.

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -2205,7 +2205,6 @@ void
 MainWindow::configChanged(qint32)
 {
 
-#if 0
 // Windows and Linux menu bar should match chrome
 #if defined (WIN32) || defined (Q_OS_LINUX)
     QColor textCol(Qt::black);
@@ -2214,7 +2213,6 @@ MainWindow::configChanged(qint32)
     menuBar()->setStyleSheet(QString("QMenuBar { color: %1; background: %2; }"
                              "QMenuBar::item { color: %1; background: %2; }")
                              .arg(textCol.name()).arg(menuColorString));
-#endif
 #endif
 
 // Mac and Linux
