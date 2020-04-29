c0bed13cfa65c69da41a7a6137451d59854d30a3
Ale Martinez <amtriathlon@gmail.com>
2020-04-29 15:17:26 -0300

    Add vlc plugins to Linux AppImage for Travis-ci builds This avoids the need to install vlc 2.2 and set VLC_PLUGIN_PATH Fixes #3394

--- travis/linux/after_success.sh
+++ travis/linux/after_success.sh
@@ -34,6 +34,9 @@ mkdir appdir/lib
 cp /usr/local/lib/libssl.so.1.1 appdir/lib
 cp /usr/local/lib/libcrypto.so.1.1 appdir/lib
 
+### Add vlc
+cp -r /usr/lib/vlc appdir/lib/vlc
+
 ### Download current version of linuxdeployqt
 wget --no-verbose -c https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
 chmod a+x linuxdeployqt-6-x86_64.AppImage
