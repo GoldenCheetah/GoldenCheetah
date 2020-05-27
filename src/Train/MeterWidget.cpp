c7eda60adddbb2a51293cc22781d22456aa729c6
Peter <human705-one@yahoo.com>
2020-05-27 10:12:07 -0400

    Removed GOOGLE_KEY, moved Trainer Status to top center.

--- src/Train/MeterWidget.cpp
+++ src/Train/MeterWidget.cpp
@@ -21,10 +21,9 @@
 #include "MeterWidget.h"
 #include "ErgFile.h"
 #include "Context.h"
+#include "Units.h"
 
 #include <QtWebEngineWidgets/QWebEngineView>
-
-#define GOOGLE_KEY "ABQIAAAAS9Z2oFR8vUfLGYSzz40VwRQ69UCJw2HkJgivzGoninIyL8-QPBTtnR-6pM84ljHLEk3PDql0e2nJmg"
 
 MeterWidget::MeterWidget(QString Name, QWidget *parent, QString Source) : QWidget(parent), m_Name(Name), m_container(parent), m_Source(Source)
 {
@@ -392,18 +391,39 @@ void ElevationMeterWidget::paintEvent(QPaintEvent* paintevent)
     painter.setPen(m_OutlinePen);
     painter.drawLine(cyclistX, 0.0, cyclistX, (double)m_Height-bubbleSize);
 
-    //Cosmetic enhancment: Display grade as #.#% 
-    std::string sGrad;
-    QString s_grad ="";
-    s_grad = ((-1.0 < this->gradientValue && this->gradientValue < 0.0)?QString("-"):QString("")) + QString::number((int) this->gradientValue);
-    s_grad += QString(".") + QString::number(abs((int)(this->gradientValue * 10.0) % 10)) + QString("%");
+    // Display grade as #.#% 
+    QString gradientString = ((-1.0 < this->gradientValue && this->gradientValue < 0.0) ? QString("-") : QString("")) + QString::number((int)this->gradientValue) +
+        QString(".") + QString::number(abs((int)(this->gradientValue * 10.0) % 10)) + QString("%");
 
     // Display gradient text to the right of the line until the middle, then display to the left of the line
-    if (cyclistX < m_Width*0.5) {
-        painter.drawText((double)cyclistX+5, ((double)m_Height * 0.95), s_grad);
-    } else {
-        painter.drawText((double)cyclistX-45, ((double)m_Height * 0.95), s_grad);
-    }
+    double gradientDrawX = cyclistX;
+    double gradientDrawY = m_Height * 0.95;
+
+    if (cyclistX < m_Width * 0.5)
+        gradientDrawX += 5.;
+    else
+        gradientDrawX = -45.;
+
+    painter.drawText(gradientDrawX, gradientDrawY, gradientString);
+
+    double routeDistance = this->Value;
+    if (!context->athlete->useMetricUnits) routeDistance *= MILES_PER_KM;
+
+    routeDistance = ((int)(routeDistance * 1000.)) / 1000.;
+
+    QString distanceString = QString::number(routeDistance, 'f', 3) +
+        ((context->athlete->useMetricUnits) ? tr("km") : tr("mi"));
+
+    double distanceDrawX = (double)cyclistX;
+    double distanceDrawY = ((double)m_Height * 0.75);
+
+    // Display distance text to the right of the line until the middle, then display to the left of the line
+    if (cyclistX < m_Width * 0.5)
+        distanceDrawX += 5;
+    else
+        distanceDrawX -= 45;
+
+    painter.drawText(distanceDrawX, distanceDrawY, distanceString);
 } 
 
 
@@ -500,7 +520,7 @@ void LiveMapWidget::createHtml(double dLat, double dLon, int iMapZoom)
     currentPage += QString("var mapOptions = {\n"
         "    center: [" + sLat + ", " + sLon + "] ,\n"
         "    zoom : " + sMapZoom + ",\n"
-        "    zoomControl : false,\n"
+        "    zoomControl : true,\n"
         "    scrollWheelZoom : false,\n"
         "    dragging : false,\n"
         "    doubleClickZoom : false}\n");
