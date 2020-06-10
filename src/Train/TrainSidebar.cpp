GoldenCheetah/GoldenCheetah

commit 1402f6ad6a87fa80474749f0eed70bbe6d0cf674

Ride GPX files in Train View
.. ErgFile now supports gpx format as well as .erg,.mrc et al
.. Location data is include in realtime data and passed through
.. the CSV file format has been fixed to record GPS with higher precision

Fixes #3021
Fixes #3024

src/Train/TrainSidebar.cpp
@@ -353,6 +353,7 @@ TrainSidebar::TrainSidebar(Context *context) : GcWindow(context), context(contex
     displaySpeed = displayCadence = slope = load = 0;
     displaySMO2 = displayTHB = displayO2HB = displayHHB = 0;
     displayLRBalance = displayLTE = displayRTE = displayLPS = displayRPS = 0;
+    displayLatitude = displayLongitude = displayAltitude = 0.0;
 
     connect(gui_timer, SIGNAL(timeout()), this, SLOT(guiUpdate()));
     connect(disk_timer, SIGNAL(timeout()), this, SLOT(diskUpdate()));
@@ -503,7 +504,7 @@ TrainSidebar::eventFilter(QObject *, QEvent *event)
             // and call any training method
             //
             // KEY        FUNCTION
-            //            Start() - will pause/unpause if running 
+            //            Start() - will pause/unpause if running
             //            Stop() - will end workout
             //            Pause() - pause only
             //            UnPause() - unpause
@@ -845,7 +846,7 @@ TrainSidebar::updateMetricLapDistance()
     if (lapmarker == -1) {
         displayLapDistance = 0;
         return;
-    } 
+    }
 
     displayLapDistance = (currentposition - lapmarker) / (double) 1000;
 }
@@ -873,7 +874,7 @@ TrainSidebar::updateMetricLapDistanceRemaining()
         // In this case, there are either no lap markers, or we are in last lap (and so no next lap)
         displayLapDistanceRemaining = -1;
         return;
-    } 
+    }
 
     displayLapDistanceRemaining = (lapmarker - currentposition) / (double) 1000;
 }
@@ -1451,6 +1452,9 @@ void TrainSidebar::updateData(RealtimeData &rtData)
     displayTHB  = rtData.gettHb();
     displayO2HB = rtData.getO2Hb();
     displayHHB = rtData.getHHb();
+    displayLatitude = rtData.getLatitude();
+    displayLongitude = rtData.getLongitude();
+    displayAltitude = rtData.getAltitude();
     // Gradient not supported
     return;
 }
@@ -1540,12 +1544,12 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
         // disabling the screen saver on a Mac instead of
         // temporarily adjusting/disabling the user preferences
         // for screen saving and power management. Makes sense.
-        
+
         CFStringRef reasonForActivity = CFSTR("TrainSidebar::guiUpdate");
         IOPMAssertionID assertionID;
         IOReturn suspendSreensaverSuccess = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep, kIOPMAssertionLevelOn, reasonForActivity, &assertionID);
 #endif
-        
+
         if(calibrating) {
             foreach(int dev, activeDevices) { // Do for selected device only
                 RealtimeData local = rtData;
@@ -1680,6 +1684,21 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
                 rtData.setLapDistance(displayLapDistance);
                 rtData.setLapDistanceRemaining(displayLapDistanceRemaining);
 
+                // Update location data
+                if (ergFile) {
+                    geolocation geoloc;
+                    if (ergFile->locationAt(displayWorkoutDistance * 1000, displayWorkoutLap, geoloc))
+                    {
+                        displayLatitude = geoloc.Lat();
+                        displayLongitude = geoloc.Long();
+                        displayAltitude = geoloc.Alt();
+
+                        rtData.setLatitude(displayLatitude);
+                        rtData.setLongitude(displayLongitude);
+                        rtData.setAltitude(displayAltitude);
+                    }
+                }
+
                 // time
                 total_msecs = session_elapsed_msec + session_time.elapsed();
                 lap_msecs = lap_elapsed_msec + lap_time.elapsed();
@@ -1754,6 +1773,9 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
             displayTHB = rtData.gettHb();
             displayO2HB = rtData.getO2Hb();
             displayHHB = rtData.getHHb();
+            displayLatitude = rtData.getLatitude();
+            displayLongitude = rtData.getLongitude();
+            displayAltitude = rtData.getAltitude();
 
             // virtual speed
             double crr = 0.004f; // typical for asphalt surfaces
@@ -1814,7 +1836,7 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
                 context->notifySetNow(rtData.getMsecs());
             }
         }
-        
+
 #ifdef Q_OS_MAC
         if (suspendSreensaverSuccess == kIOReturnSuccess)
         {
@@ -1877,6 +1899,25 @@ void TrainSidebar::warnnoConfig()
     QMessageBox::warning(this, tr("No Devices Configured"), tr("Please configure a device in Preferences."));
 }
 
+template<class T, typename T_GetMethod, typename T_SetMethod, typename T_arg>
+struct ScopedOp
+{
+    T* m_pt;
+    T_SetMethod m_setMethod;
+    T_arg m_argSave;
+
+    ScopedOp(T* pt, T_GetMethod getMethod, T_SetMethod setMethod, T_arg argNew) : m_pt(pt), m_setMethod(setMethod) {
+        m_argSave = (m_pt->*getMethod)();
+        (m_pt->*m_setMethod)(argNew);
+    }
+
+    virtual ~ScopedOp() { (m_pt->*m_setMethod)(m_argSave); }
+};
+
+struct ScopedPrecision : ScopedOp<QTextStream, int (QTextStream::*)() const, void (QTextStream::*)(int), int> {
+    ScopedPrecision(QTextStream* pc, int tempPrecision) : ScopedOp::ScopedOp(pc, &QTextStream::realNumberPrecision, &QTextStream::setRealNumberPrecision, tempPrecision) {}
+};
+
 //----------------------------------------------------------------------
 // DISK UPDATE FUNCTIONS
 //----------------------------------------------------------------------
@@ -1884,7 +1925,7 @@ void TrainSidebar::diskUpdate()
 {
     int  secs;
 
-    long torq = 0, altitude = 0;
+    long torq = 0;
     QTextStream recordFileStream(recordFile);
 
     if (calibrating) return;
@@ -1902,11 +1943,20 @@ void TrainSidebar::diskUpdate()
                         << "," << displayDistance
                         << "," << displaySpeed
                         << "," << torq
-                        << "," << displayPower
-                        << "," << altitude
-                        << "," // lon
-                        << "," // lat
-                        << "," // headwind
+                        << "," << displayPower;
+
+    // QTextStream default precision is 6, location data needs much more than that.
+    // Avoid extra precision for other fields since it isn't needed and would grow
+    // the intermediate file for no reason.
+    {
+        ScopedPrecision tempPrecision(&recordFileStream, 20);
+
+        recordFileStream << "," << displayAltitude
+                         << "," << displayLongitude
+                         << "," << displayLatitude;
+    }
+
+    recordFileStream    << "," // headwind
                         << "," // slope
                         << "," // temp
                         << "," << (displayLap + displayWorkoutLap)
@@ -1958,6 +2008,14 @@ void TrainSidebar::loadUpdate()
         }
     } else {
         slope = ergFile->gradientAt(displayWorkoutDistance*1000, curLap);
+
+        geolocation geoloc;
+        if (ergFile->locationAt(displayWorkoutDistance * 1000, curLap, geoloc))
+        {
+            displayLatitude = geoloc.Lat();
+            displayLongitude = geoloc.Long();
+            displayAltitude = geoloc.Alt();
+        }
 
         if(displayWorkoutLap != curLap)
         {
@@ -2439,7 +2497,7 @@ void TrainSidebar::adjustIntensity(int value)
                     add.val = last.val / from * to;
 
                     // recalibrate altitude if gradient changing
-                    if (context->currentErgFile()->format == CRS) add.y = last.y + ((add.x-last.x) * (add.val/100));
+                    if (context->currentErgFile()->format == CRS || context->currentErgFile()->format == CRS_LOC) add.y = last.y + ((add.x-last.x) * (add.val/100));
                     else add.y = add.val;
 
                     context->currentErgFile()->Points.insert(i, add);
@@ -2455,7 +2513,7 @@ void TrainSidebar::adjustIntensity(int value)
 
             // recalibrate altitude if in CRS mode
             p->val = p->val / from * to;
-            if (context->currentErgFile()->format == CRS) {
+            if (context->currentErgFile()->format == CRS || context->currentErgFile()->format == CRS_LOC) {
                 if (i) p->y = last.y + ((p->x-last.x) * (last.val/100));
             }
             else p->y = p->val;

src/FileIO/LocationInterpolation.cpp
@@ -34,7 +34,7 @@ xyz geolocation::toxyz()
     //
     // D. Rose (2014).
     // "Converting between Earth-Centered, Earth Fixed and Geodetic Coordinates"
-    // 
+    //
 
     //Convert Lat, Lon, Altitude to Earth-Centered-Earth-Fixed (ECEF)
     //Input is a three element array containing lat, lon (rads) and alt (m)
@@ -63,7 +63,7 @@ geolocation xyz::togeolocation()
     //
     // D. Rose (2014).
     // "Converting between Earth-Centered, Earth Fixed and Geodetic Coordinates"
-    // 
+    //
 
     //Convert Earth-Centered-Earth-Fixed (ECEF) to lat, Lon, Altitude
     //Input is a three element array containing x, y, z in meters

src/FileIO/LocationInterpolation.h
@@ -16,6 +16,9 @@
 * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#if !defined(_LOCATION_INTERPOLATION_H)
+#define _LOCATION_INTERPOLATION_H
+
 #include <tuple>
 #include <bitset>
 #include "qwt_math.h"
@@ -29,6 +32,8 @@ class v3
 public:
 
     v3(double a, double b, double c) : m_t(a, b, c) {};
+
+    v3(const v3& o) : m_t(o.m_t) {}
 
     double  x() const { return std::get<0>(m_t); }
     double  y() const { return std::get<1>(m_t); }
@@ -79,7 +84,7 @@ struct xyz : public v3
     geolocation togeolocation();
 };
 
-struct geolocation : v3 
+struct geolocation : v3
 {
     double Lat()  const { return x(); }
     double Long() const { return y(); }
@@ -139,12 +144,12 @@ struct LinearTwoPointInterpolator : public TwoPointInterpolator
     xyz InterpolateNext(xyz p0, xyz p1);
 };
 
-struct SphericalTwoPointInterpolator : public TwoPointInterpolator 
+struct SphericalTwoPointInterpolator : public TwoPointInterpolator
 {
     xyz InterpolateNext(xyz p0, xyz p1);
 };
 
-class UnitCatmullRomInterpolator 
+class UnitCatmullRomInterpolator
 {
     std::tuple<double, double, double, double> m_p;
 
@@ -156,7 +161,7 @@ public:
     double Interpolate(double u);
 };
 
-class UnitCatmullRomInterpolator3D 
+class UnitCatmullRomInterpolator3D
 {
     UnitCatmullRomInterpolator x, y, z;
 
@@ -428,6 +433,17 @@ public:
         return r;
     }
 
+    bool GetBracket(double &d0, double &d1)
+    {
+        if (!m_DistanceWindow.hasp0() || !m_DistanceWindow.hasp1())
+            return false;
+
+        d0 = m_DistanceWindow.p0();
+        d1 = m_DistanceWindow.p1();
+
+        return true;
+    }
+
     xyz Interpolate(double distance)
     {
         // Continue to advance queue after input is complete.
@@ -479,3 +495,5 @@ public:
 
     void Push(double distance, geolocation point);
 };
+
+#endif

src/Train/DialWindow.cpp
@@ -499,6 +499,21 @@ DialWindow::telemetryUpdate(const RealtimeData &rtData)
         valueLabel->setText(QString("%1").arg(value, 0, 'f', 1));
         break;
 
+    case RealtimeData::Latitude:
+    case RealtimeData::Longitude:
+        {
+            // These 2 fields should be treated as generic location update
+            double lat = rtData.value(RealtimeData::Latitude);
+            double lon = rtData.value(RealtimeData::Longitude);
+    
+            valueLabel->setText(QString("Lon:%1\nLat:%2").arg(lat, 0, 'f', 10).arg(lon, 0, 'f', 10));
+        }
+        break;
+
+    case RealtimeData::Altitude:
+        valueLabel->setText(QString("%1").arg(value, 0, 'f', 1));
+        break;
+
     default:
         valueLabel->setText(QString("%1").arg(round(displayValue)));
         break;
@@ -648,6 +663,9 @@ void DialWindow::seriesChanged()
            foreground = GColor(CRTE);
            break;
 
+    case RealtimeData::Altitude:
+           foreground = GColor(CALTITUDE);
+           break;
     }
 
     // ugh. we use style sheets because palettes don't work on labels

src/Train/ErgFile.cpp
@@ -38,6 +38,7 @@ static bool setSupported()
     ::supported << ".crs";
     ::supported << ".pgmf";
     ::supported << ".zwo";
+    ::supported << ".gpx";
     return true;
 }
 static bool isinit = setSupported();
@@ -99,6 +100,7 @@ void ErgFile::reload()
     // like we do with ride files if we end up with lots of different formats
     if (filename.endsWith(".pgmf", Qt::CaseInsensitive)) parseTacx();
     else if (filename.endsWith(".zwo", Qt::CaseInsensitive)) parseZwift();
+    else if (filename.endsWith(".gpx", Qt::CaseInsensitive)) parseGpx();
     else parseComputrainer();
 
 }
@@ -115,6 +117,7 @@ void ErgFile::parseZwift()
     MaxWatts = 0;       // maxWatts in this ergfile (scaling)
     valid = false;             // did it parse ok?
     rightPoint = leftPoint = 0;
+    interpolatorReadIndex = 0;
     format = ERG; // default to couse until we know
     Points.clear();
     Laps.clear();
@@ -153,6 +156,7 @@ void ErgFile::parseZwift()
         Duration = Points.last().x;      // last is the end point in msecs
         leftPoint = 0;
         rightPoint = 1;
+        interpolatorReadIndex = 0;
 
         // calculate climbing etc
         calculateMetrics();
@@ -171,6 +175,7 @@ void ErgFile::parseTacx()
     MaxWatts = 0;       // maxWatts in this ergfile (scaling)
     valid = false;             // did it parse ok?
     rightPoint = leftPoint = 0;
+    interpolatorReadIndex = 0;
     format = CRS; // default to couse until we know
     Points.clear();
     Laps.clear();
@@ -333,6 +338,7 @@ void ErgFile::parseTacx()
         Duration = Points.last().x;      // last is the end point in msecs
         leftPoint = 0;
         rightPoint = 1;
+        interpolatorReadIndex = 0;
 
         // calculate climbing etc
         calculateMetrics();
@@ -586,12 +592,137 @@ void ErgFile::parseComputrainer(QString p)
 
         leftPoint = 0;
         rightPoint = 1;
+        interpolatorReadIndex = 0;
 
         calculateMetrics();
 
     } else {
         valid = false;
     }
+}
+
+// parse gpx into ergfile
+void ErgFile::parseGpx()
+{
+    // Initialise
+    Version = "";
+    Units = "";
+    Filename = "";
+    Name = "";
+    Duration = -1;
+    Ftp = 0;            // FTP this file was targetted at
+    MaxWatts = 0;       // maxWatts in this ergfile (scaling)
+    valid = false;             // did it parse ok?
+    rightPoint = leftPoint = 0;
+    interpolatorReadIndex = 0;
+    format = mode = CRS;
+    Points.clear();
+    Laps.clear();
+
+    static double km = 0;
+
+    QFile gpxFile(filename);
+
+    // open the file
+    if (gpxFile.open(QIODevice::ReadOnly | QIODevice::Text) == false) {
+        valid = false;
+        return;
+    }
+
+    // Instantiate ride
+    //RideFile *RideFileFactory::openRideFile(Context *context, QFile &file,
+    //    QStringList &errors, QList<RideFile*> *rideList) const
+
+    QStringList errors_;
+    RideFile* ride = RideFileFactory::instance().openRideFile(context, gpxFile, errors_);
+    if (ride == NULL)
+    {
+        valid = false;
+        return;
+    }
+
+    // Enumerate the data types that are available from this gpx.
+    bool fHasKm    = ride->areDataPresent()->km;
+    bool fHasLat   = ride->areDataPresent()->lat;
+    bool fHasLon   = ride->areDataPresent()->lon;
+    bool fHasAlt   = ride->areDataPresent()->alt;
+    bool fHasSlope = ride->areDataPresent()->slope;
+
+    if (fHasLat && fHasLon)
+    {
+        format = mode = CRS_LOC;
+    }
+
+    if (fHasKm && fHasSlope) {}     // same as crs file
+    else if (fHasKm && fHasAlt) {}  // derive slope from distance and alt
+    else {
+        valid = false;
+        return;
+    }
+
+    int pointCount = ride->dataPoints().count();
+
+    RideFilePoint* prevPoint = NULL;
+    RideFilePoint* point = NULL;
+    RideFilePoint* nextPoint = (ride->dataPoints()[0]);
+    double alt = 0;
+    for (int i = 0; i < pointCount; i++) {
+        ErgFilePoint add;
+
+        prevPoint = point;
+        point = nextPoint;
+        nextPoint = ((i + 1) < pointCount) ? (ride->dataPoints()[i + 1]) : NULL;
+
+        // Determine slope to next point
+        double slope = 0.0;
+
+        if (fHasAlt)
+            alt = point->alt;
+
+        if (fHasSlope) {
+            slope = point->slope;
+
+            if (!fHasAlt && prevPoint)
+            {
+                alt += ((slope * (point->km - prevPoint->km))) / 100.0;
+            }
+
+        } else if (nextPoint && fHasAlt)
+        {
+            double km0 = point->km;
+            double alt0 = point->alt / 1000;
+
+            double km1 = nextPoint->km;
+            double alt1 = nextPoint->alt / 1000;
+
+            slope = 100 * (alt1 - alt0) / (km1 - km0);
+        }
+
+        add.x   = 1000 * point->km; // record distance in meters
+        add.y   = alt;
+        add.val = slope;
+
+        if (fHasLat) add.lat = point->lat;
+        if (fHasLon) add.lon = point->lon;
+
+        if (add.y > MaxWatts) MaxWatts = add.y;
+
+        Points.append(add);
+    }
+
+    gpxFile.close();
+
+    valid = true;
+
+    // set ErgFile duration
+    Duration = Points.last().x;      // end point in meters
+
+    leftPoint = 0;                   // setup initial sample bracketing
+    rightPoint = 1;
+    interpolatorReadIndex = 0;
+
+    // calculate climbing etc
+    calculateMetrics();
 }
 
 // convert points to set of sections
@@ -622,7 +753,7 @@ ErgFile::save(QStringList &errors)
 {
     // save the file including changes
     // XXX TODO we don't support writing pgmf or CRS just yet...
-    if (filename.endsWith("pgmf", Qt::CaseInsensitive) || format == CRS) {
+    if (filename.endsWith("pgmf", Qt::CaseInsensitive) || format == CRS || format == CRS_LOC) {
         errors << QString(QObject::tr("Unsupported file format"));
         return false;
     }
@@ -632,6 +763,7 @@ ErgFile::save(QStringList &errors)
     if (format==ERG) typestring = "ERG";
     if (format==MRC) typestring = "MRC";
     if (format==CRS) typestring = "CRS";
+    if (format == CRS_LOC) typestring = "CRS";
     if (filename.endsWith("zwo", Qt::CaseInsensitive)) typestring="ZWO";
 
     // get CP so we can scale back etc
@@ -1023,6 +1155,86 @@ ErgFile::gradientAt(long x, int &lapnum)
     return Points.at(leftPoint).val;
 }
 
+// Returns true if a location is determined, otherwise returns false.
+bool ErgFile::locationAt(long meters, int &lapnum, geolocation &geoLoc)
+{
+    if (!isValid()) return false; // not a valid ergfile
+
+    // is it in bounds?
+    if (meters < 0 || meters > Duration) return false;
+
+    // No location unless... format contains location...
+    if (format != CRS_LOC)  return 1;
+
+    if (Laps.count() > 0) {
+        int lap = 0;
+        for (int i = 0; i<Laps.count(); i++) {
+            if (meters >= Laps.at(i).x) lap += 1;
+        }
+        lapnum = lap;
+
+    }
+    else lapnum = 0;
+
+    // Ensure that interpolator is correctly primed for this request.
+
+    // find right section of the file
+    while (meters < Points.at(leftPoint).x || meters > Points.at(rightPoint).x) {
+        if (meters < Points.at(leftPoint).x) {
+            leftPoint--;
+            rightPoint--;
+        }
+        else if (meters > Points.at(rightPoint).x) {
+            leftPoint++;
+            rightPoint++;
+        }
+    }
+
+    // At this point leftpoint and rightpoint bracket the query distance. Three cases:
+    // Bracket Covered: If query bracket compatible with the current interpolation bracket then simply interpolate
+    // Bracket Ahead:   If query bracket is ahead of current interpolation bracket then feed points to interpolator until it matches.
+    // Bracket Behind:  New bracket is behind current interpolation bracket then interpolator must be reset and new values fed in.
+
+    double d0, d1;
+
+    if (!gpi.GetBracket(d0, d1))
+    {
+        interpolatorReadIndex = 0;
+        gpi.Reset();
+    } else if (d0 > Points.at(leftPoint).x) {
+        // Current bracket is ahead of interpolator so reset and set index back
+        // so interpolator will be re-primed.
+        gpi.Reset();
+        interpolatorReadIndex = (leftPoint > 0) ? leftPoint - 1 : leftPoint;
+    } else {
+        // Otherwise interpolatorReadIndex is set reasonably.
+        // Is possible to skip ahead though...
+        if (interpolatorReadIndex < rightPoint - 4)
+        {
+            interpolatorReadIndex = rightPoint - 4;
+        }
+    }
+
+    // Push points until distance satisfied
+    while (gpi.WantsInput(meters)) {
+        if (interpolatorReadIndex < Points.count()) {
+            const ErgFilePoint * pii = &(Points.at(interpolatorReadIndex));
+            geolocation geo(pii->lat, pii->lon, pii->y);
+            gpi.Push(pii->x, geo);
+
+            interpolatorReadIndex++;
+        }
+        else {
+            gpi.NotifyInputComplete();
+            break;
+        }
+    }
+
+    geoLoc = gpi.Interpolate(meters);
+
+    return true;
+}
+
 // Retrieve the offset for the start of next lap.
 // Params: x - current workout distance (m) / time (ms)
 // Returns: distance (m) / time (ms) offset for next lap.
@@ -1068,7 +1280,7 @@ ErgFile::calculateMetrics()
     // is it valid?
     if (!isValid()) return;
 
-    if (format == CRS) {
+    if (format == CRS || format == CRS_LOC) {
 
         ErgFilePoint last;
         bool first = true;

src/Train/ErgFile.h
@@ -32,6 +32,7 @@
 #include <QTextEdit>
 #include <QRegExp>
 #include "Zones.h"      // For zones ... see below vvvv
+#include "LocationInterpolation.h"
 
 // which section of the file are we in?
 #define NOMANSLAND  0
@@ -43,18 +44,22 @@
 #define ERG     1
 #define MRC     2
 #define CRS     3
+#define CRS_LOC 4
 
 class ErgFilePoint
 {
     public:
 
-        ErgFilePoint() : x(0), y(0), val(0) {}
-        ErgFilePoint(double x, double y, double val) : x(x), y(y), val(val) {}
+        ErgFilePoint() : x(0), y(0), val(0), lat(0), lon(0) {}
+        ErgFilePoint(double x_, double y_, double val_) : x(x_), y(y_), val(val_), lat(0), lon(0) {}
+        ErgFilePoint(double x_, double y_, double val_, double lat_, double lon_) : x(x_), y(y_), val(val_), lat(lat_), lon(lon_) {}
 
         double x;     // x axis - time in msecs or distance in meters
         double y;     // y axis - load in watts or altitude
 
         double val;   // the value to send to the device (watts/gradient)
+
+        double lat, lon; // location (alt is y, above)
 };
 
 class ErgFileSection
@@ -104,14 +109,16 @@ class ErgFile
         void reload();          // reload after messed about
 
         void parseComputrainer(QString p = ""); // its an erg,crs or mrc file
-        void parseTacx();         // its a pgmf file
-        void parseZwift();         // its a zwo file (zwift xml)
+        void parseTacx();       // its a pgmf file
+        void parseZwift();      // its a zwo file (zwift xml)
+        void parseGpx();        // its a gpx...
 
         bool isValid();         // is the file valid or not?
         double Cp;
-        int format;             // ERG, CRS or MRC currently supported
+        int format;             // ERG, CRS, MRC and CRS_LOC currently supported
         int wattsAt(long, int&);      // return the watts value for the passed msec
         double gradientAt(long, int&);      // return the gradient value for the passed meter
+        bool locationAt(long x, int& lapnum, geolocation &geoLoc); // location at meter
 
         int nextLap(long);      // return the start value (erg - time(ms) or slope - distance(m)) for the next lap
         int currentLap(long);   // return the start value (erg - time(ms) or slope - distance(m)) for the current lap
@@ -129,19 +136,21 @@ class ErgFile
                 ErgDBId,        // if downloaded from ergdb
                 Source;         // where did this come from
         QStringList Tags;       // tagged strings
-                
+
         long    Duration;       // Duration of this workout in msecs
         int     Ftp;            // FTP this file was targetted at
         int     MaxWatts;       // maxWatts in this ergfile (scaling)
         bool valid;             // did it parse ok?
         int mode;
 
-
-        int leftPoint, rightPoint;            // current points we are between
+        int leftPoint, rightPoint;     // current points we are between
+        int interpolatorReadIndex;     // next point to be fed to interpolator
 
         QList<ErgFilePoint> Points;    // points in workout
         QList<ErgFileLap>   Laps;      // interval markers in the file
         QList<ErgFileText>  Texts;     // texts to display
+
+        GeoPointInterpolator gpi; // Location interpolator
 
         void calculateMetrics(); // calculate IsoPower value for ErgFile
 

src/Train/ErgFilePlot.cpp
@@ -24,7 +24,7 @@
 // Bridge between QwtPlot and ErgFile to avoid having to
 // create a separate array for the ergfile data, we plot
 // directly from the ErgFile points array
-double ErgFileData::x(size_t i) const { 
+double ErgFileData::x(size_t i) const {
     if (context->currentErgFile()) return context->currentErgFile()->Points.at(i).x;
     else return 0;
 }
@@ -247,7 +247,7 @@ ErgFilePlot::ErgFilePlot(Context *context) : context(context)
     CPMarker->setYAxis(QwtPlot::yLeft);
     CPMarker->setYValue(274);
     CPMarker->attach(this);
-    
+
 
     // Now pointer
     NowCurve = new QwtPlotCurve("Now");
@@ -310,7 +310,7 @@ ErgFilePlot::setData(ErgFile *ergfile)
     if (ergfile) {
 
         // is this by distance or time?
-        bydist = (ergfile->format == CRS) ? true : false;
+        bydist = (ergfile->format == CRS || ergfile->format == CRS_LOC) ? true : false;
 
         if (bydist == true) {
 

src/Train/RealtimeData.cpp
@@ -29,6 +29,7 @@ RealtimeData::RealtimeData()
     lap = msecs = lapMsecs = lapMsecsRemaining = ergMsecsRemaining = 0;
     thb = smo2 = o2hb = hhb = 0.0;
     lrbalance = rte = lte = lps = rps = 0.0;
+    latitude = longitude = altitude = 0.0;
     trainerStatusAvailable = false;
     trainerReady = true;
     trainerRunning = true;
@@ -384,6 +385,15 @@ double RealtimeData::value(DataSeries series) const
     case Slope: return slope;
         break;
 
+    case Latitude: return latitude;
+        break;
+
+    case Longitude: return longitude;
+        break;
+
+    case Altitude: return altitude;
+        break;
+
     case None:
     default:
         return 0;
@@ -444,6 +454,9 @@ const QList<RealtimeData::DataSeries> &RealtimeData::listDataSeries()
         seriesList << LapDistance;
         seriesList << LapDistanceRemaining;
         seriesList << ErgTimeRemaining;
+        seriesList << Latitude;
+        seriesList << Longitude;
+        seriesList << Altitude;
     }
     return seriesList;
 }
@@ -584,6 +597,15 @@ QString RealtimeData::seriesName(DataSeries series)
 
     case LapDistanceRemaining: return tr("Lap Distance Remaining");
         break;
+
+    case Latitude: return tr("Latitude");
+        break;
+
+    case Longitude: return tr("Longitude");
+        break;
+
+    case Altitude: return tr("Altitude");
+        break;
     }
 }
 
@@ -617,3 +639,11 @@ long RealtimeData::getLap() const
 {
     return lap;
 }
+
+double RealtimeData::getLatitude() const { return latitude; }
+double RealtimeData::getLongitude() const { return longitude; }
+double RealtimeData::getAltitude() const { return altitude; }
+
+void RealtimeData::setLatitude(double d) { latitude = d; }
+void RealtimeData::setLongitude(double d) { longitude = d; }
+void RealtimeData::setAltitude(double d) { altitude = d; }

commit 20c975e7dff774e9faea020be75b496232f4b081

Don't call locationAt/gradientAt in Erg mode
Since they reset the brackets, defeating their use by wattsAt and
forcing all searches start from the beggining, and generating
problems with ErgTimeRemaining, which depends on rightPoint.
This problems was introduced by 1402f6ad6a87fa80474749f0eed70bbe6d0cf674
Fixes #3491

src/Train/TrainSidebar.cpp
@@ -1742,7 +1742,7 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
 
                 // Trust ergFile for location data, if available.
                 bool fAltitudeSet = false;
-                if (ergFile) {
+                if (!(status&RT_MODE_ERGO) && ergFile) {
                     if (!ergFile->StrictGradient) {
                         // Attempt to obtain location and derived slope from altitude in ergfile.
                         geolocation geoloc;