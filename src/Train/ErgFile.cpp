5c14300a9f6bffcf342d566bb1521c7eaf5f30ae
Eric Christoffersen <46055145+ericchristoffersen@users.noreply.github.com>
2020-11-25 14:52:58 -0800

    Fix 3680: Text, Chim and Lap Issues

--- src/Train/ErgFile.cpp
+++ src/Train/ErgFile.cpp
@@ -1007,6 +1007,7 @@ void ErgFile::parseTTS()
         // Segment Start
         add.x = segment.startKM * 1000.;
         add.LapNum = (i * 2) + 1;
+        add.LapRangeNum = i + 1;
         add.selected = false;
 
         add.name = QObject::tr("Starting") + ": " + QString::fromStdWString(segment.name + rangeString);
@@ -1016,6 +1017,7 @@ void ErgFile::parseTTS()
         // Segment End
         add.x = segment.endKM * 1000.;
         add.LapNum = (i * 2) + 2;
+        add.LapRangeNum = i + 1;
         add.selected = false;
         add.name = QObject::tr("Ending") + ": " + QString::fromStdWString(segment.name + rangeString);
         
@@ -1044,7 +1046,7 @@ void ErgFile::parseTTS()
     int xx = 0;
     qDebug() << "LAPS:";
     for (const auto &a : Laps) {
-        qDebug() << xx << ": " << a.LapNum << " start:" << a.x / 1000. << "km, name: " << a.name;
+        qDebug() << xx << ": " << a.LapNum << " start:" << a.x / 1000. << "km, name: " << a.name << " RangeId:" << a.LapRangeNum;
         xx++;
     }
 
@@ -1591,7 +1593,7 @@ ErgFile::addNewLap(double loc) const
         sortLaps();
 
         auto itr = std::find_if(Laps.begin(), Laps.end(), [&add](const ErgFileLap& otherLap) {
-            return add.x == otherLap.x && add.name == otherLap.name;
+            return add.x == otherLap.x && add.LapRangeNum == otherLap.LapRangeNum && add.name == otherLap.name;
         });
         if (itr != Laps.end()) 
             return (*itr).LapNum;
