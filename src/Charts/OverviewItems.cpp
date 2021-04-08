2a8678d416f93fff9ef7366c230557a5eee7d498
Ale Martinez <amtriathlon@gmail.com>
2021-04-08 19:03:41 -0300

    Overview Metric Tile - Enable decorations for Pace related metrics
    For Pace related metrics converting back from toString using toDouble
    fails and then sparkline, up/down, etc. are not displayed.
    This change avoids that conversion to fix this issue and to make code simpler.

--- src/Charts/OverviewItems.cpp
+++ src/Charts/OverviewItems.cpp
@@ -569,7 +569,7 @@ MetricOverviewItem::setData(RideItem *item)
     // get the metric value
     value = item->getStringForSymbol(symbol, GlobalContext::context()->useMetricUnits);
     if (value == "nan") value ="";
-    double v = (units == tr("seconds")) ? item->getForSymbol(symbol, GlobalContext::context()->useMetricUnits) : value.toDouble();
+    double v = item->getForSymbol(symbol, GlobalContext::context()->useMetricUnits);
     if (std::isinf(v) || std::isnan(v)) v=0;
 
     points << QPointF(SPARKDAYS, v);
@@ -593,15 +593,7 @@ MetricOverviewItem::setData(RideItem *item)
         // only activities with matching sport flags
         if (prior->isRun == item->isRun && prior->isSwim == item->isSwim) {
 
-            double v;
-
-            if (units == tr("seconds")) v = prior->getForSymbol(symbol, GlobalContext::context()->useMetricUnits);
-            else {
-                QString vs = prior->getStringForSymbol(symbol, GlobalContext::context()->useMetricUnits);
-                if (vs == "nan") vs="0";
-                v = vs.toDouble();
-            }
-
+            double v = prior->getForSymbol(symbol, GlobalContext::context()->useMetricUnits);
             if (std::isinf(v) || std::isnan(v)) v=0;
 
             // new no zero value
