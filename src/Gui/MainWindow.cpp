GoldenCheetah/GoldenCheetah

commit c15d1bc33b56e73b45e886c1b717e1fed57db9b3

Add What's This to Help Menu and Toolbar
To make context help easier to access and promote the use of the wiki.

src/Gui/MainWindow.cpp
@@ -336,8 +336,19 @@ MainWindow::MainWindow(const QDir &home)
                                 "              padding-top:  0px; padding-bottom: 0px; }");
         sidelist->setStyleSheet(nopad);
         lowbar->setStyleSheet(nopad);
+        whatsthis->setStyleSheet(nopad);
     }
 #endif
+
+    // What's this button
+    whatsthis = new QPushButton(this);
+    whatsthis->setIcon(myHelper->icon());
+    whatsthis->setFixedHeight(24 * dpiYFactor);
+    whatsthis->setIconSize(isize);
+    whatsthis->setStyle(toolStyle);
+    whatsthis->setToolTip(tr("What's This?"));
+    whatsthis->setPalette(metal);
+    connect(whatsthis, SIGNAL(clicked(bool)), this, SLOT(enterWhatsThisMode()));
 
     // add a search box on far right, but with a little space too
     searchBox = new SearchFilterBox(this,context,false);
@@ -357,6 +368,7 @@ MainWindow::MainWindow(const QDir &home)
     head->addWidget(sidelist);
     head->addWidget(lowbar);
     head->addWidget(styleSelector);
+    head->addWidget(whatsthis);
     head->setFixedHeight(searchBox->height() + (16 *dpiXFactor));
 
     connect(searchBox, SIGNAL(searchResults(QStringList)), this, SLOT(setFilter(QStringList)));
@@ -656,6 +668,7 @@ MainWindow::MainWindow(const QDir &home)
     // HELP MENU
     QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
     helpMenu->addAction(tr("&Help Overview"), this, SLOT(helpWindow()));
+    helpMenu->addAction(myHelper);
     helpMenu->addSeparator();
     helpMenu->addAction(tr("&User Guide"), this, SLOT(helpView()));
     helpMenu->addAction(tr("&Log a bug or feature request"), this, SLOT(logBug()));
@@ -801,6 +814,12 @@ MainWindow::showLowbar(bool want)
     if (currentTab->hasBottom()) currentTab->setBottomRequested(want);
     showhideLowbar->setChecked(want);
     setToolButtons();
+}
+
+void
+MainWindow::enterWhatsThisMode()
+{
+    QWhatsThis::enterWhatsThisMode();
 }
 
 void

src/Gui/MainWindow.h
@@ -205,6 +205,7 @@ class MainWindow : public QMainWindow
         void setStyleFromSegment(int); // special case for linux/win qtsegmentcontrol toggline
         void toggleLowbar();
         void showLowbar(bool want);
+        void enterWhatsThisMode();
 
         // Analysis View
         void showEstimateCP();
@@ -289,7 +290,7 @@ class MainWindow : public QMainWindow
         SearchFilterBox *searchBox;
 
         // Not on Mac so use other types
-        QPushButton *sidelist, *lowbar;
+        QPushButton *sidelist, *lowbar, *whatsthis;
         QPushButton *back, *forward;
         QtSegmentControl *styleSelector;
         GcToolBar *head;

commit 133364e5972a1faa6cb8d5643e71c316bbeeaed7

Fix startup crash on Windows/Linux with HighDPI monitors
The problem is setStyle being applied before button creation...
This bug was introduced in c15d1bc, sorry.

src/Gui/MainWindow.cpp
@@ -332,6 +332,16 @@ MainWindow::MainWindow(const QDir &home)
     styleSelector->setPalette(metal);
     connect(styleSelector, SIGNAL(segmentSelected(int)), this, SLOT(setStyleFromSegment(int))); //avoid toggle infinitely
 
+    // What's this button
+    whatsthis = new QPushButton(this);
+    whatsthis->setIcon(myHelper->icon());
+    whatsthis->setFixedHeight(24 * dpiYFactor);
+    whatsthis->setIconSize(isize);
+    whatsthis->setStyle(toolStyle);
+    whatsthis->setToolTip(tr("What's This?"));
+    whatsthis->setPalette(metal);
+    connect(whatsthis, SIGNAL(clicked(bool)), this, SLOT(enterWhatsThisMode()));
+
  #if defined(WIN32) || defined (Q_OS_LINUX)
     // are we in hidpi mode? if so undo global defaults for toolbar pushbuttons
     if (dpiXFactor > 1) {
@@ -342,16 +352,6 @@ MainWindow::MainWindow(const QDir &home)
         whatsthis->setStyleSheet(nopad);
     }
 #endif
-
-    // What's this button
-    whatsthis = new QPushButton(this);
-    whatsthis->setIcon(myHelper->icon());
-    whatsthis->setFixedHeight(24 * dpiYFactor);
-    whatsthis->setIconSize(isize);
-    whatsthis->setStyle(toolStyle);
-    whatsthis->setToolTip(tr("What's This?"));
-    whatsthis->setPalette(metal);
-    connect(whatsthis, SIGNAL(clicked(bool)), this, SLOT(enterWhatsThisMode()));
 
     // add a search box on far right, but with a little space too
     searchBox = new SearchFilterBox(this,context,false);