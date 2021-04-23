96c8508da291a42f65694027ceedbf2f791be8a3
Ale Martinez <amtriathlon@gmail.com>
2021-04-23 19:55:51 -0300

    Get Equipment name on Strava download
    When the downloaded activity has gear_id, details are retrieved
    and name is added to Equipment metadata field.
    [publish binaries]

--- src/Cloud/Strava.h
+++ src/Cloud/Strava.h
@@ -77,6 +77,7 @@ class Strava : public CloudService {
         QByteArray* prepareResponse(QByteArray* data);
 
         void addSamples(RideFile* ret, QString remoteid);
+        void addEquipment(RideFile* ret, QString gear_id);
         void fixLapSwim(RideFile* ret, QJsonArray laps);
         void fixSmartRecording(RideFile* ret);
 
