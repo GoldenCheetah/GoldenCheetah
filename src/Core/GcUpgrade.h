73a354156d6b53847719b03d854ff6942cce36d3
Mark Liversedge <liversedge@gmail.com>
2021-07-09 14:37:30 +0100

    VERSION 3.6 DEVELOPMENT BUILD JULY 2021
    .. marking up for release of a new development build.
    
    [publish binaries]

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -98,6 +98,7 @@
 // 4002 - V3.6 DEVELOPMENT 2009 (SEP 2020)
 // 4003 - V3.6 DEVELOPMENT 2101 (JAN 2021)
 // 4004 - V3.6 DEVELOPMENT 2106 (JUN 2021)
+// 4005 - V3.6 DEVELOPMENT 2107 (JUL 2021)
 
 #define VERSION3_BUILD    3010 // released
 #define VERSION3_SP1      3030 // released
@@ -108,14 +109,14 @@
 #define VERSION33_BUILD   3933 // development release
 #define VERSION34_BUILD   3955 // released
 #define VERSION35_BUILD   3990 // released
-#define VERSION36_BUILD   4004 // development build June 2020
+#define VERSION36_BUILD   4005 // development build July 2021
 
 // will keep changing during testing and before final release
 #define VERSION31_BUILD VERSION31_UPG
 
 // the next two will with each build/release
-#define VERSION_LATEST 4004
-#define VERSION_STRING "V3.6-DEV2106"
+#define VERSION_LATEST 4005
+#define VERSION_STRING "V3.6-DEV2107"
 
 // default config for this release cycle
 #define VERSION_CONFIG_PREFIX "http://www.goldencheetah.org/defaults/3.5"
