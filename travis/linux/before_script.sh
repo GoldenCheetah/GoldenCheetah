cc3f2911135ab42f2ec2ca17b35d99376f4fb02c
Alejandro Martinez <amtriathlon@gmail.com>
2021-08-12 13:36:04 -0300

    Add encrypted API Key for RideWithGPS to Travis-ci builds [skip AppVeyor] To enable #3371 for Linux and macOS builds

--- travis/linux/before_script.sh
+++ travis/linux/before_script.sh
@@ -61,6 +61,7 @@ sed -i "s/__GC_NOKIA_CLIENT_SECRET__/"$GC_NOKIA_CLIENT_SECRET"/" src/Core/Secret
 sed -i "s/__GC_SPORTTRACKS_CLIENT_SECRET__/"$GC_SPORTTRACKS_CLIENT_SECRET"/" src/Core/Secrets.h
 sed -i "s/OPENDATA_DISABLE/OPENDATA_ENABLE/" src/Core/Secrets.h
 sed -i "s/__GC_CLOUD_OPENDATA_SECRET__/"$GC_CLOUD_OPENDATA_SECRET"/" src/Core/Secrets.h
+sed -i "s/__GC_RWGPS_API_KEY__/"$GC_RWGPS_API_KEY"/" src/Core/Secrets.h
 cat src/gcconfig.pri
 # update translations
 lupdate src/src.pro
--- travis/osx/before_script.sh
+++ travis/osx/before_script.sh
@@ -82,6 +82,7 @@ sed -i "" "s/__GC_NOKIA_CLIENT_SECRET__/"$GC_NOKIA_CLIENT_SECRET"/" src/Core/Sec
 sed -i "" "s/__GC_SPORTTRACKS_CLIENT_SECRET__/"$GC_SPORTTRACKS_CLIENT_SECRET"/" src/Core/Secrets.h
 sed -i "" "s/OPENDATA_DISABLE/OPENDATA_ENABLE/" src/Core/Secrets.h
 sed -i "" "s/__GC_CLOUD_OPENDATA_SECRET__/"$GC_CLOUD_OPENDATA_SECRET"/" src/Core/Secrets.h
+sed -i "" "s/__GC_RWGPS_API_KEY__/"$GC_RWGPS_API_KEY"/" src/Core/Secrets.h
 cat src/gcconfig.pri
 # update translations
 /usr/local/opt/qt5/bin/lupdate src/src.pro
