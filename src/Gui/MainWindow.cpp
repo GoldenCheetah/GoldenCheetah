GoldenCheetah/GoldenCheetah

commit 91f2c46c3e73e1dd5198abb3e52d63ff7c3ab3d6

Athlete/View switch update perspective selector
.. along the way renamed AthleteTab related methods in MainWindow
   to reflect the last commit renaming the classes.

.. there are also a handful of fixups to SEGV when no ride is
   selected in DataFilter (triggered by opening a second athlete
   and switching to trends view, which need to recreate the athlete
   switch bug that is also part of #3997).

.. and the logic to reset perspectives is changed in MainWindow
   with a special method resetPerspective that is called everywhere
   but will check the athlete/view combination has not already
   been set (to avoid multiple passes).

.. multi-athlete and perspectives need better testing as there are
   probably more SEGV in there, and if we fix them we could also
   remove the requirement for the opening view to always be Analysis.

Fixes #3997.

src/Gui/MainWindow.h
@@ -94,7 +94,7 @@ class MainWindow : public QMainWindow
         int loading;
 
         // currently selected tab
-        AthleteTab *athleteTab() { return currentTab; }
+        AthleteTab *athleteTab() { return currentAthleteTab; }
         NewSideBar *newSidebar() { return sidebar; }
 
         // tab view keeps this up to date
@@ -109,8 +109,8 @@ class MainWindow : public QMainWindow
         // have already been opened
         friend class ::ChooseCyclistDialog;
         friend class ::AthleteLoader;
-        QMap<QString,AthleteTab*> tabs;
-        AthleteTab *currentTab;
+        QMap<QString,AthleteTab*> athletetabs;
+        AthleteTab *currentAthleteTab;
         QList<AthleteTab*> tabList;
 
         virtual void resizeEvent(QResizeEvent*);
@@ -147,6 +147,7 @@ class MainWindow : public QMainWindow
         // perspective selected
         void perspectiveSelected(int index);
         void perspectivesChanged(); // when the list of perspectives is updated in PerspectivesDialog
+        void resetPerspective(int view); // reset when view changes
 
         // import and export perspectives
         void exportPerspective();
@@ -160,14 +161,14 @@ class MainWindow : public QMainWindow
 
         void setOpenTabMenu(); // set the Open Tab menu
         void newCyclistTab();  // create a new Cyclist
-        void openTab(QString name);
+        void openAthleteTab(QString name);
         void loadCompleted(QString name, Context *context);
         void closeTabClicked(int index); // user clicked to close tab
-        bool closeTab(QString name); // close named athlete
-        bool closeTab();       // close current, might not if the user
+        bool closeAthleteTab(QString name); // close named athlete
+        bool closeAthleteTab();       // close current, might not if the user
                                // changes mind if there are unsaved changes.
-        void removeTab(AthleteTab*);  // remove without question
-        void switchTab(int index); // for switching between one tab and another
+        void removeAthleteTab(AthleteTab*);  // remove without question
+        void switchAthleteTab(int index); // for switching between one tab and another
 
         // sidebar selecting views and actions
         void sidebarClicked(int id);
@@ -190,7 +191,6 @@ class MainWindow : public QMainWindow
         void selectDiary();
         void selectAnalysis();
         void selectTrain();
-        void selectInterval();
 
         void setChartMenu();
         void setSubChartMenu();

src/Core/DataFilter.cpp
@@ -3480,6 +3480,9 @@ Result Leaf::eval(DataFilterRuntime *df, Leaf *leaf, const Result &x, long it, R
 
         if (leaf->function == "daterange") {
 
+            // cannot get a context via ride as none selected or available
+            if (m == NULL) return Result(0);
+
             // sets the daterange for the expression, a bit like a closure
             // so we don't have to add parameters to functions that do things
             // differently when working in trends view. e.g. measures, metrics etc
@@ -4365,8 +4368,10 @@ Result Leaf::eval(DataFilterRuntime *df, Leaf *leaf, const Result &x, long it, R
             if (wantstrings) returning.isNumber=false;
 
             FilterSet fs;
-            fs.addFilter(m->context->isfiltered, m->context->filters);
-            fs.addFilter(m->context->ishomefiltered, m->context->homeFilters);
+            if (m) {
+                fs.addFilter(m->context->isfiltered, m->context->filters);
+                fs.addFilter(m->context->ishomefiltered, m->context->homeFilters);
+            }
             Specification spec;
             spec.setFilterSet(fs);
 
@@ -4395,6 +4400,9 @@ Result Leaf::eval(DataFilterRuntime *df, Leaf *leaf, const Result &x, long it, R
             } else {
                 spec.setDateRange(d); // fallback to daterange selected
             }
+
+            // no ride selected, or none available
+            if (m == NULL) return Result(0);
 
             // loop through rides for daterange
             int count=0;
@@ -4474,6 +4482,9 @@ Result Leaf::eval(DataFilterRuntime *df, Leaf *leaf, const Result &x, long it, R
             } else {
                 currentride = true;
             }
+
+            // no rides available or selected
+            if (m == NULL) return Result(0);
 
             // loop through rides for daterange or currentride
             foreach(RideItem *ride, m->context->athlete->rideCache->rides()) {
@@ -6262,6 +6273,8 @@ Result Leaf::eval(DataFilterRuntime *df, Leaf *leaf, const Result &x, long it, R
                     } else {
 
                         Result returning(0);
+
+                        if (m == NULL) return returning; // no ride
 
                         // date range, returning a vector
                         foreach(PDEstimate pde, m->context->athlete->getPDEstimates()) {

src/Gui/AthleteTab.cpp
@@ -174,6 +174,8 @@ AthleteTab::view(int index)
 void
 AthleteTab::selectView(int index)
 {
+    if (views->currentIndex() == index) return; // not changing
+
     emit viewChanged(index);
 
     // first we deselect the current
@@ -184,6 +186,7 @@ AthleteTab::selectView(int index)
     view(index)->setSelected(true);
     masterControls->setCurrentIndex(index);
     context->setIndex(index);
+    context->mainWindow->resetPerspective(index); // set perspective for this view
 }
 
 void

src/Gui/AthleteView.cpp
@@ -209,8 +209,8 @@ AthleteCard::configAthlete()
 void
 AthleteCard::clicked()
 {
-    if (loadprogress==100) parent->context->mainWindow->closeTab(path);
-    else parent->context->mainWindow->openTab(path);
+    if (loadprogress==100) parent->context->mainWindow->closeAthleteTab(path);
+    else parent->context->mainWindow->openAthleteTab(path);
 }
 
 void

src/Gui/ChooseCyclistDialog.cpp
@@ -118,7 +118,7 @@ ChooseCyclistDialog::getList()
 
         // only allow selection of cyclists which are not already open
         foreach (MainWindow *x, mainwindows) {
-            QMapIterator<QString, AthleteTab*> t(x->tabs);
+            QMapIterator<QString, AthleteTab*> t(x->athletetabs);
             while (t.hasNext()) {
                 t.next();
                 if (t.key() == name)

commit d449933ab1dd31fa02a80b9984c6df1b0092540b

Refresh Perspective Selector on Add/Delete
.. commit 91f2c46 introduced a regression where the selector
   is not updated when the perspectives are changed (via
   the manage/add perspective functions).

.. this was because resetPerspective() returned if the
   athlete/view combination was last used to set the
   selector -- we now override this, but only when the
   perspectives config is changed.

src/Gui/MainWindow.cpp
@@ -1437,12 +1437,12 @@ MainWindow::switchPerspective(int index)
 }
 
 void
-MainWindow::resetPerspective(int view)
+MainWindow::resetPerspective(int view, bool force)
 {
     static AthleteTab *lastathlete=NULL;
     static int lastview=-1;
 
-    if (lastview == view && lastathlete == currentAthleteTab) return;
+    if (!force && lastview == view && lastathlete == currentAthleteTab) return;
 
     // remember who last updated it.
     lastathlete = currentAthleteTab;
@@ -1549,8 +1549,10 @@ MainWindow::perspectivesChanged()
     // which perspective is currently being selected (before we go setting the combobox)
     Perspective *prior = current->perspective_;
 
-    // ok, so reset the combobox
-    resetPerspective(view);
+    // ok, so reset the combobox and force, since whilst it may have already
+    // been set for this athlete+view combination the config was just changed
+    // so it needs to be redone.
+    resetPerspective(view, true);
     //current->setPerspectives(perspectiveSelector);
 
     // is the old selected perspective still available?

src/Gui/MainWindow.h
@@ -147,7 +147,7 @@ class MainWindow : public QMainWindow
         // perspective selected
         void perspectiveSelected(int index);
         void perspectivesChanged(); // when the list of perspectives is updated in PerspectivesDialog
-        void resetPerspective(int view); // reset when view changes
+        void resetPerspective(int view, bool force=false); // reset when view changes
 
         // import and export perspectives
         void exportPerspective();