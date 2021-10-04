f0f5274507c76edad00a32c8f777df93c181d736
Erik Botö <erik.boto@gmail.com>
2021-10-04 12:20:10 +0200

    travis/linux: use --no-verbose on all wget commands
    Use --no-verbose in order to avoid having a lot of wget progress
    information in the travis logs, since the logs seems to be sometimes
    becoming larger than 4 MB which results in aborted jobs.

--- travis/linux/after_success.sh
+++ travis/linux/after_success.sh
@@ -46,7 +46,7 @@ chmod a+x linuxdeployqt-7-x86_64.AppImage
 ./linuxdeployqt-7-x86_64.AppImage appdir/GoldenCheetah -verbose=2 -bundle-non-qt-libs -exclude-libs=libqsqlmysql,libqsqlpsql,libnss3,libnssutil3,libxcb-dri3.so.0 -unsupported-allow-new-glibc
 
 # Add Python and core modules
-wget https://github.com/niess/python-appimage/releases/download/python3.7/python3.7.12-cp37-cp37m-manylinux1_x86_64.AppImage
+wget --no-verbose https://github.com/niess/python-appimage/releases/download/python3.7/python3.7.12-cp37-cp37m-manylinux1_x86_64.AppImage
 chmod +x python3.7.12-cp37-cp37m-manylinux1_x86_64.AppImage
 ./python3.7.12-cp37-cp37m-manylinux1_x86_64.AppImage --appimage-extract
 rm -f python3.7.12-cp37-cp37m-manylinux1_x86_64.AppImage
@@ -58,7 +58,7 @@ mv squashfs-root/opt appdir/opt
 rm -rf squashfs-root
 
 # Generate AppImage
-wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+wget --no-verbose "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod a+x appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage appdir
 
