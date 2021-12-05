fa380d570c6fa9a6213953ec8bcb3677ce8e670b
Peret <19463568+peret2000@users.noreply.github.com>
2021-12-05 18:15:50 +0100

    Python automated processors fix: they can access all data of ride being accessed
    the context has the correct ride on it, and also, the ride has all its data refreshed, so GC.activityMetrics() can be called, and GC.setTag(), etc will work

--- src/FileIO/FixPyDataProcessor.cpp
+++ src/FileIO/FixPyDataProcessor.cpp
@@ -23,8 +23,21 @@ bool FixPyDataProcessor::postProcess(RideFile *rideFile, DataProcessorConfig *se
 
     QString errText;
     bool useNewThread = op != "PYTHON";
-    FixPyRunner pyRunner(nullptr, rideFile, useNewThread);
-    return pyRunner.run(pyScript->source, pyScript->iniKey, errText) == 0;
+
+
+    // Creating a RideItem object from rideFile, to substitute the current one in context object,
+    // so that activityMetrics() can access its data
+
+    QDateTime dt = rideFile->startTime();
+    RideItem *ride = new RideItem(rideFile, dt, rideFile->context);
+    RideItem *prior = rideFile->context->ride;
+    // It must be refreshed to have metadata, etc updated and accesible from within python code
+    ride->refresh();
+    rideFile->context->ride = ride;
+    FixPyRunner pyRunner(rideFile->context, rideFile, useNewThread);
+    bool result = pyRunner.run(pyScript->source, pyScript->iniKey, errText) == 0;
+    rideFile->context->ride = prior;
+    return result;
 }
 
 DataProcessorConfig *FixPyDataProcessor::processorConfig(QWidget *parent, const RideFile* ride)
