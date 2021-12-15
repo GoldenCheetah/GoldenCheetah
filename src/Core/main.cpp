GoldenCheetah/GoldenCheetah

commit 7b93b2a533a978d54f6cd206a545e743d123a2fc

Enable translation for Color Selector and Add Tile Wizard
registerItems is now a static member of OverviewItemConfig called from main,
and GCColor::setupColors() is called a second time, both after the translator
is installed to enable translations.
Fixes #4089

src/Core/main.cpp
@@ -28,6 +28,7 @@
 #include "IdleTimer.h"
 #include "PowerProfile.h"
 #include "GcCrashDialog.h" // for versionHTML
+#include "OverviewItems.h"
 
 #include <QApplication>
 #include <QDesktopWidget>
@@ -637,11 +638,17 @@ main(int argc, char *argv[])
             gcTranslator.load(":translations" + translation_file);
         application->installTranslator(&gcTranslator);
 
+        // Now the translator is installed, set default colors with translated names
+        GCColor::setupColors();
+
         // Initialize metrics once the translator is installed
         RideMetricFactory::instance().initialize();
 
         // Initialize global registry once the translator is installed
         GcWindowRegistry::initialize();
+
+        // initialize Overview Items once the translator is installed
+        OverviewItemConfig::registerItems();
 
         // initialise the trainDB
         trainDB = new TrainDB(home);

src/Charts/OverviewItems.cpp
@@ -48,7 +48,8 @@
 #include <QJsonArray>
 #include <QJsonValue>
 
-static bool _registerItems()
+bool
+OverviewItemConfig::registerItems()
 {
     // get the factory
     ChartSpaceItemRegistry &registry = ChartSpaceItemRegistry::instance();
@@ -70,7 +71,6 @@ static bool _registerItems()
 
     return true;
 }
-static bool registered = _registerItems();
 
 static void setFilter(ChartSpaceItem *item, Specification &spec)
 {

src/Charts/OverviewItems.h
@@ -63,6 +63,8 @@ class OverviewItemConfig : public QWidget
         OverviewItemConfig(ChartSpaceItem *);
         ~OverviewItemConfig();
 
+        static bool registerItems();
+
     public slots:
 
         // retrieve values when user edits them (if they're valid)

commit d7e9e305c542050f6eb62382c4ad8bc98724e42e

Fixup color initialisation
.. color initialisation was broken by 7b93b2a5

src/Core/main.cpp
@@ -439,11 +439,6 @@ main(int argc, char *argv[])
     // read defaults
     initPowerProfile();
 
-    // set default colors
-    GCColor::setupColors();
-    appsettings->migrateQSettingsSystem(); // colors must be setup before migration can take place, but reading has to be from the migrated ones
-    GCColor::readConfig();
-
     // output colors as configured so we can cut and paste into Colors.cpp
     // uncomment when developers working on theme colors
     //GCColor::dumpColors();
@@ -640,6 +635,10 @@ main(int argc, char *argv[])
 
         // Now the translator is installed, set default colors with translated names
         GCColor::setupColors();
+
+        // migration
+        appsettings->migrateQSettingsSystem(); // colors must be setup before migration can take place, but reading has to be from the migrated ones
+        GCColor::readConfig();
 
         // Initialize metrics once the translator is installed
         RideMetricFactory::instance().initialize();