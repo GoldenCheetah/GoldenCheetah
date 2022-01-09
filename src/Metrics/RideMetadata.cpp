GoldenCheetah/GoldenCheetah

commit e0ec5ba2d1c780a12ab0060f080a414b3b915ce0

Tweak Metadata Styling on MacOS
.. removes spin boxes and border around fields

src/Metrics/RideMetadata.cpp
@@ -412,6 +412,13 @@ RideMetadata::configChanged(qint32)
         // tab bar look reasonably modern
         QString styling = QString("QTabWidget { background: %1; }"
                               "QTabWidget::pane { border: 0px; }"
+#ifdef Q_OS_MAC
+                              "QLineEdit { border: 0px; background-color: %7; color: %6; }"
+                              "QDateEdit { border: 0px; background-color: %7; color: %6; }"
+                              "QTimeEdit { border: 0px; background-color: %7; color: %6; }"
+                              "QSpinBox { border: 0px; background-color: %7; color: %6; }"
+                              "QDoubleSpinBox { border: 0px; background-color: %7; color: %6; }"
+#endif
                               "QTabBar::tab { background: %1; "
                               "               color: %6; "
                               "               min-width: %5px; "
@@ -426,7 +433,8 @@ RideMetadata::configChanged(qint32)
                               .arg(4*dpiYFactor)                                          // 3 selected bar width
                               .arg(2*dpiXFactor)                                          // 4 padding
                               .arg(75*dpiXFactor)                                         // 5 tab minimum width
-                              .arg(GCColor::invertColor(GColor(CPLOTBACKGROUND)).name()); // 6 tab text color
+                              .arg(GCColor::invertColor(GColor(CPLOTBACKGROUND)).name())  // 6 tab text color
+                              .arg( GCColor::alternateColor(GColor(CPLOTBACKGROUND)).name()); // 7 lineedit background
         tabs->setStyleSheet(styling);
 
         metadataChanged(); // re-read the values!
@@ -695,9 +703,7 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
     case FIELD_INTEGER : // integer
         widget = new QSpinBox(this);
         //widget->setFixedHeight(18);
-        ((QSpinBox*)widget)->setSingleStep(1);
-        ((QSpinBox*)widget)->setMinimum(-99999999);
-        ((QSpinBox*)widget)->setMaximum(99999999);
+        ((QSpinBox*)widget)->setButtonSymbols(QAbstractSpinBox::NoButtons);
         connect (widget, SIGNAL(valueChanged(int)), this, SLOT(dataChanged()));
         connect (widget, SIGNAL(editingFinished()), this, SLOT(editFinished()));
         break;
@@ -705,9 +711,7 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
     case FIELD_DOUBLE : // double
         widget = new QDoubleSpinBox(this);
         //widget->setFixedHeight(18);
-        ((QDoubleSpinBox*)widget)->setSingleStep(0.01);
-        ((QDoubleSpinBox*)widget)->setMinimum(-999999.99);
-        ((QDoubleSpinBox*)widget)->setMaximum(999999.99);
+        ((QDoubleSpinBox*)widget)->setButtonSymbols(QAbstractSpinBox::NoButtons);
         if (GlobalContext::context()->specialFields.isMetric(field.name)) {
 
             enabled = new QCheckBox(this);
@@ -732,6 +736,7 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
 
     case FIELD_DATE : // date
         widget = new QDateEdit(this);
+        ((QDateEdit*)widget)->setButtonSymbols(QAbstractSpinBox::NoButtons);
         //widget->setFixedHeight(18);
         connect (widget, SIGNAL(dateChanged(const QDate)), this, SLOT(dataChanged()));
         connect (widget, SIGNAL(editingFinished()), this, SLOT(editFinished()));
@@ -739,6 +744,7 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
 
     case FIELD_TIME : // time
         widget = new QTimeEdit(this);
+        ((QTimeEdit*)widget)->setButtonSymbols(QAbstractSpinBox::NoButtons);
         //widget->setFixedHeight(18);
         ((QTimeEdit*)widget)->setDisplayFormat("hh:mm:ss");
         connect (widget, SIGNAL(timeChanged(const QTime)), this, SLOT(dataChanged()));

commit 50f5efbfdb3b9480973ce3e17c1719cfd9cef2bb

Resintate metadata numeric min/max limits
.. a regression introduced in e0ec5ba2d1c78 removed the limits
   for double and integer metadata fields.

Fixes #4125

src/Metrics/RideMetadata.cpp
@@ -705,7 +705,8 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
 
     case FIELD_INTEGER : // integer
         widget = new QSpinBox(this);
-        ((QSpinBox*)widget)->setMaximum(100);
+        ((QSpinBox*)widget)->setMinimum(-9999999);
+        ((QSpinBox*)widget)->setMaximum(9999999);
         ((QSpinBox*)widget)->setButtonSymbols(QAbstractSpinBox::NoButtons);
         connect (widget, SIGNAL(valueChanged(int)), this, SLOT(dataChanged()));
         connect (widget, SIGNAL(editingFinished()), this, SLOT(editFinished()));
@@ -715,6 +716,8 @@ FormField::FormField(FieldDefinition field, RideMetadata *meta) : definition(fie
         widget = new QDoubleSpinBox(this);
         //widget->setFixedHeight(18);
         ((QDoubleSpinBox*)widget)->setButtonSymbols(QAbstractSpinBox::NoButtons);
+        ((QDoubleSpinBox*)widget)->setMinimum(-9999999.99);
+        ((QDoubleSpinBox*)widget)->setMaximum(9999999.99);
         if (GlobalContext::context()->specialFields.isMetric(field.name)) {
 
             enabled = new QCheckBox(this);