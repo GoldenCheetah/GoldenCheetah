150155ba846dba1b2a6c9e95f85ccd17589c3d28
Mark Liversedge <liversedge@gmail.com>
2022-02-09 09:15:30 +0000

    VERSION 3.6 RELEASE CANDIDATE 1 FEB 2022
    .. start of release cycle, pushed out to get the recent
       Withings API update out.
    
       standard config still needs to be fixed up before
       we move to the final release
    
    [publish binaries]

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -102,6 +102,7 @@
 // 4007 - V3.6 DEVELOPMENT 2108 (AUG 2021)
 // 4008 - V3.6 DEVELOPMENT 2110 (OCT 2021) (Pre-release)
 // 4009 - V3.6 DEVELOPMENT 2110 (NOV 2021) (Pre-release)
+// 4010 - V3.6 DEVELOPMENT 2202 (FEB 2022) (Pre-release)
 
 
 #define VERSION3_BUILD    3010 // released
@@ -113,14 +114,14 @@
 #define VERSION33_BUILD   3933 // development release
 #define VERSION34_BUILD   3955 // released
 #define VERSION35_BUILD   3990 // released
-#define VERSION36_BUILD   4009 // testing upgrade processing
+#define VERSION36_BUILD   4010 // first release candidate
 
 // will keep changing during testing and before final release
 #define VERSION31_BUILD VERSION31_UPG
 
 // the next two will with each build/release
-#define VERSION_LATEST 4009
-#define VERSION_STRING "V3.6-BETA"
+#define VERSION_LATEST 4010
+#define VERSION_STRING "V3.6-RC1"
 
 // default config for this release cycle
 #define VERSION_CONFIG_PREFIX "http://www.goldencheetah.org/defaults/3.5"
