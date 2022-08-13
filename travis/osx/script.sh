02cab3029b5f0d514df9bf4a6c66b3b0767d44ea
Alejandro Martinez <amtriathlon@gmail.com>
2022-08-13 13:00:53 -0300

    Travis-ci - cache macOS app package [skip AppVeyor] To avoid rebuild when after_success is cancelled, so restarting the job can complete the packaging. Drawback is we need to clear the cache to rebuild [publish binaries]

--- travis/osx/script.sh
+++ travis/osx/script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ev
+# Build only if a package is not already cached
+if [ -z "$(ls -A src/GoldenCheetah.app)" ]; then
 CC=clang CXX=clang++ /usr/local/opt/qt5/bin/qmake -makefile -recursive QMAKE_CXXFLAGS_WARN_ON+="-Wno-unused-private-field -Wno-c++11-narrowing -Wno-deprecated-declarations -Wno-deprecated-register -Wno-nullability-completeness -Wno-sign-compare -Wno-inconsistent-missing-override" QMAKE_CFLAGS_WARN_ON+="-Wno-deprecated-declarations -Wno-sign-compare"
 CC=clang CXX=clang++ make qmake_all
 CC=clang CXX=clang++ make -j4 sub-qwt --silent
 CC=clang CXX=clang++ make -j4 sub-src --silent || CC=clang CXX=clang++ make sub-src
+fi
 exit
