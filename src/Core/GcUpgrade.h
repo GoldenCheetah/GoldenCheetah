d8dc986c1ed1943e2b338eb142eb61a38b4938d9
Mark Liversedge <liversedge@gmail.com>
2022-08-13 10:28:22 +0100

    VERSION 3.6 RELEASE CANDIDATE 2 AUGUST 2022
    .. second release candidate with no open defects in the
       backlog for 3.6 this is likely the last release candidate
       before we release (unless we find a major defect that
       needs to be corrected and tested).

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -102,7 +102,8 @@
 // 4007 - V3.6 DEVELOPMENT 2108 (AUG 2021)
 // 4008 - V3.6 DEVELOPMENT 2110 (OCT 2021) (Pre-release)
 // 4009 - V3.6 DEVELOPMENT 2110 (NOV 2021) (Pre-release)
-// 4010 - V3.6 DEVELOPMENT 2202 (FEB 2022) (Pre-release)
+// 4010 - V3.6 DEVELOPMENT 2202 (FEB 2022) (Pre-release RC1)
+// 4011 - V3.6 DEVELOPMENT 2208 (AUG 2022) (Pre-release RC2)
 
 
 #define VERSION3_BUILD    3010 // released
@@ -120,8 +121,8 @@
 #define VERSION31_BUILD VERSION31_UPG
 
 // the next two will with each build/release
-#define VERSION_LATEST 4010
-#define VERSION_STRING "V3.6-RC1"
+#define VERSION_LATEST 4011
+#define VERSION_STRING "V3.6-RC2"
 
 // default config for this release cycle
 #define VERSION_CONFIG_PREFIX "http://www.goldencheetah.org/defaults/3.6"
