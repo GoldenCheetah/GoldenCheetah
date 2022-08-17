8001421b1ee6fcfd8e612a6e8d302d2b497c8f25
Paul Johnson <paulj49457@gmail.com>
2022-08-17 21:43:45 +0100

    Corrects Athlete QTabBar tab colours
    Tidy up
    
    Updated to track NewSideBar colours
    
    Corrected for the case when the user has changed the colours so CCHROME & CTOOLBAR are different colours, they are the same when a Theme has been applied.

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -2535,15 +2535,33 @@ MainWindow::configChanged(qint32)
     whatsthis->setStyleSheet(buttonstyle);
 
     // All platforms
-    QPalette tabbarPalette;
     tabbar->setAutoFillBackground(true);
     tabbar->setShape(QTabBar::RoundedSouth);
     tabbar->setDrawBase(false);
 
-    tabbarPalette.setBrush(backgroundRole(), GColor(CTOOLBAR));
-    tabbarPalette.setBrush(foregroundRole(), GCColor::invertColor(GColor(CTOOLBAR)));
-    tabbar->setPalette(tabbarPalette);
-    athleteView->setPalette(tabbarPalette);
+    // *** Code below copied NewSideBarItem::configChanged(qint32)
+    
+     // if foreground is white then we're "dark" if its
+    // black the we're "light" so this controls palette
+    bool dark = (GCColor::invertColor(GColor(CTOOLBAR)) == QColor(Qt::white));
+    bool isblack = (GColor(CTOOLBAR) == QColor(Qt::black)); // e.g. mustang theme
+
+    // on select
+    QColor bg_select = GColor(CTOOLBAR);
+    if (dark) bg_select = bg_select.lighter(200);
+    else bg_select = bg_select.darker(200);
+    if (isblack) bg_select = QColor(30, 30, 30);
+    QColor fg_select = GCColor::invertColor(bg_select);
+
+    // *** Code above copied NewSideBarItem::configChanged(qint32)
+
+    tabbar->setStyleSheet(QString("QTabBar::tab { background-color: %1; color: %2;}"
+        "QTabBar::tab::selected { background-color: %3; color: %4; }").arg(GColor(CTOOLBAR).name())
+                                                                        .arg(GCColor::invertColor(GColor(CTOOLBAR)).name())
+                                                                        .arg(bg_select.name())
+                                                                        .arg(fg_select.name()));
+    tabbar->setDocumentMode(true);
+    athleteView->setPalette(tabbar->palette());
 
     head->updateGeometry();
     repaint();
