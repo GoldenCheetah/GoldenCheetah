bb972b7e8a53b7bf905b7a6448bf77436580e4be
Alejandro Martinez <amtriathlon@gmail.com>
2022-12-23 17:25:37 -0300

    Biweekly snapshot build
    [publish binaries]
    Cumulative changes from RC3
    018bd404e FTMS: Add support for Spin Down Calibration (#4073)
    4f97bae97 DOB - display using 4 digits year
    dfeb8532a macOS plist - Add required BTLE keys for newer versions
    2a99c8762 Python 3.7.15 AppImage is no longer available, upgrade to 3.7.16 (#4299)
    0296693dd Workout Chart - Imperial units support (#4232)
    8919eab5b Map Chart - Update default Tile Servers
    7d0e6de0b Bug Fixes for live map base url and remove erroneous debug message (#3901)
    1c5bbecf1 Train - Improve L/R Balance Dial display
    8e0b0a67a R/Python API - Sport case insensitive in Athlete Zones
    b08e662c9 Train - Show lap notifications only when available
    2c3eabdd8 RideCache - Fix refresh to inclu=de the oldest activity
    9c7d1d4f3 Strava Sync - Tag simulation rides as VirtualRide
    53f8f5102 Fix memory leak in Python API - part 2
    e477b05a9 ErgDB Download - Decode title to be used as name
    3a16d1ecc Keep Perspective Selecto synced when switching athletes
    63d13ba9e Update Italian translation
    f947b6bd3 VideoSync - Distance scaling only applies to CRS workouts
    67962bfff Fix memory leak in Python API
    5f22d12a6 fix(doc): mention QMAKE_MOVE requirement for bison > 3.7
    996ee63cd Remove MapQuest API Key from gcconfig.pri.in
    a7ad1346e Measures CSV Import - Last field can be empty
    90172a4a6 Replace Tracker in default Trends layout
    68b70e0e4 Fix Elevation - Avoid 302 network error
    434580ee8 Disable execution of Python DPs from formulas and Python API
    cb23914a5 Honor chart filters in Trends view Histogram and CP charts (#4287)
    1941b9af7 Minimum bison version is now 2.7

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -104,7 +104,7 @@
 // 4009 - V3.6 DEVELOPMENT 2110 (NOV 2021) (Pre-release)
 // 4010 - V3.6 DEVELOPMENT 2202 (FEB 2022) (Pre-release RC1)
 // 4011 - V3.6 DEVELOPMENT 2208 (AUG 2022) (Pre-release RC2)
-// 4012 - V3.6 DEVELOPMENT 2208 (OCT 2022) (Pre-release RC3) - triggered 29/10
+// 4012 - V3.6 DEVELOPMENT 2208 (OCT 2022) (Pre-release RC3) - triggered 29/10 - updated snapshot 23/12
 
 #define VERSION3_BUILD    3010 // released
 #define VERSION3_SP1      3030 // released
