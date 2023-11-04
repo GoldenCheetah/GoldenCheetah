15bafbd4448f0ec09a2d4caf5a0e6e1eec8f9536
Alejandro Martinez <amtriathlon@gmail.com>
2023-11-04 10:19:31 -0300

    Update snapshot builds
    Summary of changes from v3.6 release:
    ec0653f81 ErgFilePlot - realtime curves honor FFWD/RWND and lap F/B
    43368addf Mapview: Added interactivity to the Smallplot (#4408)
    72de6bbee Fix lap data auto-reset in slope workouts
    c2f440c1c Fix broken workout directory check for the first start (#4410)
    b095b403b Fix shortcuts not available due to translations
    e6ff1a157 Python Chart Editor Cosmetics
    00e182c41 Better user experience when drawing segments on the map
    (#4403)
    f746f7361 GPX parser - support ns3 TrackPointExtension name
    9ae7521e5 Upgrade Travis-ci macOS version to macOS 11.6
    e8132b1a5 Upgrade Travis-ci Linux version to Ubuntu 20.04
    8f2ba0e11 Special case Start Date and Time in the cache
    110652bf4 Update German translation
    fabc344aa Add Wahoo PowrLink
    f78381466 Train library - add path to the list of possible errors
    48d91d02d Fix issues after perspective switch (#786)
    59a8ca231 Implemented compare mode for RideMapWindow (#786)
    733db9cb1 Ignore dot folders on athlete open/backup/delete
    951e5f360 Fix crash on invalid perspective filter
    [publish binaries]

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -106,7 +106,7 @@
 // 4011 - V3.6 DEVELOPMENT 2208 (AUG 2022) (Pre-release RC2)
 // 4012 - V3.6 DEVELOPMENT 2210 (OCT 2022) (Pre-release RC3)
 // 4013 - V3.6 DEVELOPMENT 2303 (MAR 2023) (Pre-release RC4)
-// 5000 - V3.6 RELEASE (August 2023) - latest snapshot 24/9
+// 5000 - V3.6 RELEASE (August 2023) - latest snapshot 4/11
 
 #define VERSION3_BUILD    3010 // released
 #define VERSION3_SP1      3030 // released
