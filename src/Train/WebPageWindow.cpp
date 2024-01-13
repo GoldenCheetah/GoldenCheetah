GoldenCheetah/GoldenCheetah

commit c72b7d7a4f06c674b8d8b0937342e9f474cc378c

Replace QWebEngineDownloadItem for Qt6
It was renamed as QWebEngineDownloadRequest

src/Train/WebPageWindow.cpp
@@ -38,7 +38,11 @@
 
 #include <QtWebChannel>
 #include <QWebEngineProfile>
+#if QT_VERSION < 0x060000
 #include <QWebEngineDownloadItem>
+#else
+#include <QWebEngineDownloadRequest>
+#endif
 
 // overlay helper
 #include "AbstractView.h"
@@ -174,7 +178,11 @@ WebPageWindow::WebPageWindow(Context *context) : GcChartWindow(context), context
     configChanged(CONFIG_APPEARANCE);
 
     // intercept downloads
+#if QT_VERSION < 0x060000
     connect(view->page()->profile(), SIGNAL(downloadRequested(QWebEngineDownloadItem*)), this, SLOT(downloadRequested(QWebEngineDownloadItem*)));
+#else
+    connect(view->page()->profile(), SIGNAL(downloadRequested(QWebEngineDownloadRequest*)), this, &downloadRequested);
+#endif
     connect(view->page(), SIGNAL(linkHovered(QString)), this, SLOT(linkHovered(QString)));
 }
 
@@ -255,7 +263,11 @@ WebPageWindow::event(QEvent *event)
 }
 
 void
+#if QT_VERSION < 0x060000
 WebPageWindow::downloadRequested(QWebEngineDownloadItem *item)
+#else
+WebPageWindow::downloadRequested(QWebEngineDownloadRequest *item)
+#endif
 {
     // only do it if I am visible, as shared across web page instances
     if (!amVisible()) return;

src/Train/WebPageWindow.h
@@ -49,7 +49,11 @@ class SmallPlot;
 
 // trick the maps api into ignoring gestures by
 // pretending to be chrome. see: http://developer.qt.nokia.com/forums/viewthread/1643/P15
+#if QT_VERSION < 0x060000
 class QWebEngineDownloadItem;
+#else
+class QWebEngineDownloadRequest;
+#endif
 class simpleWebPage : public QWebEnginePage
 {
 };
@@ -83,7 +87,11 @@ class WebPageWindow : public GcChartWindow
 
         void downloadProgress(qint64, qint64);
         void downloadFinished();
+#if QT_VERSION < 0x060000
         void downloadRequested(QWebEngineDownloadItem*);
+#else
+        void downloadRequested(QWebEngineDownloadRequest*);
+#endif
         void linkHovered(QString);
 
     private:

commit 646b2151f59c09d17b41007728c4367fa1875c4c

WebPageWindows - Fix typo
Introduced by c72b7d7a4f06c674b8d8b0937342e9f474cc378c

src/Train/WebPageWindow.cpp
@@ -181,7 +181,7 @@ WebPageWindow::WebPageWindow(Context *context) : GcChartWindow(context), context
 #if QT_VERSION < 0x060000
     connect(view->page()->profile(), SIGNAL(downloadRequested(QWebEngineDownloadItem*)), this, SLOT(downloadRequested(QWebEngineDownloadItem*)));
 #else
-    connect(view->page()->profile(), SIGNAL(downloadRequested(QWebEngineDownloadRequest*)), this, &downloadRequested);
+    connect(view->page()->profile(), SIGNAL(downloadRequested(QWebEngineDownloadRequest*)), this, SLOT(downloadRequested(QWebEngineDownloadRequest*)));
 #endif
     connect(view->page(), SIGNAL(linkHovered(QString)), this, SLOT(linkHovered(QString)));
 }