889b1894c5d44f6d1b1e32b7f381375a95ad275e
Paul Johnson <paulj49457@gmail.com>
2024-05-24 01:39:27 +0100

    Fixes build error at VideoWindow line 403 (#4500)
    Introduced by https://github.com/GoldenCheetah/GoldenCheetah/commit/77df5b372c33dcb292e4299a1472111d87feefdb

--- src/Train/VideoWindow.cpp
+++ src/Train/VideoWindow.cpp
@@ -398,9 +398,9 @@ void VideoWindow::startPlayback()
             }
         }
 #endif
-#if defined(GC_VIDEO_QT5)
+#ifdef GC_VIDEO_QT5
         // QT doesn't expose media frame rate so make due with duration.
-        double videoSyncDuration = currentVideoSyncFile->Duration;
+        double videoSyncDuration = currentVideoSyncFile->duration();
         if (videoSyncDuration > 0) {
             double mediaDuration = (double)mp->duration();
             if (mediaDuration > 0) {
