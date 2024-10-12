GoldenCheetah/GoldenCheetah

commit 60f9033617996c30a7b18a608adea27e8e370153

Cycling dynamics in train mode (#4337)
* TRAIN - add cycling dynamics
Power sensors which deliver cycling dynamics such as power start angle,
rider position, etc. are supported during hometrainer session.
Goldencheetah requests power sensor capabilities and, when available,
request the sensor to enable them (power phase, pedal position, etc.
and 8Hz transmission mode which is mandatory when accessing those
additional data pages)

* COSMETIC - FIX comment related to gc csv header
The comment was not reflecting the up-to-date file
content as per latest CSV header

* FIX load/slope in CSV
Only target power was recorded in CSV records during hometrainer
session.
When not using ERG mode but slope mode then the data was lost.
This patch take care of training mode to determine which data
is to be recorded.

* TRAIN - flexible gc CSV
Allows to add fields in CSV file recorded during hometrainer session
without impacting a lot of files in the code
Historical first fields are still used to determine taht this file
has been created by GoldenCheetah.
During CSV parsing GoldenCheetah will use CSV header to determine which
fields are part of the file and to locate the column which contain it

* TRAIN - add cycling dynamics to CSV records
during hometrainer session


* CsvRideFile fixes
Free gcSeries and Qt6 compatibility
---------

Co-authored-by: Alejandro Martinez <amtriathlon@gmail.com>

src/Train/RealtimeData.h
@@ -46,6 +46,9 @@ public:
                       VirtualSpeed, AltWatts, LRBalance, LapTimeRemaining,
                       LeftTorqueEffectiveness, RightTorqueEffectiveness,
                       LeftPedalSmoothness, RightPedalSmoothness, Slope, 
+                      RightPowerPhaseBegin, RightPowerPhaseEnd,
+                      RightPowerPhasePeakBegin, RightPowerPhasePeakEnd,
+                      Position, RightPCO, LeftPCO,
                       LapDistance, LapDistanceRemaining, ErgTimeRemaining,
                       Latitude, Longitude, Altitude, RouteDistance,
                       DistanceRemaining };
@@ -95,7 +98,20 @@ public:
     void setRTE(double);
     void setLPS(double);
     void setRPS(double);
+    void setRppb(double);
+    void setRppe(double);
+    void setRpppb(double);
+    void setRpppe(double);
+    void setLppb(double);
+    void setLppe(double);
+    void setLpppb(double);
+    void setLpppe(double);
+    void setRightPCO(double);
+    void setLeftPCO(double);
+    void setPosition(RealtimeData::riderPosition);
     void setTorque(double);
+    void setRTorque(double);
+    void setLTorque(double);
     void setLatitude(double);
     void setLongitude(double);
     void setAltitude(double);
@@ -149,6 +165,17 @@ public:
     double getRTE() const;
     double getLPS() const;
     double getRPS() const;
+    double getRppb() const;
+    double getRppe() const;
+    double getRpppb() const;
+    double getRpppe() const;
+    double getLppb() const;
+    double getLppe() const;
+    double getLpppb() const;
+    double getLpppe() const;
+    double getRightPCO() const;
+    double getLeftPCO() const;
+    RealtimeData::riderPosition getPosition() const;
     double getTorque() const;
     double getLatitude() const;
     double getLongitude() const;
@@ -177,9 +204,14 @@ private:
     double cadence;      // in rpm
     double smo2, thb;
     double lte, rte, lps, rps; // torque efficiency and pedal smoothness
+    double rppb, rppe, rpppb, rpppe;
+    double lppb, lppe, lpppb, lpppe;
+    double rightPCO, leftPCO;
     double torque; // raw torque data for calibration display
+    double RTorque, LTorque;
     double latitude, longitude, altitude;
     double vo2, vco2, rf, rmv, tv, feo2;
+    RealtimeData::riderPosition position;
 
     std::chrono::high_resolution_clock::time_point wheelRpmSampleTime;
 

src/ANT/ANT.cpp
@@ -54,7 +54,7 @@ const ant_sensor_type_t ANT::ant_sensor_types[] = {
   { true, ANTChannel::CHANNEL_TYPE_UNUSED, 0, 0, 0, 0, "Unused", '?', "" },
   { true, ANTChannel::CHANNEL_TYPE_HR, ANT_SPORT_HR_PERIOD, ANT_SPORT_HR_TYPE,
                 ANT_SPORT_FREQUENCY, ANT_SPORT_NETWORK_NUMBER, "Heartrate", 'h', ":images/IconHR.png" },
-  { true, ANTChannel::CHANNEL_TYPE_POWER, ANT_SPORT_POWER_PERIOD, ANT_SPORT_POWER_TYPE,
+  { true, ANTChannel::CHANNEL_TYPE_POWER, ANT_SPORT_POWER_8HZ_PERIOD, ANT_SPORT_POWER_TYPE,
                 ANT_SPORT_FREQUENCY, ANT_SPORT_NETWORK_NUMBER, "Power", 'p', ":images/IconPower.png" },
   { true, ANTChannel::CHANNEL_TYPE_SPEED, ANT_SPORT_SPEED_PERIOD, ANT_SPORT_SPEED_TYPE,
                 ANT_SPORT_FREQUENCY, ANT_SPORT_NETWORK_NUMBER, "Speed", 's', ":images/IconSpeed.png" },
@@ -110,6 +110,7 @@ ANT::ANT(QObject *parent, DeviceConfiguration *devConf, QString athlete) : QThre
     vortexID = vortexChannel = -1;
 
     fecChannel = -1;
+    pwrChannel = -1;
 
     // current and desired modes/load/gradients
     // set so first time through current != desired
@@ -153,6 +154,8 @@ ANT::ANT(QObject *parent, DeviceConfiguration *devConf, QString athlete) : QThre
 
         // R-R data
         connect(antChannel[i], SIGNAL(rrData(uint16_t, uint8_t, uint8_t)), this, SIGNAL(rrData(uint16_t, uint8_t, uint8_t)));
+
+        connect(antChannel[i], SIGNAL(posData(uint8_t)), this, SIGNAL(posData(uint8_t)));
 
         // timer for master channel broadcasts
         connect(antChannel[i], SIGNAL(broadcastTimerStart(int)), this, SLOT(slotStartBroadcastTimer(int)));
@@ -364,6 +367,28 @@ void ANT::requestFecCapabilities()
 void ANT::requestFecCalibration(uint8_t type)
 {
     sendMessage(ANTMessage::fecRequestCalibration(fecChannel, type));
+}
+
+void ANT::requestPwrCapabilities1(const uint8_t chan)
+{
+    sendMessage(ANTMessage::requestPwrCapabilities1(chan!=-1?chan:pwrChannel));
+}
+
+void ANT::requestPwrCapabilities2(const uint8_t chan)
+{
+    sendMessage(ANTMessage::requestPwrCapabilities2(chan!=-1?chan:pwrChannel));
+}
+
+void ANT::enablePwrCapabilities1(const uint8_t chan, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup)
+{
+    // qDebug()<<chan<<qPrintable("Request to enable some capabilities from sub-page 1 of power sensor: 0x" + QString("%1").arg(capabilitiesSetup, 2, 16, QChar('0')).toUpper());
+    sendMessage(ANTMessage::enablePwrCapabilities1(chan, capabilitiesMask, capabilitiesSetup));
+}
+
+void ANT::enablePwrCapabilities2(const uint8_t chan, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup)
+{
+    // qDebug()<<chan<<qPrintable("Request to enable some capabilities from sub-page 2 of power sensor: 0x" + QString("%1").arg(capabilitiesSetup, 2, 16, QChar('0')).toUpper());
+    sendMessage(ANTMessage::enablePwrCapabilities2(chan, capabilitiesMask, capabilitiesSetup));
 }
 
 void ANT::requestPwrCalibration(uint8_t channel, uint8_t type)
@@ -827,6 +852,13 @@ ANT::channelInfo(int channel, int device_number, int device_id)
     if (!configuring && antChannel[channel]->is_fec) {
         antChannel[channel]->capabilities();
         qDebug()<<"ANT FE-C device found."<<device_number<<"on channel"<<channel;
+    }
+
+    // ANT PWR DEVICE DETECTED - ACT ACCORDINGLY !
+    // if we just got an PWR sensor, request the capabilities
+    if (!configuring && antChannel[channel]->is_power) {
+        antChannel[channel]->capabilities();
+        qDebug()<<channel<<"ANT power device found."<<device_number;
     }
 
     //qDebug()<<"found device number"<<device_number<<"type"<<device_id<<"on channel"<<channel
@@ -1350,6 +1382,11 @@ void ANT::setVortexData(int channel, int id)
 void ANT::setFecChannel(int channel)
 {
     fecChannel = channel;
+}
+
+void ANT::setPwrChannel(int channel)
+{
+    pwrChannel = channel;
 }
 
 void ANT::setControlChannel(int channel)

src/ANT/ANT.h
@@ -190,6 +190,10 @@ struct setChannelAtom {
 #define ANT_CW_INIT            0x53
 #define ANT_CW_TEST            0x48
 
+#define ANT_REQUEST_DATA_PAGE  0x46
+#define ANT_RQST_MST_DATA_PAGE 0x01
+#define ANT_RQST_SLV_DATA_PAGE 0x03
+
 #define ANT_TX_TYPE_SLAVE      0x00
 #define ANT_TX_TYPE_MASTER     0x05 // typical tx type for master device, see ANT Message Protocol and Usage doc
 
@@ -235,6 +239,7 @@ struct setChannelAtom {
 // ANT+sport
 #define ANT_SPORT_HR_PERIOD 8070
 #define ANT_SPORT_POWER_PERIOD 8182
+#define ANT_SPORT_POWER_8HZ_PERIOD 4091 // when accessing additional data shall be 8Hz
 #define ANT_SPORT_FOOTPOD_PERIOD 8134
 #define ANT_SPORT_SPEED_PERIOD 8118
 #define ANT_SPORT_CADENCE_PERIOD 8102
@@ -284,6 +289,33 @@ struct setChannelAtom {
 
 #define ANT_SPORT_AUTOZERO_OFF                        0x00
 #define ANT_SPORT_AUTOZERO_ON                         0x01
+
+// std power pages
+#define POWER_POWERONLY_DATA_PAGE                     0x10
+#define POWER_GETSET_PARAM_PAGE                       0x02
+#define POWER_MANUFACTURER_ID                         0x50
+#define POWER_PRODUCT_INFO                            0x51
+#define POWER_ADV_CAPABILITIES1_SUBPAGE               0xFD
+#define POWER_ADV_CAPABILITIES2_SUBPAGE               0xFE
+// std power ANT transmissions parameters
+#define POWER_CAPABILITIES_DELAY                      2
+#define POWER_CAPABILITIES_RQST_NBR                   2
+
+// Cycling dynamics data pages
+#define POWER_CYCL_DYN_R_FORCE_ANGLE_PAGE             0xE0
+#define POWER_CYCL_DYN_L_FORCE_ANGLE_PAGE             0xE1
+#define POWER_CYCL_DYN_PEDALPOSITION_PAGE             0xE2
+#define POWER_CYCL_DYN_TORQUE_BARYC_PAGE              0x14
+
+// note: capabilities are available or enabled when set to "0"
+#define POWER_NO_4HZ_MODE_CAPABILITY                  0x01
+#define POWER_NO_8HZ_MODE_CAPABILITY                  0x02
+#define POWER_NO_POWERPHASE_CAPABILITY                0x08
+#define POWER_NO_PCO_CAPABILITY                       0x10
+#define POWER_NO_POSITION_CAPABILITY                  0x20
+#define POWER_NO_TORQUE_BARYCENTER_CAPABILITY         0x40
+
+#define POWER_CYCL_DYN_INVALID                        0xC0
 
 // kickr
 #define KICKR_COMMAND_INTERVAL         60 // every 60 ms
@@ -423,6 +455,8 @@ signals:
     // signal instantly on data receipt for R-R data
     // made a special case to support HRV tool without complication
     void rrData(uint16_t  rrtime, uint8_t heartrateBeats, uint8_t instantHeartrate);
+
+    void posData(uint8_t position);
 
     // signal for passing remote control commands
     void antRemoteControl(uint16_t command);
@@ -646,6 +680,19 @@ public:
         telemetry.setRPS(rps);
     }
 
+    void setRppb(double value) { telemetry.setRppb(value); }
+    void setRppe(double value) { telemetry.setRppe(value); }
+    void setRpppb(double value) { telemetry.setRpppb(value); }
+    void setRpppe(double value) { telemetry.setRpppe(value); }
+    void setLppb(double value) { telemetry.setLppb(value); }
+    void setLppe(double value) { telemetry.setLppe(value); }
+    void setLpppb(double value) { telemetry.setLpppb(value); }
+    void setLpppe(double value) { telemetry.setLpppe(value); }
+    void setRightPCO(uint8_t value) { telemetry.setRightPCO(value); }
+    void setLeftPCO(uint8_t value) { telemetry.setLeftPCO(value); }
+    void setPosition(RealtimeData::riderPosition value) { telemetry.setPosition(value); }
+    void setRTorque(double torque) { telemetry.setRTorque(torque); }
+    void setLTorque(double torque) { telemetry.setLTorque(torque); }
     void setTorque(double torque) {
         telemetry.setTorque(torque);
     }
@@ -656,7 +703,12 @@ public:
     void requestFecCapabilities();
     void requestFecCalibration(uint8_t type);
 
+    void setPwrChannel(int channel);
     void requestPwrCalibration(uint8_t channel, uint8_t type);
+    void requestPwrCapabilities1(const uint8_t chan);
+    void requestPwrCapabilities2(const uint8_t chan);
+    void enablePwrCapabilities1(const uint8_t chan, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup);
+    void enablePwrCapabilities2(const uint8_t chan, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup);
 
     void setVortexData(int channel, int id);
     void refreshVortexLoad();
@@ -735,6 +787,7 @@ private:
 
     // fitness equipment data
     int fecChannel;
+    int pwrChannel;
 
     // tacx vortex (we'll probably want to abstract this out cf. kickr)
     int vortexID;

src/ANT/ANTChannel.h
@@ -97,6 +97,15 @@ class ANTChannel : public QObject {
         uint8_t fecPrevRawDistance;
         uint8_t  fecCapabilities;
 
+        // sensor capabilities, requested capabilities and enabled capabilities
+        uint16_t  pwrCapabilities;
+        uint16_t  pwrReqCapabilities;
+        uint16_t  pwrEnCapabilities;
+        uint8_t   pwrCapabilitiesMsgDelay;
+        bool      pwrSkipCapability1Rqst;
+        bool      pwrSkipCapability1Setup;
+        bool      pwrCapabilitiesSetupComplete;
+
         double blanking_timestamp;
         int blanked;
         char id[10]; // short identifier
@@ -109,6 +118,7 @@ class ANTChannel : public QObject {
         qint64 lastMessageTimestamp; // for time comparisons
         qint64 lastMessageTimestamp2;
         uint16_t lastHRmeasurement;
+        uint8_t  lastPositionData;
 
         unsigned char rx_burst_data[RX_BURST_DATA_LEN];
         int           rx_burst_data_index;
@@ -165,6 +175,7 @@ class ANTChannel : public QObject {
         bool is_old_cinqo; // bool, set for cinqo needing separate control channel
         bool is_srm;
         bool is_fec;
+        bool is_power;
         bool is_alt; // is alternative channel for power
         bool is_master; // is a master channel (for remote control)
 
@@ -202,12 +213,15 @@ class ANTChannel : public QObject {
         double channelValue() { return value; }
         double channelValue2() { return value2; }
         double value,value2; // used during config, rather than rtData
-        uint8_t capabilities();
+        uint16_t capabilities();
 
         // search
         int isSearching();
 
         QTimer *channelTimer; // timer for master channel broadcast events
+
+        // timer for sensors setup
+        QTimer *sensorSetupTimer;
 
         // Cinqo support
         void sendCinqoError();
@@ -218,6 +232,12 @@ class ANTChannel : public QObject {
 
         void setAlt(bool value) { is_alt = value; }
 
+    public slots:
+       // setup sensors
+        void slotStartSensorSetupTimer();
+        void slotStopSensorSetupTimer();
+        void slotSensorSetupTimerEvent();
+
     signals:
         void channelInfo(int number, int device_number, int device_id); // we got a channel info message
         void dropInfo(int number, int dropped, int received);    // we dropped a packet
@@ -227,9 +247,13 @@ class ANTChannel : public QObject {
         void searchComplete(int number); // search completed successfully
         void broadcastTimerStart(int number);
         void broadcastTimerStop(int number);
+        void sensorSetupTimerStart(); // setup sensors timer
+        void sensorSetupTimerStop();
 
         // signal instantly on data receipt for R-R data
         void rrData(uint16_t  rrtime, uint8_t heartrateBeats, uint8_t instantHeartrate);
+
+        void posData(uint8_t position);
 
         // signal for passing remote control commands
         void antRemoteControl(uint16_t command);

src/ANT/ANTMessage.cpp
@@ -183,6 +183,8 @@
  *                                                operating_time_msb,voltage_lsb,descriptive_bits
  */
 
+#define bradToDeg(brads) (static_cast<double>(brads)/256.0*360.0)
+
 // construct a null message
 ANTMessage::ANTMessage(void) {
     init();
@@ -354,6 +356,13 @@ ANTMessage::ANTMessage(ANT *parent, const unsigned char *message) {
             //   0x12 - Crank Torque (Quarq)
             //   0x13 - Torque Efficiency and Pedal Smoothness - optional extension to 0x10 Standard Power or 0x11/0x12 Wheel/Crank Torque
             //   0x20 - Crank Torque Frequency (SRM)
+            //   0x50 - Manufacturer’s Identification
+            //   0x51 - Product Information
+            //   0xE0 - Pedal right force angle
+            //   0xE1 - Pedal left force angle
+            //   0xE2 - Pedal position data
+            //   0xFD - Capabilities #1
+            //   0xFE - Capabilities #2
             //   0x50 - Manufacturer UD
             //   0x52 - Battery Voltage
 
@@ -420,13 +429,42 @@ ANTMessage::ANTMessage(ANT *parent, const unsigned char *message) {
                 switch (data_page) {
 
                 case ANT_STANDARD_POWER: // 0x10 - standard power
-
                     eventCount = message[5];
                     pedalPowerContribution = (( message[6] != 0xFF ) && ( message[6]&0x80) ) ; // left/right is defined if NOT 0xFF (= no Pedal Power) AND BIT 7 is set
                     pedalPower = (message[6]&0x7F); // right pedalPower % - stored in bit 0-6
                     instantCadence = message[7];
                     sumPower = message[8] + (message[9]<<8);
                     instantPower = message[10] + (message[11]<<8);
+                    break;
+
+                // subpages 0xE0/E1/E2: cycling dynamics from power device
+                case POWER_CYCL_DYN_R_FORCE_ANGLE_PAGE:
+                case POWER_CYCL_DYN_L_FORCE_ANGLE_PAGE:
+                    eventCount = message[5];
+                    if (message[6]==POWER_CYCL_DYN_INVALID  && message[7]==POWER_CYCL_DYN_INVALID)
+                    {
+                        // change invalid data (0xC0 as per ANT+ specification) to "0"
+                        instantStartAngle=instantEndAngle=0.0;
+                    } else
+                    {
+                        instantStartAngle     = bradToDeg(message[6]);
+                        instantEndAngle       = bradToDeg(message[7]);
+                    }
+                    if (message[8]==POWER_CYCL_DYN_INVALID  && message[9]==POWER_CYCL_DYN_INVALID)
+                    {
+                        instantStartPeakAngle=instantEndPeakAngle=0.0;
+                    } else
+                    {
+                        instantStartPeakAngle = bradToDeg(message[8]);
+                        instantEndPeakAngle   = bradToDeg(message[9]);
+                    }
+                    torque                = message[10] + (message[11]<<8);
+                    break;
+                case POWER_CYCL_DYN_PEDALPOSITION_PAGE:
+                    eventCount = message[5];
+                    riderPosition         = message[6] >> 6;
+                    rightPCO              = message[8];
+                    leftPCO               = message[9];
                     break;
 
                 case ANT_WHEELTORQUE_POWER: // 0x11 - wheel torque (Powertap)
@@ -512,6 +550,43 @@ ANTMessage::ANTMessage(ANT *parent, const unsigned char *message) {
 
                     }
                     break;
+
+                case POWER_GETSET_PARAM_PAGE:
+                {
+                    uint8_t data_subpage = message[5];
+
+                    switch(data_subpage) {
+                        case POWER_ADV_CAPABILITIES1_SUBPAGE:
+                            pwrCapabilities1         = message[8];
+                            pwrEnCapabilities1       = message[10];
+                            // qDebug()<<channel
+                            //     << qPrintable(QString("Received power advanced capabilities page 1, pwrCapabilities1=0x")+QString("%1").arg(pwrCapabilities1, 2, 16, QChar('0')).toUpper()
+                            //     +  QString(", enCapabilities1=0x")+QString("%1").arg(pwrEnCapabilities1, 2, 16, QChar('0')).toUpper());
+                            break;
+
+                        case POWER_ADV_CAPABILITIES2_SUBPAGE:
+                            pwrCapabilities2         = message[8];
+                            pwrEnCapabilities2       = message[10];
+                            // qDebug()<<channel
+                            //     << qPrintable(QString("Received power advanced capabilities page 2, pwrCapabilities2=0x")+QString("%1").arg(pwrCapabilities2, 2, 16, QChar('0')).toUpper()
+                            //     +  QString(", enCapabilities2=0x")+QString("%1").arg(pwrEnCapabilities2, 2, 16, QChar('0')).toUpper());
+                            break;
+                    }
+                    break;
+                }
+
+                case POWER_MANUFACTURER_ID:
+                {
+                    uint16_t manuf_id = (message[8] + (static_cast<uint16_t>(message[9])<<8));
+                    uint16_t product_id = (message[10] + (static_cast<uint16_t>(message[11])<<8));
+                    // qDebug()<<channel<<"Received power sensor info: Manufacturer id:" << manuf_id << ", Product id:" << product_id;
+                    break;
+                }
+
+                case POWER_PRODUCT_INFO:
+                    // qDebug()<<channel<<"Received power product info page";
+                    break;
+
                 } // data_page
                 break;
 
@@ -845,6 +920,9 @@ void ANTMessage::init()
     autoZeroStatus = autoZeroEnable = 0;
     pedalPowerContribution = false;
     pedalPower = 0;
+    instantStartAngle = instantEndAngle = instantStartPeakAngle = instantEndPeakAngle = 0;
+    riderPosition = 0;
+    rightPCO = leftPCO = 0;
     leftTorqueEffectiveness = rightTorqueEffectiveness = 0;
     leftOrCombinedPedalSmoothness = rightPedalSmoothness = 0;
     fecSpeed = fecInstantPower = fecAccumulatedPower = 0;
@@ -1183,6 +1261,72 @@ ANTMessage ANTMessage::requestPwrCalibration(const uint8_t channel, const uint8_
                       0xFF, 0xFF,                           // reserved
                       0xFF, 0xFF,                           // reserved
                       0xFF, 0xFF);                          // reserved
+}
+
+ANTMessage ANTMessage::requestPwrCapabilities1(const uint8_t channel)
+{
+    // qDebug()<<channel<<"Requesting Capabilities sub-page 1 from power device";
+
+    // based on ANT+ Common Pages, Rev 2.4 p 14: 6.2  Common Data Page 70: Request Data Page
+    return ANTMessage(9, ANT_ACK_DATA, channel,
+                      ANT_REQUEST_DATA_PAGE,              // data page request
+                      0xFF, 0xFF,                         // reserved
+                      POWER_ADV_CAPABILITIES1_SUBPAGE,    // descriptor 1 sub page
+                      0xFF,                               // descriptor 2
+                      POWER_CAPABILITIES_RQST_NBR,        // requested transmission response
+                      POWER_GETSET_PARAM_PAGE,            // requested page
+                      ANT_RQST_MST_DATA_PAGE);            // request data page from master
+}
+
+ANTMessage ANTMessage::requestPwrCapabilities2(const uint8_t channel)
+{
+    // qDebug()<<channel<<"Requesting Capabilities sub-page 2 from power device";
+
+    // based on ANT+ Common Pages, Rev 2.4 p 14: 6.2  Common Data Page 70: Request Data Page
+    return ANTMessage(9, ANT_ACK_DATA, channel,
+                      ANT_REQUEST_DATA_PAGE,              // data page request
+                      0xFF, 0xFF,                         // reserved
+                      POWER_ADV_CAPABILITIES2_SUBPAGE,    // descriptor 1 sub page
+                      0xFF,                               // descriptor 2
+                      POWER_CAPABILITIES_RQST_NBR,        // requested transmission response
+                      POWER_GETSET_PARAM_PAGE,            // requested page
+                      ANT_RQST_MST_DATA_PAGE);            // request data page from master
+}
+
+ANTMessage ANTMessage::enablePwrCapabilities1(const uint8_t channel, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup)
+{
+    // qDebug()<<channel<<"Setup power sensor capabilities from sub-page 1 to"
+    //     << qPrintable(QString("0x")+QString("%1").arg(capabilitiesSetup, 2, 16, QChar('0')).toUpper());
+
+    // based on ANT+ Device Profile - Bicycle Power Rev 5.1
+    // page 21: 4.5.2 Enabling Cycling Dynamics
+    // page 71: 15.2.3 Subpage 0x04 – Rider Position Configuration
+    return ANTMessage(9, ANT_ACK_DATA, channel,
+                      POWER_GETSET_PARAM_PAGE,              // data page
+                      POWER_ADV_CAPABILITIES1_SUBPAGE,      // subpage
+                      0xFF, 0xFF,                           // reserved
+                      capabilitiesMask,                     // capabilities mask
+                      0xFF,                                 // reserved
+                      capabilitiesSetup,                    // capabilities details
+                      0xFF);                                // reserved
+}
+
+ANTMessage ANTMessage::enablePwrCapabilities2(const uint8_t channel, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup)
+{
+    // qDebug()<<channel<<"Setup power sensor capabilities from sub-page 2 to"
+    //     << qPrintable(QString("0x")+QString("%1").arg(capabilitiesSetup, 2, 16, QChar('0')).toUpper());
+
+    // based on ANT+ Device Profile - Bicycle Power Rev 5.1
+    // page 21: 4.5.2 Enabling Cycling Dynamics
+    // page 71: 15.2.3 Subpage 0x04 – Rider Position Configuration
+    return ANTMessage(9, ANT_ACK_DATA, channel,
+                      POWER_GETSET_PARAM_PAGE,              // data page
+                      POWER_ADV_CAPABILITIES2_SUBPAGE,      // subpage
+                      0xFF, 0xFF,                           // reserved
+                      capabilitiesMask,                     // capabilities mask
+                      0xFF,                                 // reserved
+                      capabilitiesSetup,                    // capabilities details
+                      0xFF);                                // reserved
 }
 
 ANTMessage ANTMessage::controlDeviceAvailability(const uint8_t channel)

src/ANT/ANTMessage.h
@@ -89,6 +89,12 @@ class ANTMessage {
         // Power meter calibration
         static ANTMessage requestPwrCalibration(const uint8_t channel, const uint8_t type);
 
+        // Power meter capabilities
+        static ANTMessage requestPwrCapabilities1(const uint8_t channel);
+        static ANTMessage requestPwrCapabilities2(const uint8_t channel);
+        static ANTMessage enablePwrCapabilities1(const uint8_t channel, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup);
+        static ANTMessage enablePwrCapabilities2(const uint8_t channel, const uint8_t capabilitiesMask, const uint8_t capabilitiesSetup);
+
         // remote control
         static ANTMessage controlDeviceAvailability(const uint8_t channel);
 
@@ -144,6 +150,14 @@ class ANTMessage {
         uint16_t accumulatedTorque;
         uint8_t  elapsedTime;
         uint8_t instantCadence; // cadence
+        double  instantStartAngle;
+        double  instantEndAngle;
+        double  instantStartPeakAngle;
+        double  instantEndPeakAngle;
+        uint8_t riderPosition;
+        uint8_t rightPCO;
+        uint8_t leftPCO;
+
         uint8_t autoZeroStatus, autoZeroEnable;
         bool utcTimeRequired; // moxy
         uint8_t moxyCapabilities; //moxy
@@ -154,6 +168,7 @@ class ANTMessage {
         uint16_t vortexId, vortexSpeed, vortexPower, vortexCadence;
         uint8_t vortexCalibration, vortexCalibrationState, vortexPage;
         uint8_t vortexUsingVirtualSpeed;
+        uint8_t  pwrCapabilities1, pwrEnCapabilities1, pwrCapabilities2, pwrEnCapabilities2;
 
         // fitness equipment data fields
         uint16_t fecSpeed, fecInstantPower, fecAccumulatedPower;

src/ANT/ANTlocalController.cpp
@@ -48,6 +48,8 @@ ANTlocalController::ANTlocalController(TrainSidebar *parent, DeviceConfiguration
     // collecting R-R HRV data?
     connect(myANTlocal, SIGNAL(rrData(uint16_t, uint8_t, uint8_t)), this, SIGNAL(rrData(uint16_t, uint8_t, uint8_t)));
 
+    connect(myANTlocal, SIGNAL(posData(uint8_t)), this, SIGNAL(posData(uint8_t)));
+
     // connect slot receiving ANT remote control commands & translating to native
     connect(myANTlocal, SIGNAL(antRemoteControl(uint16_t)), this, SLOT(antRemoteControl(uint16_t)));
 

commit 05fd4193023019322c9ee89f77a752ff2238dae5

avoid regression caused by #4337 due to changed order of data series enum

src/Train/RealtimeData.h
@@ -35,6 +35,9 @@ public:
     ErgFileFormat mode;
 
     // abstract to dataseries
+    // the order matters. if we remove any entry, the corresponding layouts
+    // will break since the data series is defined by its enum value. new data
+    // series need to be appended.
     enum dataseries { None=0, Time, LapTime, Distance, Lap,
                       Watts, Speed, Cadence, HeartRate, Load,
                       XPower, BikeScore, RI, Joules, SkibaVI,
@@ -46,12 +49,12 @@ public:
                       VirtualSpeed, AltWatts, LRBalance, LapTimeRemaining,
                       LeftTorqueEffectiveness, RightTorqueEffectiveness,
                       LeftPedalSmoothness, RightPedalSmoothness, Slope, 
-                      RightPowerPhaseBegin, RightPowerPhaseEnd,
-                      RightPowerPhasePeakBegin, RightPowerPhasePeakEnd,
-                      Position, RightPCO, LeftPCO,
                       LapDistance, LapDistanceRemaining, ErgTimeRemaining,
                       Latitude, Longitude, Altitude, RouteDistance,
-                      DistanceRemaining };
+                      DistanceRemaining,
+                      RightPowerPhaseBegin, RightPowerPhaseEnd,
+                      RightPowerPhasePeakBegin, RightPowerPhasePeakEnd,
+                      Position, RightPCO, LeftPCO };
 
     typedef enum dataseries DataSeries;
 