4f23cf4f889eb8dcd5931b08cae1a8dde5ca6585
Joachim Kohlhammer <joachim.kohlhammer@gmx.de>
2024-11-13 18:06:05 +0100

    Added override keyword

--- src/FileIO/FixPyDataProcessor.h
+++ src/FileIO/FixPyDataProcessor.h
@@ -8,11 +8,11 @@ class FixPyDataProcessor : public DataProcessor
 {
 public:
     FixPyDataProcessor(FixPyScript *pyScript);
-    bool postProcess(RideFile *rideFile, DataProcessorConfig *settings, QString op);
-    DataProcessorConfig *processorConfig(QWidget *parent, const RideFile* ride = NULL);
-    QString name() { return pyScript->name; }
-    bool isCoreProcessor() const { return false; }
-    bool isAutomatedOnly() const;
+    bool postProcess(RideFile *rideFile, DataProcessorConfig *settings, QString op) override;
+    DataProcessorConfig *processorConfig(QWidget *parent, const RideFile* ride = NULL) override;
+    QString name() override { return pyScript->name; }
+    bool isCoreProcessor() const override { return false; }
+    bool isAutomatedOnly() const override;
 
 private:
     FixPyScript *pyScript;
