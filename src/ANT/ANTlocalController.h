ec2486a4f9eff013dca2a67df2f2a6d2de455060
Ivor Hewitt <ivor.hewitt@gmail.com>
2025-02-07 21:16:18 +0000

    Correct incomplete signal (#4616)
    Complements #4611

--- src/ANT/ANTlocalController.h
+++ src/ANT/ANTlocalController.h
@@ -86,7 +86,7 @@ signals:
     void rrData(uint16_t  measurementTime, uint8_t heartrateBeats, uint8_t instantHeartrate);
 
     void posData(uint8_t position);
-    void tcoreData(float core, float skin, int qual);
+    void tcoreData(float core, float skin, float hsi, int qual);
 
     // signal for passing remote control commands to train view
     void remoteControl(uint16_t command);
