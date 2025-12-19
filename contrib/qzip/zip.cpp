GoldenCheetah/GoldenCheetah

commit c67d913f11ce32c7bb7f1147191d131f17f8aaac

Deprecate QT5 support (#4751)
* Remove Qt5 Compatibility
* Remove QT compiler directives < QT6.5.3, set QT6.5.3 as minimum GC version
* Remove references to QT5 Video
Fixes #4750

contrib/qzip/zip.cpp
@@ -739,7 +739,7 @@ void ZipWriterPrivate::addEntry(EntryType type, const QString &fileName, const Q
 */
 ZipReader::ZipReader(const QString &archive, QIODevice::OpenMode mode)
 {
-    QScopedPointer<QFile> f(new QFile(archive));
+    std::unique_ptr<QFile> f = std::make_unique<QFile>(archive);
     f->open(mode);
     ZipReader::Status status;
     if (f->error() == QFile::NoError)
@@ -755,8 +755,7 @@ ZipReader::ZipReader(const QString &archive, QIODevice::OpenMode mode)
             status = FileError;
     }
 
-    d = new ZipReaderPrivate(f.data(), /*ownDevice=*/true);
-    f.take();
+    d = new ZipReaderPrivate(&(*f), /*ownDevice=*/true);
     d->status = status;
 }
 
@@ -1024,7 +1023,7 @@ void ZipReader::close()
 */
 ZipWriter::ZipWriter(const QString &fileName, QIODevice::OpenMode mode)
 {
-    QScopedPointer<QFile> f(new QFile(fileName));
+    std::unique_ptr<QFile> f = std::make_unique<QFile>(fileName);
     f->open(mode);
     ZipWriter::Status status;
     if (f->error() == QFile::NoError)
@@ -1040,8 +1039,7 @@ ZipWriter::ZipWriter(const QString &fileName, QIODevice::OpenMode mode)
             status = ZipWriter::FileError;
     }
 
-    d = new ZipWriterPrivate(f.data(), /*ownDevice=*/true);
-    f.take();
+    d = new ZipWriterPrivate(&(*f), /*ownDevice=*/true);
     d->status = status;
 }
 

commit be01a861b867a0511195839e14e57baa789750ce

Fix crash when importing compressed zip files
The bug was introduced by #4751 when QScopedPointer was replaced
by std::unique_ptr but release() was not called to replace take().

contrib/qzip/zip.cpp
@@ -514,7 +514,7 @@ void ZipReaderPrivate::scanFiles()
     int num_dir_entries = 0;
     EndOfDirectory eod;
     while (start_of_directory == -1) {
-        int pos = device->size() - sizeof(EndOfDirectory) - i;
+        size_t pos = device->size() - sizeof(EndOfDirectory) - i;
         if (pos < 0 || i > 65535) {
             qWarning() << "QZip: EndOfDirectory not found";
             return;
@@ -755,7 +755,7 @@ ZipReader::ZipReader(const QString &archive, QIODevice::OpenMode mode)
             status = FileError;
     }
 
-    d = new ZipReaderPrivate(&(*f), /*ownDevice=*/true);
+    d = new ZipReaderPrivate(&(*f.release()), /*ownDevice=*/true);
     d->status = status;
 }
 
@@ -1039,7 +1039,7 @@ ZipWriter::ZipWriter(const QString &fileName, QIODevice::OpenMode mode)
             status = ZipWriter::FileError;
     }
 
-    d = new ZipWriterPrivate(&(*f), /*ownDevice=*/true);
+    d = new ZipWriterPrivate(&(*f.release()), /*ownDevice=*/true);
     d->status = status;
 }
 