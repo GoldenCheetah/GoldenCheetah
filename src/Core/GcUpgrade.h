36abcc1edc320efb1f68f6a3ab2038887d27b603
Alejandro Martinez <amtriathlon@gmail.com>
2026-05-24 16:52:16 -0300

    Update Snapshot Builds
    Including Apple Silicon binary, feature complete
    but without access to Cloud Services yet.
    [publish binaries]

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -128,7 +128,7 @@
 #define VERSION36_BUILD   5000 // released 5/8/23
 #define VERSION37_BUILD   5005 // released 28/3/25
 #define VERSION37_SP1     5006 // released 20/11/25
-#define VERSION38_DEV2603 5011 // (MAR 2026) - latest snapshot 31/3/26
+#define VERSION38_DEV2603 5011 // (MAR 2026) - latest snapshot 24/5/26
 
 // will keep changing during testing and before final release
 #define VERSION31_BUILD VERSION31_UPG
@@ -136,7 +136,7 @@
 // the next two will with each build/release
 #define VERSION_LATEST 5011
 #define VERSION_STRING "V3.8-DEV2603"
-#define GC_VERSION VERSION_STRING // To force version string on non-tagged ci builds
+//#define GC_VERSION VERSION_STRING // To force version string on non-tagged ci builds
 
 // default config for this release cycle
 #define VERSION_CONFIG_PREFIX "http://www.goldencheetah.org/defaults/3.8"
