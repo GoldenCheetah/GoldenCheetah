efcaec3fd45def53e377b6ff021707886c482d2c
Alejandro Martinez <amtriathlon@gmail.com>
2026-08-09 19:06:20 -0300

    Update Snapshot Builds
    Changes from v3.8-RC1
    367514130 Optionally download default icons during upgrade or first use (#4909)
    ef01151a5 elevProfile distance on Train Html chart corrected (#4911)
    3fd5d61a2 Update German Translation
    7f822ab71 Cloud Upload - Don't allow for Planned Activities
    034799a5b Update Spanish translation (#4908)
    070ca8288 RealtimeData - Tidy up W'Bal computation
    345a79854 Pass RealtimeData by const-ref across Train telemetry slots
    1e80f0aee Remove Diary perspective from default Trends layout
    f7fa288b4 R/Python: add all=True/False parameter to Season Intervals API (#4907)
    d08f8aca8 Fix Python Chart crash from stale interval/ride counts (#4905)
    [publish binaries]

--- src/Core/GcUpgrade.h
+++ src/Core/GcUpgrade.h
@@ -130,7 +130,7 @@
 #define VERSION36_BUILD   5000 // released 5/8/23
 #define VERSION37_BUILD   5005 // released 28/3/25
 #define VERSION37_SP1     5006 // released 20/11/25
-#define VERSION38_RC1     5013 // RC1 (JUL 2026) - latest snapshot 25/7/26
+#define VERSION38_RC1     5013 // RC1 (JUL 2026) - latest snapshot 9/8/26
 
 // will keep changing during testing and before final release
 #define VERSION31_BUILD VERSION31_UPG
