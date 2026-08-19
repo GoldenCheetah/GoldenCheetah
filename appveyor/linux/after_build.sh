14539072ab8e780359d9f18ba9c37a9ee43ede5b
Alejandro Martinez <amtriathlon@gmail.com>
2026-08-18 22:55:13 -0300

    AppVeyor - Update Python AppImage version
    For Linux builds

--- appveyor/linux/after_build.sh
+++ appveyor/linux/after_build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # Python version configuration - update this when upgrading Python
-PYTHON_APPIMAGE_VERSION="3.11.15"
+PYTHON_APPIMAGE_VERSION="3.11.16"
 
 ### This script should be run from GoldenCheetah src directory after build
 cd src
